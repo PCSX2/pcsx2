2d450531cca6b88da05d629bbcff631641d20b75
cottonvibes <cottonvibes@a6443dda-0b58-4228-96e9-037be469359c>
2008-10-21 01:05:58 +0000

    reverted last counters change for now.
    git-svn-id: http://pcsx2-playground.googlecode.com/svn/trunk@223 a6443dda-0b58-4228-96e9-037be469359c

--- pcsx2/Counters.c
+++ pcsx2/Counters.c
@@ -67,12 +67,12 @@ void rcntSet() {
 	}
 
 	//Calculate HBlank
-	c = HBLANKCNT(0.5); //counters[4].CycleT - (cpuRegs.cycle - counters[4].sCycleT);
+	c = counters[4].CycleT - (cpuRegs.cycle - counters[4].sCycleT);//HBLANKCNT(0.5); 
 	if (c < nextCounter) nextCounter = c;
 	//if(nextCounter > 0x1000) SysPrintf("Nextcounter %x HBlank %x VBlank %x\n", nextCounter, c, counters[5].CycleT - (cpuRegs.cycle - counters[5].sCycleT));
 	
 	//Calculate VBlank
-	c = (VBLANKCNT(1) / 2); //counters[5].CycleT - (cpuRegs.cycle - counters[5].sCycleT);
+	c = counters[5].CycleT - (cpuRegs.cycle - counters[5].sCycleT);//(VBLANKCNT(1) / 2);
 	if (c < nextCounter) nextCounter = c;
 }
 
