36a2178e683d5dcbd2434796cd63c0092ababd6a
Jake.Stine <Jake.Stine@a6443dda-0b58-4228-96e9-037be469359c>
2009-02-03 06:28:08 +0000

    Part Two of plugin folder structure reorganization. Pcsx2 and SPU2ghz compile fine now, other plugins might need a wee bit of work yet.
    git-svn-id: http://pcsx2-playground.googlecode.com/svn/trunk@684 a6443dda-0b58-4228-96e9-037be469359c

--- common/tsvnrev/updateRevision.cmd
+++ common/tsvnrev/updateRevision.cmd
@@ -1,5 +1,5 @@
 @echo off
-%1 %2 %3\svnrev_template.h %2\svnrev.h
+SubWCRev.exe %1 %2\svnrev_template.h %1\svnrev.h
 if not ERRORLEVEL 0 (
     echo Automatic revision update unavailable, using generic template instead.
     echo You can safely ignore this message - see svnrev.h for details.
