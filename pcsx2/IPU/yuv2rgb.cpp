530989be427803a695326ef89b4f4ed495cf2b58
Forrest McDonald <forrestmcdonald@utexas.edu>
2014-07-06 14:25:56 -0700

    fix some undefined and perhaps broken behavior in yuv2rgb. The left shift multiplied by 4 and clears the sign bit, but we don't want the sign bit cleared. Do a multiply instead

--- pcsx2/IPU/yuv2rgb.cpp
+++ pcsx2/IPU/yuv2rgb.cpp
@@ -62,7 +62,7 @@ void yuv2rgb_reference(void)
 // Know the specification before you touch it.
 #define SSE_BYTES(x) {x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x}
 #define SSE_WORDS(x) {x, x, x, x, x, x, x, x}
-#define SSE_COEFFICIENTS(x) SSE_WORDS((x)<<2)
+#define SSE_COEFFICIENTS(x) SSE_WORDS((x)*4)
 
 struct SSE2_Tables
 {
