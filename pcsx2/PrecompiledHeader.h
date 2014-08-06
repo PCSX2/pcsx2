9fdb3187767ae3589a98e126175f885c9b8391e6
archshift <admin@archshift.com>
2014-08-06 03:46:15 -0700

    PrecompiledHeader.h: Removed WTF-redefines
    Removed redefinition of TRUE and FALSE, and removed typedef of int to BOOL

--- pcsx2/PrecompiledHeader.h
+++ pcsx2/PrecompiledHeader.h
@@ -58,13 +58,6 @@
 using std::min;
 using std::max;
 
-typedef int BOOL;
-
-#undef TRUE
-#undef FALSE
-#define TRUE  1
-#define FALSE 0
-
 
 //////////////////////////////////////////////////////////////////////////////////////////
 // Begin Pcsx2 Includes: Add items here that are local to Pcsx2 but stay relatively
