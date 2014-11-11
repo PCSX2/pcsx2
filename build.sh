06f53b268954adf49643ab580bdb9855404eeff7
3kinox <fafangi@gmail.com>
2014-11-11 14:50:56 +0100

    use -fabi_version=6 only for GSdx, solve bug with wxwidget reenable avx build for GSdx

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@
 
 #set -e # This terminates the script in case of any error
 
-arch64Build=$(uname -a | grep ARCH)
+arch64Build=$(uname -a | grep ARCH | grep x86_64)
 if [[ -e "arch64Build" ]]; then
 #add flags for archlinux
 flags=(-DCMAKE_BUILD_PO=FALSE -DwxWidgets_CONFIG_EXECUTABLE='/usr/bin/wx-config32-2.8' -DwxWidgets_wxrc_EXECUTABLE='/usr/bin/wxrc32-2.8')
