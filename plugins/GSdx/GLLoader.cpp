263583ebcc50c49c7e396f6e6f97a1c6af1554f5
Gregory Hainaut <gregory.hainaut@gmail.com>
2015-06-16 18:32:23 +0200

    Revert "gsdx-ogl: enable again geometry shader on Mesa"
    This partially reverts commit e774fecb779696871d15d61045828e332d233bde.
    
    Fix issue #605

--- plugins/GSdx/GLLoader.cpp
+++ plugins/GSdx/GLLoader.cpp
@@ -406,6 +406,10 @@ namespace GLLoader {
 		GLuint major_gl = s[dot-1]-'0';
 		GLuint minor_gl = s[dot+1]-'0';
 
+		if (mesa_amd_buggy_driver) {
+			fprintf(stderr, "Buggy driver detected. Geometry shaders will be disabled\n");
+			found_geometry_shader = false;
+		}
 		if (theApp.GetConfig("override_geometry_shader", -1) != -1) {
 			found_geometry_shader = !!theApp.GetConfig("override_geometry_shader", -1);
 			fprintf(stderr, "Overriding geometry shaders detection\n");
