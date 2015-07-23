65a7df95116c59af24899c5dfbfc1462a2cece27
Gregory Hainaut <gregory.hainaut@gmail.com>
2015-07-23 09:29:32 +0200

    gsdx-ogl: create a copy rect with conversion function
    Previous CopyRect function does a memcopy without conversion.
    
    This function will allow to use different format for input/output. Just a
    possibility for the future

--- plugins/GSdx/GSDeviceOGL.h
+++ plugins/GSdx/GSDeviceOGL.h
@@ -629,6 +629,7 @@ class GSDeviceOGL : public GSDevice
 	GSTexture* CopyOffscreen(GSTexture* src, const GSVector4& sRect, int w, int h, int format = 0, int ps_shader = 0);
 
 	void CopyRect(GSTexture* sTex, GSTexture* dTex, const GSVector4i& r);
+	void CopyRectConv(GSTexture* sTex, GSTexture* dTex, const GSVector4i& r);
 	void StretchRect(GSTexture* sTex, const GSVector4& sRect, GSTexture* dTex, const GSVector4& dRect, int shader = 0, bool linear = true);
 	void StretchRect(GSTexture* sTex, const GSVector4& sRect, GSTexture* dTex, const GSVector4& dRect, GLuint ps, bool linear = true);
 	void StretchRect(GSTexture* sTex, const GSVector4& sRect, GSTexture* dTex, const GSVector4& dRect, GLuint ps, GSBlendStateOGL* bs, bool linear = true);
