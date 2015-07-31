c83c3b37ee671a401a05ab27c251cef7e033999d
Gregory Hainaut <gregory.hainaut@gmail.com>
2015-07-31 23:23:17 +0200

    gsdx-ogl: depth support: better support of 16 bits z buffer
    Fix issue in socom2

--- plugins/GSdx/GSDeviceOGL.h
+++ plugins/GSdx/GSDeviceOGL.h
@@ -524,7 +524,7 @@ class GSDeviceOGL : public GSDevice
 
 	struct {
 		GLuint vs;		// program object
-		GLuint ps[15];	// program object
+		GLuint ps[16];	// program object
 		GLuint ln;		// sampler object
 		GLuint pt;		// sampler object
 		GSDepthStencilOGL* dss;
