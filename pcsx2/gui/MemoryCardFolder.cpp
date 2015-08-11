PCSX2/pcsx2

commit 30eb292b48c59c8c89751b3627562ef94d0c41cf

FolderMemoryCard: Remove the Mode parameter for opening files, just always open in r+b so we can both read and write with the same handle. This breaks metadata writes when a file is read before it's written.

pcsx2/gui/MemoryCardFolder.cpp
@@ -687,7 +687,7 @@ bool FolderMemoryCard::ReadFromFile( u8 *dest, u32 adr, u32 dataLength ) {
 	auto it = m_fileMetadataQuickAccess.find( fatCluster );
 	if ( it != m_fileMetadataQuickAccess.end() ) {
 		const u32 clusterNumber = it->second.consecutiveCluster;
-		wxFFile* file = m_lastAccessedFile.ReOpen( m_folderName, &it->second, L"rb" );
+		wxFFile* file = m_lastAccessedFile.ReOpen( m_folderName, &it->second );
 		if ( file->IsOpened() ) {
 			const u32 clusterOffset = ( page % 2 ) * PageSize + offset;
 			const u32 fileOffset = clusterNumber * ClusterSize + clusterOffset;
@@ -1111,7 +1111,7 @@ bool FolderMemoryCard::WriteToFile( const u8* src, u32 adr, u32 dataLength ) {
 		const u32 clusterNumber = it->second.consecutiveCluster;
 		
 		if ( m_performFileWrites ) {
-			wxFFile* file = m_lastAccessedFile.ReOpen( m_folderName, &it->second, L"r+b", true );
+			wxFFile* file = m_lastAccessedFile.ReOpen( m_folderName, &it->second, true );
 			if ( file->IsOpened() ) {
 				const u32 clusterOffset = ( page % 2 ) * PageSize + offset;
 				const u32 fileSize = entry->entry.data.length;
@@ -1316,7 +1316,7 @@ FileAccessHelper::~FileAccessHelper() {
 	this->Close();
 }
 
-wxFFile* FileAccessHelper::Open( const wxFileName& folderName, MemoryCardFileMetadataReference* fileRef, const wxString& mode, bool writeMetadata ) {
+wxFFile* FileAccessHelper::Open( const wxFileName& folderName, MemoryCardFileMetadataReference* fileRef, bool writeMetadata ) {
 	this->Close();
 
 	wxFileName fn( folderName );
@@ -1331,9 +1331,8 @@ wxFFile* FileAccessHelper::Open( const wxFileName& folderName, MemoryCardFileMet
 		createEmptyFile.Close();
 	}
 
-	m_file = new wxFFile( filename, mode );
+	m_file = new wxFFile( filename, L"r+b" );
 	m_entry = fileRef->entry;
-	m_mode = mode;
 
 	if ( writeMetadata ) {
 		const MemoryCardFileEntry* const entry = fileRef->entry;
@@ -1366,11 +1365,11 @@ wxFFile* FileAccessHelper::Open( const wxFileName& folderName, MemoryCardFileMet
 	return m_file;
 }
 
-wxFFile* FileAccessHelper::ReOpen( const wxFileName& folderName, MemoryCardFileMetadataReference* fileRef, const wxString& mode, bool writeMetadata ) {
-	if ( m_file && fileRef->entry == m_entry && mode == m_mode ) {
+wxFFile* FileAccessHelper::ReOpen( const wxFileName& folderName, MemoryCardFileMetadataReference* fileRef, bool writeMetadata ) {
+	if ( m_file && fileRef->entry == m_entry ) {
 		return m_file;
 	} else {
-		return this->Open( folderName, fileRef, mode, writeMetadata );
+		return this->Open( folderName, fileRef, writeMetadata );
 	}
 }
 

pcsx2/gui/MemoryCardFolder.h
@@ -189,14 +189,13 @@ class FileAccessHelper {
 protected:
 	wxFFile* m_file;
 	const MemoryCardFileEntry* m_entry;
-	wxString m_mode;
 
 public:
 	FileAccessHelper();
 	~FileAccessHelper();
 
 	// Get an already opened file if possible, or open a new one and remember it
-	wxFFile* ReOpen( const wxFileName& folderName, MemoryCardFileMetadataReference* fileRef, const wxString& mode, bool writeMetadata = false );
+	wxFFile* ReOpen( const wxFileName& folderName, MemoryCardFileMetadataReference* fileRef, bool writeMetadata = false );
 	// Close an open file, if any
 	void Close();
 
@@ -206,7 +205,7 @@ public:
 
 protected:
 	// Open a new file and remember it for later
-	wxFFile* Open( const wxFileName& folderName, MemoryCardFileMetadataReference* fileRef, const wxString& mode, bool writeMetadata = false );
+	wxFFile* Open( const wxFileName& folderName, MemoryCardFileMetadataReference* fileRef, bool writeMetadata = false );
 };
 
 // --------------------------------------------------------------------------------------

commit cb0152367705be19d169a5dafca935a65f4dc95a

FolderMemoryCard: Fix the metadata writing problem that was introduced in 30eb292b48c59c8c89751b3627562ef94d0c41cf.

pcsx2/gui/MemoryCardFolder.cpp
@@ -1317,9 +1317,11 @@ void FolderMemoryCard::CalculateECC( u8* ecc, const u8* data ) {
 
 FileAccessHelper::FileAccessHelper() {
 	m_files.clear();
+	m_lastWrittenFileRef = nullptr;
 }
 
 FileAccessHelper::~FileAccessHelper() {
+	m_lastWrittenFileRef = nullptr;
 	this->CloseAll();
 }
 
@@ -1341,37 +1343,67 @@ wxFFile* FileAccessHelper::Open( const wxFileName& folderName, MemoryCardFileMet
 	m_files.emplace( entry, file );
 
 	if ( writeMetadata ) {
-		// write metadata of file if it's nonstandard
 		fn.AppendDir( L"_pcsx2_meta" );
-		if ( cleanedFilename || entry->entry.data.mode != MemoryCardFileEntry::DefaultFileMode || entry->entry.data.attr != 0 ) {
-			if ( !fn.DirExists() ) {
-				fn.Mkdir();
-			}
-			wxFFile metaFile( fn.GetFullPath(), L"wb" );
-			if ( metaFile.IsOpened() ) {
-				metaFile.Write( entry->entry.raw, sizeof( entry->entry.raw ) );
-				metaFile.Close();
-			}
-		} else {
-			// if metadata is standard remove metadata file if it exists
-			if ( fn.FileExists() ) {
-				wxRemoveFile( fn.GetFullPath() );
-
-				// and remove the metadata dir if it's now empty
-				wxDir metaDir( fn.GetPath() );
-				if ( metaDir.IsOpened() && !metaDir.HasFiles() ) {
-					wxRmdir( fn.GetPath() );
-				}
-			}
-		}
+		const bool metadataIsNonstandard = cleanedFilename || entry->entry.data.mode != MemoryCardFileEntry::DefaultFileMode || entry->entry.data.attr != 0;
+		WriteMetadata( metadataIsNonstandard, fn, entry );
 	}
 
 	return file;
 }
 
+void FileAccessHelper::WriteMetadata( const wxFileName& folderName, MemoryCardFileMetadataReference* fileRef ) {
+	wxFileName fn( folderName );
+	bool cleanedFilename = fileRef->GetPath( &fn );
+	fn.AppendDir( L"_pcsx2_meta" );
+
+	const MemoryCardFileEntry* const entry = fileRef->entry;
+	const bool metadataIsNonstandard = cleanedFilename || entry->entry.data.mode != MemoryCardFileEntry::DefaultFileMode || entry->entry.data.attr != 0;
+
+	WriteMetadata( metadataIsNonstandard, fn, entry );
+}
+
+void FileAccessHelper::WriteMetadata( bool metadataIsNonstandard, const wxFileName& metadataFilename, const MemoryCardFileEntry* const entry ) {
+	if ( metadataIsNonstandard ) {
+		// write metadata of file if it's nonstandard
+		if ( !metadataFilename.DirExists() ) {
+			metadataFilename.Mkdir();
+		}
+		wxFFile metaFile( metadataFilename.GetFullPath(), L"wb" );
+		if ( metaFile.IsOpened() ) {
+			metaFile.Write( entry->entry.raw, sizeof( entry->entry.raw ) );
+			metaFile.Close();
+		}
+	} else {
+		// if metadata is standard remove metadata file if it exists
+		if ( metadataFilename.FileExists() ) {
+			wxRemoveFile( metadataFilename.GetFullPath() );
+
+			// and remove the metadata dir if it's now empty
+			wxDir metaDir( metadataFilename.GetPath() );
+			if ( metaDir.IsOpened() && !metaDir.HasFiles() ) {
+				wxRmdir( metadataFilename.GetPath() );
+			}
+		}
+	}
+}
+
 wxFFile* FileAccessHelper::ReOpen( const wxFileName& folderName, MemoryCardFileMetadataReference* fileRef, bool writeMetadata ) {
 	auto it = m_files.find( fileRef->entry );
 	if ( it != m_files.end() ) {
+		// we already have a handle to this file
+
+		// if the caller wants to write metadata and we haven't done this recently, do so and remember that we did
+		if ( writeMetadata ) {
+			if ( m_lastWrittenFileRef != fileRef ) {
+				WriteMetadata( folderName, fileRef );
+				m_lastWrittenFileRef = fileRef;
+			}
+		} else {
+			if ( m_lastWrittenFileRef != nullptr ) {
+				m_lastWrittenFileRef = nullptr;
+			}
+		}
+
 		return it->second;
 	} else {
 		return this->Open( folderName, fileRef, writeMetadata );

pcsx2/gui/MemoryCardFolder.h
@@ -188,6 +188,7 @@ struct MemoryCardFileMetadataReference {
 class FileAccessHelper {
 protected:
 	std::map<const MemoryCardFileEntry* const, wxFFile*> m_files;
+	MemoryCardFileMetadataReference* m_lastWrittenFileRef; // we remember this to reduce redundant metadata checks/writes
 
 public:
 	FileAccessHelper();
@@ -209,6 +210,9 @@ public:
 protected:
 	// Open a new file and remember it for later
 	wxFFile* Open( const wxFileName& folderName, MemoryCardFileMetadataReference* fileRef, bool writeMetadata = false );
+
+	void WriteMetadata( const wxFileName& folderName, MemoryCardFileMetadataReference* fileRef );
+	void WriteMetadata( bool metadataIsNonstandard, const wxFileName& metadataFilename, const MemoryCardFileEntry* const entry );
 };
 
 // --------------------------------------------------------------------------------------