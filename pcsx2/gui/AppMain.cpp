55eebb5fd2a50f930c939ec85fb7b73026839d77
Jonathan Li <jonathan.li@hotmail.co.uk>
2015-08-20 20:28:47 +0100

    Fix cheats path and folder creation
    For the install version on Linux, PCSX2 now searches for cheats in $HOME
    instead of /usr. For the install version on Windows, PCSX2 now searches
    for cheats in Documents/PCSX2 instead of the cheats directory where
    PCSX2 is installed.
    
    Also make a few small consistency changes.

--- pcsx2/gui/AppMain.cpp
+++ pcsx2/gui/AppMain.cpp
@@ -834,6 +834,7 @@ void AppApplySettings( const AppConfig* oldconf )
 	g_Conf->Folders.MemoryCards.Mkdir();
 	g_Conf->Folders.Savestates.Mkdir();
 	g_Conf->Folders.Snapshots.Mkdir();
+	g_Conf->Folders.Cheats.Mkdir();
 	g_Conf->Folders.CheatsWS.Mkdir();
 
 	g_Conf->EmuOptions.BiosFilename = g_Conf->FullpathToBios();
