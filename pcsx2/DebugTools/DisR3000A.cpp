130a84d2af58265f9afb80f1dd789c5e1b6af123
Gregory Hainaut <gregory.hainaut@gmail.com>
2015-09-21 09:32:13 +0200

    pcsx2:debugger: miss a coma
    Thanks RebelliousX

--- pcsx2/DebugTools/DisR3000A.cpp
+++ pcsx2/DebugTools/DisR3000A.cpp
@@ -28,7 +28,7 @@ namespace R3000A
 		"r0", "at", "v0", "v1", "a0", "a1","a2", "a3",
 		"t0", "t1", "t2", "t3", "t4", "t5","t6", "t7",
 		"s0", "s1", "s2", "s3", "s4", "s5","s6", "s7",
-		"t8", "t9", "k0", "k1", "gp", "sp","fp", "ra"
+		"t8", "t9", "k0", "k1", "gp", "sp","fp", "ra",
 		"HI", "LO" };
 
 	const char * const disRNameCP0[] = {
