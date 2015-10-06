fa04f0970e2435f0bf4077a696b795729f1da07b
Avi Halachmi (:avih) <avihpit@yahoo.com>
2015-10-07 01:10:39 +0300

    gui: ESC on fullscreen: better resilience
    Make sure we exit fullscreen only if the GS window is visible and in fullscreen.
    
    While we don't currently have such cases, it's possible that the Sys_Suspend
    command is called while it's already suspended (and the GS window is hidden)
    but the code without this patch would have incorrectly made it visible again.

--- pcsx2/gui/GlobalCommands.cpp
+++ pcsx2/gui/GlobalCommands.cpp
@@ -282,7 +282,7 @@ namespace Implementations
 	void Sys_Suspend()
 	{
 		GSFrame* gsframe = wxGetApp().GetGsFramePtr();
-		if (gsframe) {
+		if (gsframe && gsframe->IsShown() && gsframe->IsFullScreen()) {
 			// On some cases, probably due to driver bugs, if we don't exit fullscreen then
 			// the content stays on screen. Try to prevent that by first exiting fullscreen,
 			// but don't update the internal PCSX2 state/config, and PCSX2 will restore
