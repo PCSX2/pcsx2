ff77708e0e6107d325977bd447a033c24d0487b8
Akash <ssj2akash@gmail.com>
2015-11-04 21:20:40 +0530

    SPU2-X: New INI variable for cycle delay

--- plugins/spu2-x/src/Linux/Config.cpp
+++ plugins/spu2-x/src/Linux/Config.cpp
@@ -60,6 +60,7 @@ float VolumeAdjustBR;
 float VolumeAdjustSL;
 float VolumeAdjustSR;
 float VolumeAdjustLFE;
+int delayCycles;
 
 bool postprocess_filter_enabled = true;
 bool postprocess_filter_dealias = false;
@@ -108,6 +109,7 @@ void ReadSettings()
 	VolumeAdjustSL = powf(10, VolumeAdjustSLdb / 10);
 	VolumeAdjustSR = powf(10, VolumeAdjustSRdb / 10);
 	VolumeAdjustLFE = powf(10, VolumeAdjustLFEdb / 10);
+	delayCycles = CfgReadInt(L"DEBUG", L"DelayCycles", 4);
 
 
 	wxString temp;
@@ -165,6 +167,7 @@ void WriteSettings()
 	CfgWriteStr(L"OUTPUT",L"Output_Module", mods[OutputModule]->GetIdent() );
 	CfgWriteInt(L"OUTPUT",L"Latency", SndOutLatencyMS);
 	CfgWriteInt(L"OUTPUT",L"Synch_Mode", SynchMode);
+	CfgWriteInt(L"DEBUG", L"DelayCycles", delayCycles);
 
 	PortaudioOut->WriteSettings();
 	SoundtouchCfg::WriteSettings();
