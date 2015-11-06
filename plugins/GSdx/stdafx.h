0eb771c2a672d0880246aaf6f2ca7c499eb345b4
Jonathan Li <jonathan.li@hotmail.co.uk>
2015-11-06 18:52:33 +0000

    gsdx|spu2-x: Use atlcomcli.h for CComPtr definition
    VS2013/VS2015 community includes it, so everything will still compile
    for everyone.
    
    Also delete common/include/comptr.h.

--- plugins/GSdx/stdafx.h
+++ plugins/GSdx/stdafx.h
@@ -42,8 +42,7 @@
 #include <d3d9.h>
 #include <d3dx9.h>
 #include <comutil.h>
-#include "../../common/include/comptr.h"
-
+#include <atlcomcli.h>
 
 #define D3DCOLORWRITEENABLE_RGBA (D3DCOLORWRITEENABLE_RED | D3DCOLORWRITEENABLE_GREEN | D3DCOLORWRITEENABLE_BLUE | D3DCOLORWRITEENABLE_ALPHA)
 #define D3D11_SHADER_MACRO D3D10_SHADER_MACRO
