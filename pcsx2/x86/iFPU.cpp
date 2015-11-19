fb1a19f15771b1f298a2a64b89b7e11eb0b38414
Gregory Hainaut <gregory.hainaut@gmail.com>
2015-11-20 00:46:26 +0100

    x86emitter: clean unused legacy type
    Note: no need to check -1 on register allocation failure (a exception will be fired)

--- pcsx2/x86/iFPU.cpp
+++ pcsx2/x86/iFPU.cpp
@@ -439,7 +439,6 @@ void FPU_ADD_SUB(int regd, int regt, int issub)
 
 	if (tempecx != ECX)	{ Console.Error("FPU: ADD/SUB Allocation Error!"); tempecx = ECX;}
 	if (temp2 == -1)	{ Console.Error("FPU: ADD/SUB Allocation Error!"); temp2 = EAX;}
-	if (xmmtemp == -1)	{ Console.Error("FPU: ADD/SUB Allocation Error!"); xmmtemp = XMM0;}
 
 	SSE2_MOVD_XMM_to_R(tempecx, regd);
 	SSE2_MOVD_XMM_to_R(temp2, regt);
