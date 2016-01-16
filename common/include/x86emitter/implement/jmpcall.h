395d43fd9c4adee480033b37ccde388a08d72fd5
Gregory Hainaut <gregory.hainaut@gmail.com>
2016-01-17 00:02:21 +0100

    x86emitter: jump: use base type directly
    which fix 16 bits call (no prefix)

--- common/include/x86emitter/implement/jmpcall.h
+++ common/include/x86emitter/implement/jmpcall.h
@@ -43,11 +43,8 @@ struct xImpl_JmpCall
 {
 	bool	isJmp;
 
-	void operator()( const xRegister32& absreg ) const;
-	void operator()( const xIndirect32& src ) const;
-
-	void operator()( const xRegister16& absreg ) const;
-	void operator()( const xIndirect16& src ) const;
+	void operator()( const xRegisterInt& absreg ) const;
+	void operator()( const xIndirect32orLess& src ) const;
 
 	// Special form for calling functions.  This form automatically resolves the
 	// correct displacement based on the size of the instruction being generated.
