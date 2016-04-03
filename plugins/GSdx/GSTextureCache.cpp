c4ebdd1f37ac6018f67663bc6f7f01aa6f5f6708
Gregory Hainaut <gregory.hainaut@gmail.com>
2016-04-03 12:30:14 +0200

    gsdx ogl: allow to read the depth buffer
    Unfortunately can't be tested on gs dump
    
    Ought to impact #1276 (Incognito/Eat Sleep Play's games, Nocturne too)

--- plugins/GSdx/GSTextureCache.cpp
+++ plugins/GSdx/GSTextureCache.cpp
@@ -719,6 +719,14 @@ void GSTextureCache::InvalidateLocalMem(GSOffset* off, const GSVector4i& r)
 	// No depth handling please.
 	if (psm == PSM_PSMZ32 || psm == PSM_PSMZ24 || psm == PSM_PSMZ16 || psm == PSM_PSMZ16S) {
 		GL_INS("ERROR: InvalidateLocalMem depth format isn't supported");
+		if (m_can_convert_depth) {
+			for(auto t : m_dst[DepthStencil]) {
+				if(GSUtil::HasSharedBits(bp, psm, t->m_TEX0.TBP0, t->m_TEX0.PSM)) {
+					// Read the full depth buffer for easy testing
+					Read(t, t->m_valid);
+				}
+			}
+		}
 		return;
 	}
 
