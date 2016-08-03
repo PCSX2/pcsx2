PCSX2/pcsx2

commit 46ba9aa1177308fdb438438eabe58de2f5c9279d

gsdx: Defer GSdxApp initialisation on Linux only
vector push_back causes a SIGILL signal on a Nehalem (SSE4.2) QEMU VM
when compiled with GCC 6.1.1.

However, an empty constructor causes illegal instruction exceptions to be
generated on a Windows VM.

So here's an inbetween that looks stupid but works on what I've tested.

plugins/GSdx/GSdx.cpp
@@ -127,6 +127,21 @@ GSdxApp theApp;
 
 GSdxApp::GSdxApp()
 {
+	// Empty constructor causes an illegal instruction exception on an SSE4.2 machine on Windows.
+	// Non-empty doesn't, but raises a SIGILL signal when compiled against GCC 6.1.1.
+	// So here's a compromise.
+#ifdef _WIN32
+	Init();
+#endif
+}
+
+void GSdxApp::Init()
+{
+	static bool is_initialised = false;
+	if (is_initialised)
+		return;
+	is_initialised = true;
+
 	m_ini = "inis/GSdx.ini";
 	m_section = "Settings";
 

plugins/GSdx/GS.cpp
@@ -127,6 +127,8 @@ EXPORT_C_(int) GSinit()
 	// can crash if the CPU does not support the instruction set.
 	// Initialise it here instead - it's not ideal since we have to strip the
 	// const type qualifier from all the affected variables.
+	theApp.Init();
+
 	GSBlock::InitVectors();
 	GSClut::InitVectors();
 	GSDrawScanlineCodeGenerator::InitVectors();
@@ -814,6 +816,8 @@ EXPORT_C GSconfigure()
 	try
 	{
 		if(!GSUtil::CheckSSE()) return;
+
+		theApp.Init();
 
 #ifdef _WIN32
 		GSDialog::InitCommonControls();

plugins/GSdx/GSdx.h
@@ -35,7 +35,8 @@ class GSdxApp
 public:
 	GSdxApp();
 
-    void* GetModuleHandlePtr();
+	void Init();
+	void* GetModuleHandlePtr();
 
 #ifdef _WIN32
  	HMODULE GetModuleHandle() {return (HMODULE)GetModuleHandlePtr();}

plugins/GSdx/PSX/GPU.cpp
@@ -59,6 +59,8 @@ EXPORT_C_(uint32) PSEgetLibVersion()
 
 EXPORT_C_(int32) GPUinit()
 {
+	theApp.Init();
+
 	GSVector4i::InitVectors();
 	GSVector4::InitVectors();
 #if _M_SSE >= 0x500
@@ -153,6 +155,8 @@ EXPORT_C_(int32) GPUopen(void* hWnd)
 
 EXPORT_C_(int32) GPUconfigure()
 {
+	theApp.Init();
+
 #ifdef _WIN32
 
 	GPUSettingsDlg dlg;

commit 869f4d1898c2fa4c5df15074e6be04e4eb086feb

gsdx: Only set ini path in GSdxApp::Init() if it's empty
Fixes a regression introduced by 46ba9aa1177308fdb438438eabe58de2f5c9279d,
where the Linux GS replayer would always use the options in inis/GSdx.ini
(or use the default options if that doesn't exist) to replay the dump,
instead of using the GSdx.ini from the specified ini folder.

plugins/GSdx/GSdx.cpp
@@ -142,7 +142,8 @@ void GSdxApp::Init()
 		return;
 	is_initialised = true;
 
-	m_ini = "inis/GSdx.ini";
+	if (m_ini.empty())
+		m_ini = "inis/GSdx.ini";
 	m_section = "Settings";
 
 #ifdef _WIN32