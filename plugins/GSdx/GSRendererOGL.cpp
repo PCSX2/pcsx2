6c6ed5f44313530d353859d89e652a850438f24d
Gregory Hainaut <gregory.hainaut@gmail.com>
2016-09-25 10:05:44 +0200

    gsdx ogl: trigger the special RW framebuffer shader when only Alpha is written (#1588)
    * gsdx ogl: trigger the special RW framebuffer shader when only Alpha is written
    
    It will work for both Jak and Ratchet&Clank and without CRC

--- plugins/GSdx/GSRendererOGL.cpp
+++ plugins/GSdx/GSRendererOGL.cpp
@@ -1155,7 +1155,8 @@ void GSRendererOGL::DrawPrims(GSTexture* rt, GSTexture* ds, GSTextureCache::Sour
 	// Always check if primitive overlap. The function will return PRIM_OVERLAP_UNKNOW for non sprite primitive
 	m_prim_overlap = PrimitiveOverlap();
 	if ((m_context->FRAME.Block() == m_context->TEX0.TBP0) && PRIM->TME && m_sw_blending && (m_prim_overlap != PRIM_OVERLAP_NO) && (m_vertex.next > 2)) {
-		if (m_game.title == CRC::Jak1 || m_game.title == CRC::Jak2 || m_game.title == CRC::Jak3 || m_game.title == CRC::JakX) {
+		if (m_context->FRAME.FBMSK == 0x00FFFFFF) {
+			// Ratchet & Clank / Jak uses this pattern to compute the shadows. Alpha (multiplication) tfx is mostly equivalent to -1/+1 stencil operation
 			GL_INS("ERROR: Source and Target are the same! Let's sample the framebuffer");
 			m_ps_sel.tex_is_fb = 1;
 			m_require_full_barrier = true;
