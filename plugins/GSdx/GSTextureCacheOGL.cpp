ae6f26f3efbeec217ce616c0e51d7488614ab558
Jonathan Li <jonathan.li@hotmail.co.uk>
2016-11-09 22:39:53 +0000

    gsdx: Skip texture cache read if any dimension is 0
    Fixes a crash at the PSX logo if either the DX9 or DX11 hardware
    renderer is used.

--- plugins/GSdx/GSTextureCacheOGL.cpp
+++ plugins/GSdx/GSTextureCacheOGL.cpp
@@ -30,7 +30,7 @@ GSTextureCacheOGL::GSTextureCacheOGL(GSRenderer* r)
 
 void GSTextureCacheOGL::Read(Target* t, const GSVector4i& r)
 {
-	if (!t->m_dirty.empty() || (r.width() == 0 && r.height() == 0))
+	if (!t->m_dirty.empty() || r.width() == 0 || r.height() == 0)
 		return;
 
 	const GIFRegTEX0& TEX0 = t->m_TEX0;
