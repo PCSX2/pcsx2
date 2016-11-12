65c1a449123dbe8bab628f5b470db7250c72fd0e
Gregory Hainaut <gregory.hainaut@gmail.com>
2016-11-12 12:16:54 +0100

    pcsx2 gui: use builtin wx checkbox list
    The widget comes with an integrated scrollbar so it will greatly reduce
    the size of the panel.
    
    If you can increase the size of the window (gnome3/gtk3?), the scrollbar
    ought to be removed (not tested)
    
    v2: avoid scrollbar on gamefix panel for big screen

--- pcsx2/gui/Panels/GameFixesPanel.cpp
+++ pcsx2/gui/Panels/GameFixesPanel.cpp
@@ -22,110 +22,56 @@ using namespace pxSizerFlags;
 Panels::GameFixesPanel::GameFixesPanel( wxWindow* parent )
 	: BaseApplicableConfigPanel_SpecificConfig( parent )
 {
-	wxStaticBoxSizer& groupSizer = *new wxStaticBoxSizer( wxVERTICAL, this, _("Gamefixes") );
-
 	// NOTE: Order of checkboxes must match the order of the bits in the GamefixOptions structure!
 	// NOTE2: Don't make this static, because translations can change at run-time :)
-	// NOTE3: This panel is way to big, header text was reduced as a temporary solution.
-	//		  However if you want add more gamefix, a final solution must be found (noteboox/scrolling...)
 
-	struct CheckTextMess
+	const wxString check_text[GamefixId_COUNT] =
 	{
-		wxString label, tooltip;
+		_("VU Add Hack - Fixes Tri-Ace games boot crash."),
+		_("VU Clip Flag Hack - For Persona games (SuperVU recompiler only!)"),
+		_("FPU Compare Hack - For Digimon Rumble Arena 2."),
+		_("FPU Multiply Hack - For Tales of Destiny."),
+		_("FPU Negative Div Hack - For Gundam games."),
+		_("VU XGkick Hack - For Erementar Gerad."),
+		_("FFX videos fix - Fixes bad graphics overlay in FFX videos."),
+		_("EE timing hack - Multi purpose hack. Try if all else fails."),
+		_("Skip MPEG hack - Skips videos/FMVs in games to avoid game hanging/freezes."),
+		_("OPH Flag hack - Try if your game freezes showing the same frame."),
+		_("Ignore DMAC writes when it is busy."),
+		_("Simulate VIF1 FIFO read ahead. Fixes slow loading games."),
+		_("Delay VIF1 Stalls (VIF1 FIFO) - For SOCOM 2 HUD and Spy Hunter loading hang."),
+		_("Enable the GIF FIFO (slower but needed for Hotwheels, Wallace and Gromit, DJ Hero)"),
+		_("Switch to GSdx software rendering when an FMV plays"),
+		_("Preload TLB hack to avoid tlb miss on Goemon"),
+		_("VU I bit Hack avoid constant recompilation (Scarface The World Is Yours)"),
 	};
 
-	const CheckTextMess check_text[GamefixId_COUNT] =
-	{
-		{
-			_("VU Add Hack - Fixes Tri-Ace games boot crash."),
-			_("Games that need this hack to boot:\n * Star Ocean 3\n * Radiata Stories\n * Valkyrie Profile 2")
-		},
-		{
-			_("VU Clip Flag Hack - For Persona games (SuperVU recompiler only!)"),
-			wxEmptyString
-		},
-		{
-			_("FPU Compare Hack - For Digimon Rumble Arena 2."),
-			wxEmptyString
-		},
-		{
-			_("FPU Multiply Hack - For Tales of Destiny."),
-			wxEmptyString
-		},
-		{
-			_("FPU Negative Div Hack - For Gundam games."),
-			wxEmptyString
-		},
-		{
-			_("VU XGkick Hack - For Erementar Gerad."),
-			wxEmptyString
-		},
-		{
-			_("FFX videos fix - Fixes bad graphics overlay in FFX videos."),
-			wxEmptyString
-		},
-		{
-			_("EE timing hack - Multi purpose hack. Try if all else fails."),
-			pxEt( L"Known to affect following games:\n * Digital Devil Saga (Fixes FMV and crashes)\n * SSX (Fixes bad graphics and crashes)\n * Resident Evil: Dead Aim (Causes garbled textures)"
-			)
-		},
-		{
-			_("Skip MPEG hack - Skips videos/FMVs in games to avoid game hanging/freezes."),
-			wxEmptyString
-		},
-		{
-			_("OPH Flag hack - Try if your game freezes showing the same frame."),
-			pxEt( L"Known to affect following games:\n * Bleach Blade Battler\n * Growlanser II and III\n * Wizardry"
-			)
-		},
-		{
-			_("Ignore DMAC writes when it is busy."),
-			pxEt( L"Known to affect following games:\n * Mana Khemia 1 (Going \"off campus\")\n"
-			)
-		},
-		{
-			_("Simulate VIF1 FIFO read ahead. Fixes slow loading games."),
-			pxEt( L"Known to affect following games:\n * Test Drive Unlimited\n * Transformers"
-			)
-		},
-		{
-			_("Delay VIF1 Stalls (VIF1 FIFO) - For SOCOM 2 HUD and Spy Hunter loading hang."),
-			wxEmptyString
-		},
-		{
-			_("Enable the GIF FIFO (slower but needed for Hotwheels, Wallace and Gromit, DJ Hero)"),
-			wxEmptyString
-		},
-		{
-			_("Switch to GSdx software rendering when an FMV plays"),
-			wxEmptyString
-		},
-		{
-			_("Preload TLB hack to avoid tlb miss on Goemon"),
-			wxEmptyString
-		},
-		{
-			_("VU I bit Hack avoid constant recompilation (Scarface The World Is Yours)"),
-			wxEmptyString
-		}
-	};
+	m_listbox = new wxCheckListBox( this, wxID_ANY, wxDefaultPosition, wxDefaultSize,
+			GamefixId_COUNT, check_text, wxLB_NEEDED_SB );
 
-	for( int i=0; i<GamefixId_COUNT; ++i )
-	{
-		groupSizer += (m_checkbox[i] = new pxCheckBox( this, check_text[i].label ));
-		m_checkbox[i]->SetToolTip( check_text[i].tooltip );
-	}
+	m_listbox->SetToolTip( pxEt(
+				L"VU Add Hack - Games that need this hack to boot:\n * Star Ocean 3\n * Radiata Stories\n * Valkyrie Profile 2\n\n"
+				L"EE timing hack - Known to affect following games:\n * Digital Devil Saga (Fixes FMV and crashes)\n * SSX (Fixes bad graphics and crashes)\n * Resident Evil: Dead Aim (Causes garbled textures)\n\n"
+				L"OPH Flag hack - Known to affect following games:\n * Bleach Blade Battler\n * Growlanser II and III\n * Wizardry\n\n"
+				L"DMAC write - Known to affect following games:\n * Mana Khemia 1 (Going \"off campus\")\n\n"
+				L"VIF1 FIFO - Known to affect following games:\n * Test Drive Unlimited\n * Transformers\n\n"
+				));
 
 	m_check_Enable = new pxCheckBox( this, _("Enable manual game fixes [Not recommended]"),
 		pxE( L"It's better to enable 'Automatic game fixes' at the main menu instead, and leave this page empty. ('Automatic' means: selectively use specific tested fixes for specific games). Manual game fixes will NOT increase your performance. In fact they may decrease it."
 		)
 	);
 
+	// By default, wx will create a small widget with a scrollbar. If display
+	// is big enough, set a min size to avoid the scrollbar
+	if( wxSystemSettings::GetMetric(wxSYS_SCREEN_Y) > 850 )
+		m_listbox->SetMinSize(wxSize(-1, 400));
+
 	m_check_Enable->SetToolTip(pxE(L"Gamefixes can work around wrong emulation in some titles. \nThey may also cause compatibility or performance issues.\n\nThe safest way is to make sure that all game fixes are completely disabled.")).SetSubPadding( 1 );
 	m_check_Enable->SetValue( g_Conf->EnableGameFixes );
 
 	*this	+= m_check_Enable	| StdExpand();
-	*this	+= groupSizer		| pxCenter;
+	*this	+= m_listbox 		| pxCenter;
 
 	Bind(wxEVT_CHECKBOX, &GameFixesPanel::OnEnable_Toggled, this, m_check_Enable->GetId());
 
@@ -138,7 +84,7 @@ void Panels::GameFixesPanel::Apply()
 
 	Pcsx2Config::GamefixOptions& opts( g_Conf->EmuOptions.Gamefixes );
 	for (GamefixId i=GamefixId_FIRST; i < pxEnumEnd; ++i)
-		opts.Set((GamefixId)i, m_checkbox[i]->GetValue());
+		opts.Set((GamefixId)i, m_listbox->IsChecked(i));
 
 	// make sure the user's command line specifications are disabled (if present).
 	wxGetApp().Overrides.ApplyCustomGamefixes = false;
@@ -147,8 +93,8 @@ void Panels::GameFixesPanel::Apply()
 void Panels::GameFixesPanel::EnableStuff( AppConfig* configToUse )
 {
 	if (!configToUse) configToUse = g_Conf.get();
-	for (GamefixId i=GamefixId_FIRST; i < pxEnumEnd; ++i)
-		m_checkbox[i]->Enable(m_check_Enable->GetValue() && !configToUse->EnablePresets);
+
+	m_listbox->Enable(m_check_Enable->GetValue() && !configToUse->EnablePresets);
 
 	Layout();
 }
@@ -171,10 +117,10 @@ void Panels::GameFixesPanel::ApplyConfigToGui( AppConfig& configToApply, int fla
 {
 	const Pcsx2Config::GamefixOptions& opts( configToApply.EmuOptions.Gamefixes );
 	for (GamefixId i=GamefixId_FIRST; i < pxEnumEnd; ++i)
-		m_checkbox[i]->SetValue( opts.Get((GamefixId)i) );//apply the use/don't-use fix values
-	
+		m_listbox->Check( (int)i, opts.Get((GamefixId)i) );//apply the use/don't-use fix values
+
 	m_check_Enable->SetValue( configToApply.EnableGameFixes );//main gamefixes checkbox
 	EnableStuff( &configToApply );// enable/disable the all the fixes controls according to the main one
-	
+
 	this->Enable(!configToApply.EnablePresets);
 }
