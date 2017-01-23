702267512eaf9431b8718c510f1f71a890f4f4f0
lightningterror <lightningterror@yahoo.com>
2017-01-23 17:20:35 +0100

    Update Copyright v2
    Revernt changes , update Gregory's name

--- plugins/GSdx/GSOsdManager.cpp
+++ plugins/GSdx/GSOsdManager.cpp
@@ -1,5 +1,5 @@
 /*
- *	Copyright (C) 2014-2016 Gregory hainaut
+ *	Copyright (C) 2014-2016 Gregory Hainaut
  *	Copyright (C) 2016-2016 Jason Brown
  *
  *  This Program is free software; you can redistribute it and/or modify
