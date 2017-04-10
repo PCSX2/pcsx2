dc53ecd88f791a03d7d0e3cbb5b6ee57ca386547
KousukeItsagame <bleukousuke@inbox.com>
2017-04-10 10:02:26 +0200

    removing line

--- common/include/Pcsx2Defs.h
+++ common/include/Pcsx2Defs.h
@@ -168,7 +168,6 @@ static const int __pagesize = PCSX2_PAGESIZE;
 #define PCSX2_ALIGNED16_EXTERN(x) extern __declspec(align(16)) x
 
 #define __noinline __declspec(noinline)
-#define thread_local __declspec(thread)
 
 // Don't know if there are Visual C++ equivalents of these.
 #define likely(x) (!!(x))
