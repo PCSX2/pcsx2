PCSX2/pcsx2

commit 358954d69ec359e33fa92c64760ef57e98fdcec3

GSdx-ogl: Printing console messages v2 (#1954)
GSdx-ogl: Console messages v2

Follow up to
commit/ec63b04719fd9c05a6aeeacb55dc1c54f5ef145b

Add intel broken driver wiki link message in console (OpenGL).
Print intel / amd buggy driver message once in console (OpenGL).
Pring texture barrier and viewpoint array info once in console (OpenGL).

plugins/GSdx/GLLoader.cpp
@@ -442,25 +442,33 @@ namespace GLLoader {
 		status &= status_and_override(found_GL_ARB_texture_barrier, "GL_ARB_texture_barrier");
 		status &= status_and_override(found_GL_ARB_get_texture_sub_image, "GL_ARB_get_texture_sub_image");
 
-		if (fglrx_buggy_driver) {
-			fprintf(stderr, "The OpenGL hardware renderer is slow on AMD GPUs due to an inefficient driver.\n"
-			"Check out the link below for further information.\n"
-			"https://github.com/PCSX2/pcsx2/wiki/OpenGL-and-AMD-GPUs---All-you-need-to-know\n");
-		}
+		if (s_first_load) {
+			if (fglrx_buggy_driver) {
+				fprintf(stderr, "The OpenGL hardware renderer is slow on AMD GPUs due to an inefficient driver.\n"
+					"Check out the link below for further information.\n"
+					"https://github.com/PCSX2/pcsx2/wiki/OpenGL-and-AMD-GPUs---All-you-need-to-know\n");
+			}
 
-		if (!found_GL_ARB_viewport_array) {
-			fprintf(stderr, "GL_ARB_viewport_array: not supported ! function pointer will be replaced\n");
-			glScissorIndexed   = ReplaceGL::ScissorIndexed;
-			glViewportIndexedf = ReplaceGL::ViewportIndexedf;
-		}
+			if (intel_buggy_driver) {
+				fprintf(stderr, "The OpenGL renderer is inefficient on Intel GPUs due to an inefficient driver.\n"
+					"Check out the link below for further information.\n"
+					"https://github.com/PCSX2/pcsx2/wiki/OpenGL-and-Intel-GPUs-All-you-need-to-know\n");
+			}
 
-		if (!found_GL_ARB_texture_barrier) {
-			fprintf(stderr, "GL_ARB_texture_barrier: not supported ! Rendering will be corrupted\n");
-			glTextureBarrier = ReplaceGL::TextureBarrier;
+			if (!found_GL_ARB_viewport_array) {
+				fprintf(stderr, "GL_ARB_viewport_array is not supported! Function pointer will be replaced\n");
+				glScissorIndexed   = ReplaceGL::ScissorIndexed;
+				glViewportIndexedf = ReplaceGL::ViewportIndexedf;
+			}
+
+			if (!found_GL_ARB_texture_barrier) {
+				fprintf(stderr, "GL_ARB_texture_barrier is not supported! Blending emulation will not be supported\n");
+				glTextureBarrier = ReplaceGL::TextureBarrier;
+			}
 		}
 
 #ifdef _WIN32
-		// Thanks you Intel to not provide support of basic feature on your iGPU
+		// Thank you Intel for not providing support of basic features on your IGPUs.
 		if (!found_GL_ARB_direct_state_access) {
 			Emulate_DSA::Init();
 		}

commit 2bc8a647a69c9fa2deb5327f99fd46c6ed7fce98

GSdx-ogl: Fix GSdx crashing on Intel igpus
Regression was introduced in #1954
GSdx caused the emulator to crash when the renderer was restarted.

It may have affected older gpus from nvidia/amd
with older OpenGL support as well.

plugins/GSdx/GLLoader.cpp
@@ -454,17 +454,19 @@ namespace GLLoader {
 					"Check out the link below for further information.\n"
 					"https://github.com/PCSX2/pcsx2/wiki/OpenGL-and-Intel-GPUs-All-you-need-to-know\n");
 			}
+		}
 
-			if (!found_GL_ARB_viewport_array) {
+		if (!found_GL_ARB_viewport_array) {
+			glScissorIndexed   = ReplaceGL::ScissorIndexed;
+			glViewportIndexedf = ReplaceGL::ViewportIndexedf;
+			if (s_first_load)
 				fprintf(stderr, "GL_ARB_viewport_array is not supported! Function pointer will be replaced\n");
-				glScissorIndexed   = ReplaceGL::ScissorIndexed;
-				glViewportIndexedf = ReplaceGL::ViewportIndexedf;
-			}
+		}
 
-			if (!found_GL_ARB_texture_barrier) {
+		if (!found_GL_ARB_texture_barrier) {
+			glTextureBarrier = ReplaceGL::TextureBarrier;
+			if (s_first_load)
 				fprintf(stderr, "GL_ARB_texture_barrier is not supported! Blending emulation will not be supported\n");
-				glTextureBarrier = ReplaceGL::TextureBarrier;
-			}
 		}
 
 #ifdef _WIN32