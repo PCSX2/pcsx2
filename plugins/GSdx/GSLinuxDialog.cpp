1cbd4c53873e270a4a08cd302282df4cc1bc333f
lightningterror <lightningterror@yahoo.com>
2017-10-04 23:50:36 +0200

    GSdx: Add CPU Frame buffer Conversion Hack
    Add HW Hack that enables Framebuffer Conversion on the CPU instead of the GPU.
    Can fix broken textures on games but at the cost of slower performance.
    List of games: Harry Potter games, FIFA Street games.
    Games like Call of Duty, Kung fu Panda might also be affected as well as others
    especially on Direct3D.
    Add HW Hack GUI option on Windows/Linux for 4-bit and 8-bit Framebuffer conversion hack
    named "Frame Buffer Conversion".

--- plugins/GSdx/GSLinuxDialog.cpp
+++ plugins/GSdx/GSLinuxDialog.cpp
@@ -380,7 +380,7 @@ void populate_shader_table(GtkWidget* shader_table)
 	InsertWidgetInTable(shader_table , shaderfx_check);
 	InsertWidgetInTable(shader_table , shader_label        , shader);
 	InsertWidgetInTable(shader_table , shader_conf_label   , shader_conf);
-	InsertWidgetInTable(shader_table , tv_shader_label, tv_shader);
+	InsertWidgetInTable(shader_table , tv_shader_label     , tv_shader);
 }
 
 void populate_hack_table(GtkWidget* hack_table)
@@ -396,6 +396,7 @@ void populate_hack_table(GtkWidget* hack_table)
 	GtkWidget* preload_gs_check    = CreateCheckBox("Preload Frame", "preload_frame_with_gs_data");
 	GtkWidget* hack_fast_inv       = CreateCheckBox("Fast Texture Invalidation", "UserHacks_DisablePartialInvalidation");
 	GtkWidget* hack_depth_check    = CreateCheckBox("Disable Depth Emulation", "UserHacks_DisableDepthSupport");
+	GtkWidget* hack_cpu_fbcv       = CreateCheckBox("Frame Buffer Conversion", "UserHacks_CPU_FB_Conversion");
 	GtkWidget* hack_auto_flush     = CreateCheckBox("Auto Flush Primitives", "UserHacks_AutoFlush");
 	GtkWidget* hack_unscale_prim   = CreateCheckBox("Unscale Point&Line Primitives", "UserHacks_unscale_point_line");
 	GtkWidget* hack_merge_sprite   = CreateCheckBox("Merge Sprite", "UserHacks_merge_pp_sprite");
@@ -422,6 +423,7 @@ void populate_hack_table(GtkWidget* hack_table)
 	AddTooltip(preload_gs_check, IDC_PRELOAD_GS);
 	AddTooltip(hack_fast_inv, IDC_FAST_TC_INV);
 	AddTooltip(hack_depth_check, IDC_TC_DEPTH);
+	AddTooltip(hack_cpu_fbcv, IDC_CPU_FB_CONVERSION);
 	AddTooltip(hack_auto_flush, IDC_AUTO_FLUSH);
 	AddTooltip(hack_unscale_prim, IDC_UNSCALE_POINT_LINE);
 	AddTooltip(hack_merge_sprite, IDC_MERGE_PP_SPRITE);
@@ -434,7 +436,7 @@ void populate_hack_table(GtkWidget* hack_table)
 	// Hack
 	InsertWidgetInTable(hack_table , hack_fast_inv       , hack_auto_flush);
 	InsertWidgetInTable(hack_table , hack_depth_check    , preload_gs_check);
-	InsertWidgetInTable(hack_table , hack_wrap_mem);
+	InsertWidgetInTable(hack_table , hack_wrap_mem       , hack_cpu_fbcv);
 	// Upscaling hack
 	InsertWidgetInTable(hack_table , hack_wild_check     , align_sprite_check);
 	InsertWidgetInTable(hack_table , hack_unscale_prim   , hack_merge_sprite);
@@ -448,19 +450,19 @@ void populate_hack_table(GtkWidget* hack_table)
 
 void populate_main_table(GtkWidget* main_table)
 {
-	GtkWidget* render_label     = left_label("Renderer:");
-	GtkWidget* render_combo_box = CreateComboBoxFromVector(theApp.m_gs_renderers, "Renderer");
+	GtkWidget* render_label        = left_label("Renderer:");
+	GtkWidget* render_combo_box    = CreateComboBoxFromVector(theApp.m_gs_renderers, "Renderer");
 	GtkWidget* interlace_label     = left_label("Interlacing (F5):");
 	GtkWidget* interlace_combo_box = CreateComboBoxFromVector(theApp.m_gs_interlace, "interlace");
-	GtkWidget* filter_label     = left_label("Texture Filtering:");
-	GtkWidget* filter_combo_box = CreateComboBoxFromVector(theApp.m_gs_bifilter, "filter");
+	GtkWidget* filter_label        = left_label("Texture Filtering:");
+	GtkWidget* filter_combo_box    = CreateComboBoxFromVector(theApp.m_gs_bifilter, "filter");
 
 	AddTooltip(filter_label, filter_combo_box, IDC_FILTER);
 
 	s_table_line = 0;
 	InsertWidgetInTable(main_table, render_label, render_combo_box);
 	InsertWidgetInTable(main_table, interlace_label, interlace_combo_box);
-	InsertWidgetInTable(main_table, filter_label  , filter_combo_box);
+	InsertWidgetInTable(main_table, filter_label, filter_combo_box);
 }
 
 void populate_debug_table(GtkWidget* debug_table)
