PCSX2/pcsx2

commit f314c2a4d9a0ab349c0357b692312c137e2e6ac6

cdvdgigaherz: Simplify Q subchannel relative offset calculation
That way there's no need to calculate the track length for each track.

plugins/cdvdGigaherz/src/CDVD.cpp
@@ -352,15 +352,10 @@ s32 CALLBACK CDVDreadSubQ(u32 lsn, cdvdSubQ *subq)
     lsn_to_msf(&subq->discM, &subq->discS, &subq->discF, lsn + 150);
 
     u8 i = strack;
-    while (i <= etrack) {
-        if (lsn <= tracks[i].length)
-            break;
-        lsn -= tracks[i].length;
-        i++;
-    }
+    while (i < etrack && lsn < tracks[i + 1].start_lba)
+        ++i;
 
-    if (i > etrack)
-        i = etrack;
+    lsn -= tracks[i].start_lba;
 
     lsn_to_msf(&subq->trackM, &subq->trackS, &subq->trackF, lsn);
 

commit 148654a8487280f42638aff9fdf2e40628023e65

cdvdgigaherz: Fix Q subchannel relative offset calculation
The wrong comparison was used, so all the relative offsets were
completely wrong. Fixes the wrong track issue in the CD player.

Regression introduced in f314c2a4d9a0ab349c0357b692312c137e2e6ac6.

plugins/cdvdGigaherz/src/CDVD.cpp
@@ -306,7 +306,7 @@ EXPORT s32 CALLBACK CDVDreadSubQ(u32 lsn, cdvdSubQ *subq)
     lsn_to_msf(&subq->discM, &subq->discS, &subq->discF, lsn + 150);
 
     u8 i = strack;
-    while (i < etrack && lsn < tracks[i + 1].start_lba)
+    while (i < etrack && lsn >= tracks[i + 1].start_lba)
         ++i;
 
     lsn -= tracks[i].start_lba;