c4068e5188c5096d4741acc9a4909e13c6cee993
lightningterror <lightningterror@yahoo.com>
2018-05-20 16:27:50 +0200

    GSdx: Update Spyro Hw fix/hack.
    Revert merge of Spyro Eternal Night / New Beginning hw hack.
    Update Spyro New Beginning hack - fixes menu/hud flicker in HW mode.
    
    SW mode still has issues with the menu/hud elements.
    
    Improve #1490

--- plugins/GSdx/GSRendererHW.cpp
+++ plugins/GSdx/GSRendererHW.cpp
@@ -1140,8 +1140,8 @@ GSRendererHW::Hacks::Hacks()
 	m_oi_list.push_back(HackEntry<OI_Ptr>(CRC::MetalSlug6, CRC::RegionCount, &GSRendererHW::OI_MetalSlug6));
 	m_oi_list.push_back(HackEntry<OI_Ptr>(CRC::RozenMaidenGebetGarden, CRC::RegionCount, &GSRendererHW::OI_RozenMaidenGebetGarden));
 	m_oi_list.push_back(HackEntry<OI_Ptr>(CRC::StarWarsForceUnleashed, CRC::RegionCount, &GSRendererHW::OI_StarWarsForceUnleashed));
-	m_oi_list.push_back(HackEntry<OI_Ptr>(CRC::SpyroNewBeginning, CRC::RegionCount, &GSRendererHW::OI_SpyroGames));
-	m_oi_list.push_back(HackEntry<OI_Ptr>(CRC::SpyroEternalNight, CRC::RegionCount, &GSRendererHW::OI_SpyroGames));
+	m_oi_list.push_back(HackEntry<OI_Ptr>(CRC::SpyroNewBeginning, CRC::RegionCount, &GSRendererHW::OI_SpyroNewBeginning));
+	m_oi_list.push_back(HackEntry<OI_Ptr>(CRC::SpyroEternalNight, CRC::RegionCount, &GSRendererHW::OI_SpyroEternalNight));
 	m_oi_list.push_back(HackEntry<OI_Ptr>(CRC::SuperManReturns, CRC::RegionCount, &GSRendererHW::OI_SuperManReturns));
 	m_oi_list.push_back(HackEntry<OI_Ptr>(CRC::TalesOfLegendia, CRC::RegionCount, &GSRendererHW::OI_TalesOfLegendia));
 	m_oi_list.push_back(HackEntry<OI_Ptr>(CRC::ArTonelico2, CRC::RegionCount, &GSRendererHW::OI_ArTonelico2));
@@ -1581,7 +1581,29 @@ bool GSRendererHW::OI_StarWarsForceUnleashed(GSTexture* rt, GSTexture* ds, GSTex
 	return true;
 }
 
-bool GSRendererHW::OI_SpyroGames(GSTexture* rt, GSTexture* ds, GSTextureCache::Source* t)
+bool GSRendererHW::OI_SpyroNewBeginning(GSTexture* rt, GSTexture* ds, GSTextureCache::Source* t)
+{
+	// uint32 FBP = m_context->FRAME.Block();
+	uint32 TBP = m_context->TEX0.TBP0;
+	uint32 FPSM = m_context->FRAME.PSM;
+
+	// Spyro A New Beginning PAL doesn't play well with FBP so let's use TBP to fix the menu/ui flickering.
+	// We can't merge the two hacks OI_SpyroNewBeginning and OI_SpyroEternalNight because Eternal Night PAL flickers with TBP, the opposite of New Beginning.
+
+	if(PRIM->TME)
+	{
+		// Old code (FBP == 0x0 || FBP == 0x01180)
+		// First two TBP NTSC, other two PAL.
+		if((TBP == 0x37c0 || TBP == 0x3020 || TBP == 0x34a0 || TBP == 0x3a00) && FPSM == PSM_PSMCT32 && (m_vt.m_eq.z && m_vt.m_min.p.z == 0))
+		{
+			m_dev->ClearDepth(ds);
+		}
+	}
+
+	return true;
+}
+
+bool GSRendererHW::OI_SpyroEternalNight(GSTexture* rt, GSTexture* ds, GSTextureCache::Source* t)
 {
 	uint32 FBP = m_context->FRAME.Block();
 	uint32 FPSM = m_context->FRAME.PSM;
