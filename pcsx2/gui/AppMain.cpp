81faa33a38d224e593aeeb97da231ac2884fd09c
Jonathan Li <jonathan.li@hotmail.co.uk>
2018-08-24 21:14:27 +0100

    debugger: Reset breakpoint skip on boot
    Fixes an issue where the first breakpoint after rebooting a game may
    be skipped if the following sequence takes place:
     - The first breakpoint after booting the game is triggered once.
     - The user hits run to resume the game
     - The user reboots the game without any other breakpoint being
     triggered.

--- pcsx2/gui/AppMain.cpp
+++ pcsx2/gui/AppMain.cpp
@@ -1089,6 +1089,7 @@ protected:
 
 		CoreThread.ResetQuick();
 		symbolMap.Clear();
+		CBreakPoints::SetSkipFirst(0);
 
 		CDVDsys_SetFile(CDVD_SourceType::Iso, g_Conf->CurrentIso );
 		if( m_UseCDVDsrc )
