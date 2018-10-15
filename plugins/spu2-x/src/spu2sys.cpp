6b22450a1bcaf2899ec6c1f14dde80b8b314bbca
lightningterror <lightningterror@yahoo.com>
2018-10-15 13:03:31 +0200

    SPU2-X: Fixes a regression with Wallace And Gromit: Curse Of The Were-Rabbit sound cutting off.
    Follow up from PR #2315
    Partially reverts commit:
    https://github.com/PCSX2/pcsx2/commit/57cbc901b

--- plugins/spu2-x/src/spu2sys.cpp
+++ plugins/spu2-x/src/spu2sys.cpp
@@ -1080,10 +1080,12 @@ static void __fastcall RegWrite_VoiceAddr(u16 value)
 
         // NextA has been confirmed to not be allowed to be written to, so code has been commented out.
         // FlatOut & Soul Reaver 2 trigger these cases, but don't produce issues enabled or disabled.
+        // Wallace And Gromit: Curse Of The Were-Rabbit triggers case 4 to produce proper sound,
+        // without it some sound effects get cut off.
 
         case 4:
-            //thisvoice.NextA = ((value & 0x0F) << 16) | (thisvoice.NextA & 0xFFF8) | 1;
-            //thisvoice.SCurrent = 28;
+            thisvoice.NextA = ((value & 0x0F) << 16) | (thisvoice.NextA & 0xFFF8) | 1;
+            thisvoice.SCurrent = 28;
             break;
 
         case 5:
