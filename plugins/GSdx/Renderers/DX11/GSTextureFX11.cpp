c8f53f32fc0abd3bec5dbaf94d064c78965bb8e8
lightningterror <lightningterror@yahoo.com>
2018-12-11 01:09:13 +0100

    GSdx-d3d11: Add the shader fetch in GSTextureFX11.
    Allows us to fetch the channel from the tfx shader.

--- plugins/GSdx/Renderers/DX11/GSTextureFX11.cpp
+++ plugins/GSdx/Renderers/DX11/GSTextureFX11.cpp
@@ -209,7 +209,7 @@ void GSDevice11::SetupPS(PSSelector sel, const PSConstantBuffer* cb, PSSamplerSe
 
 	if(i == m_ps.end())
 	{
-		std::string str[22];
+		std::string str[23];
 
 		str[0] = format("%d", sel.fst);
 		str[1] = format("%d", sel.wms);
@@ -231,8 +231,9 @@ void GSDevice11::SetupPS(PSSelector sel, const PSConstantBuffer* cb, PSSamplerSe
 		str[17] = format("%d", sel.point_sampler);
 		str[18] = format("%d", sel.shuffle);
 		str[19] = format("%d", sel.read_ba);
-		str[20] = format("%d", sel.fmt >> 2);
-		str[21] = format("%d", m_upscale_multiplier);
+		str[20] = format("%d", sel.channel);
+		str[21] = format("%d", sel.fmt >> 2);
+		str[22] = format("%d", m_upscale_multiplier);
 
 		D3D_SHADER_MACRO macro[] =
 		{
@@ -256,8 +257,9 @@ void GSDevice11::SetupPS(PSSelector sel, const PSConstantBuffer* cb, PSSamplerSe
 			{"PS_POINT_SAMPLER", str[17].c_str()},
 			{"PS_SHUFFLE", str[18].c_str() },
 			{"PS_READ_BA", str[19].c_str() },
-			{"PS_PAL_FMT", str[20].c_str() },
-			{"PS_SCALE_FACTOR", str[21].c_str() },
+			{"PS_CHANNEL_FETCH", str[20].c_str() },
+			{"PS_PAL_FMT", str[21].c_str() },
+			{"PS_SCALE_FACTOR", str[22].c_str() },
 			{NULL, NULL},
 		};
 
