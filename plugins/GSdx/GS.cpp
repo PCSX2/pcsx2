3819e22432232f1a4dedced7f99c2797c364522a
Kojin <kojin@protonmail.com>
2018-12-14 09:26:51 -0500

    gsdx-osd: Add support for custom colors

--- plugins/GSdx/GS.cpp
+++ plugins/GSdx/GS.cpp
@@ -459,12 +459,12 @@ static int _GSopen(void** dsp, const char* title, GSRendererType renderer, int t
 
 EXPORT_C_(void) GSosdLog(const char *utf8, uint32 color)
 {
-	if(s_gs && s_gs->m_dev) s_gs->m_dev->m_osd.Log(utf8, color);
+	if(s_gs && s_gs->m_dev) s_gs->m_dev->m_osd.Log(utf8);
 }
 
 EXPORT_C_(void) GSosdMonitor(const char *key, const char *value, uint32 color)
 {
-	if(s_gs && s_gs->m_dev) s_gs->m_dev->m_osd.Monitor(key, value, color);
+	if(s_gs && s_gs->m_dev) s_gs->m_dev->m_osd.Monitor(key, value);
 }
 
 EXPORT_C_(int) GSopen2(void** dsp, uint32 flags)
