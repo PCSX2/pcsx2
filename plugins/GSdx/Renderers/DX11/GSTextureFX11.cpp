0c0e0e92666ec8af9a5cf3b842da63986e47e0ac
lightningterror <lightningterror@yahoo.com>
2018-12-18 08:37:41 +0100

    GSdx-d3d11: Add support for dfmt shader macro.

--- plugins/GSdx/Renderers/DX11/GSTextureFX11.cpp
+++ plugins/GSdx/Renderers/DX11/GSTextureFX11.cpp
@@ -209,7 +209,7 @@ void GSDevice11::SetupPS(PSSelector sel, const PSConstantBuffer* cb, PSSamplerSe
 
 	if(i == m_ps.end())
 	{
-		std::string str[26];
+		std::string str[27];
 
 		str[0] = format("%d", sel.fst);
 		str[1] = format("%d", sel.wms);
@@ -234,9 +234,10 @@ void GSDevice11::SetupPS(PSSelector sel, const PSConstantBuffer* cb, PSSamplerSe
 		str[20] = format("%d", sel.channel);
 		str[21] = format("%d", sel.tales_of_abyss_hle);
 		str[22] = format("%d", sel.urban_chaos_hle);
-		str[23] = format("%d", sel.depth_fmt);
-		str[24] = format("%d", sel.fmt >> 2);
-		str[25] = format("%d", m_upscale_multiplier);
+		str[23] = format("%d", sel.dfmt);
+		str[24] = format("%d", sel.depth_fmt);
+		str[25] = format("%d", sel.fmt >> 2);
+		str[26] = format("%d", m_upscale_multiplier);
 
 		D3D_SHADER_MACRO macro[] =
 		{
@@ -263,9 +264,10 @@ void GSDevice11::SetupPS(PSSelector sel, const PSConstantBuffer* cb, PSSamplerSe
 			{"PS_CHANNEL_FETCH", str[20].c_str() },
 			{"PS_TALES_OF_ABYSS_HLE", str[21].c_str() },
 			{"PS_URBAN_CHAOS_HLE", str[22].c_str() },
-			{"PS_DEPTH_FMT", str[23].c_str() },
-			{"PS_PAL_FMT", str[24].c_str() },
-			{"PS_SCALE_FACTOR", str[25].c_str() },
+			{"PS_DFMT", str[23].c_str() },
+			{"PS_DEPTH_FMT", str[24].c_str() },
+			{"PS_PAL_FMT", str[25].c_str() },
+			{"PS_SCALE_FACTOR", str[26].c_str() },
 			{NULL, NULL},
 		};
 
