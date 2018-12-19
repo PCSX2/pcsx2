1ad71a9b85a4093b3366c19ea5ea6261542d3854
lightningterror <lightningterror@yahoo.com>
2018-12-19 17:17:39 +0100

    GSdx-gui: Remove Direct3D9 option from the gui.
    D3D9 will be broken once #2756 is merged so let's disable it to avoid
    any potential crashing if the render is selected.
    
    Complete D3D9 purge will shortly follow afterwards.

--- plugins/GSdx/GSdx.cpp
+++ plugins/GSdx/GSdx.cpp
@@ -200,10 +200,8 @@ void GSdxApp::Init()
 	m_section = "Settings";
 
 #ifdef _WIN32
-	m_gs_renderers.push_back(GSSetting(static_cast<uint32>(GSRendererType::DX9_HW), "Direct3D 9", "Hardware"));
 	m_gs_renderers.push_back(GSSetting(static_cast<uint32>(GSRendererType::DX1011_HW), "Direct3D 11", "Hardware"));
 	m_gs_renderers.push_back(GSSetting(static_cast<uint32>(GSRendererType::OGL_HW), "OpenGL", "Hardware"));
-	m_gs_renderers.push_back(GSSetting(static_cast<uint32>(GSRendererType::DX9_SW), "Direct3D 9", "Software"));
 	m_gs_renderers.push_back(GSSetting(static_cast<uint32>(GSRendererType::DX1011_SW), "Direct3D 11", "Software"));
 	m_gs_renderers.push_back(GSSetting(static_cast<uint32>(GSRendererType::OGL_SW), "OpenGL", "Software"));
 #else // Linux
