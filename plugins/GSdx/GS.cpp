dd3a0ee3c1180fbf7e58e81fe5280937daf9494b
lightningterror <lightningterror@yahoo.com>
2018-12-19 19:41:00 +0100

    GSdx: Remove d3d9 code from GS.cpp.
    Cases and mentions of DX9_SW, DX9_HW, DX9_OpenCL.

--- plugins/GSdx/GS.cpp
+++ plugins/GSdx/GS.cpp
@@ -331,7 +331,6 @@ static int _GSopen(void** dsp, const char* title, GSRendererType renderer, int t
 
 		switch (renderer)
 		{
-		case GSRendererType::DX9_SW:
 		case GSRendererType::DX1011_SW:
 		case GSRendererType::OGL_SW:
 			renderer_mode = "(Software renderer)";
@@ -339,7 +338,6 @@ static int _GSopen(void** dsp, const char* title, GSRendererType renderer, int t
 		case GSRendererType::Null:
 			renderer_mode = "(Null renderer)";
 			break;
-		case GSRendererType::DX9_OpenCL:
 		case GSRendererType::DX1011_OpenCL:
 		case GSRendererType::OGL_OpenCL:
 			renderer_mode = "(OpenCL)";
@@ -353,13 +351,6 @@ static int _GSopen(void** dsp, const char* title, GSRendererType renderer, int t
 		{
 		default:
 #ifdef _WIN32
-		case GSRendererType::DX9_HW:
-		case GSRendererType::DX9_SW:
-		case GSRendererType::DX9_OpenCL:
-			dev = new GSDevice9();
-			s_renderer_name = " D3D9";
-			renderer_fullname = "Direct3D 9";
-			break;
 		case GSRendererType::DX1011_HW:
 		case GSRendererType::DX1011_SW:
 		case GSRendererType::DX1011_OpenCL:
@@ -395,10 +386,6 @@ static int _GSopen(void** dsp, const char* title, GSRendererType renderer, int t
 			{
 			default:
 #ifdef _WIN32
-			case GSRendererType::DX9_HW:
-				s_gs = (GSRenderer*)new GSRendererDX9();
-				s_renderer_type = " HW";
-				break;
 			case GSRendererType::DX1011_HW:
 				s_gs = (GSRenderer*)new GSRendererDX11();
 				s_renderer_type = " HW";
@@ -408,7 +395,6 @@ static int _GSopen(void** dsp, const char* title, GSRendererType renderer, int t
 				s_gs = (GSRenderer*)new GSRendererOGL();
 				s_renderer_type = " HW";
 				break;
-			case GSRendererType::DX9_SW:
 			case GSRendererType::DX1011_SW:
 			case GSRendererType::OGL_SW:
 				s_gs = new GSRendererSW(threads);
@@ -418,7 +404,6 @@ static int _GSopen(void** dsp, const char* title, GSRendererType renderer, int t
 				s_gs = new GSRendererNull();
 				s_renderer_type = "";
 				break;
-			case GSRendererType::DX9_OpenCL:
 			case GSRendererType::DX1011_OpenCL:
 			case GSRendererType::OGL_OpenCL:
 #ifdef ENABLE_OPENCL
@@ -494,18 +479,13 @@ EXPORT_C_(int) GSopen2(void** dsp, uint32 flags)
 	if (renderer != GSRendererType::Undefined && stored_toggle_state != toggle_state)
 	{
 #ifdef _WIN32
-		GSRendererType best_sw_renderer = GSUtil::CheckDirect3D11Level() >= D3D_FEATURE_LEVEL_10_0 ? GSRendererType::DX1011_SW : GSRendererType::DX9_SW;
-
 		switch (renderer) {
 			// Use alternative renderer (SW if currently using HW renderer, and vice versa, keeping the same API and API version)
-		case GSRendererType::DX9_SW: renderer = GSRendererType::DX9_HW; break;
-		case GSRendererType::DX9_HW: renderer = GSRendererType::DX9_SW; break;
-		case GSRendererType::DX1011_SW: renderer = GSRendererType::DX1011_HW; break;
-		case GSRendererType::DX1011_HW: renderer = GSRendererType::DX1011_SW; break;
-		case GSRendererType::OGL_SW: renderer = GSRendererType::OGL_HW; break;
-		case GSRendererType::OGL_HW: renderer = GSRendererType::OGL_SW; break;
-		default: renderer = best_sw_renderer; break;// If wasn't using one of the above mentioned ones, use best SW renderer.
-
+			case GSRendererType::DX1011_SW: renderer = GSRendererType::DX1011_HW; break;
+			case GSRendererType::DX1011_HW: renderer = GSRendererType::DX1011_SW; break;
+			case GSRendererType::OGL_SW: renderer = GSRendererType::OGL_HW; break;
+			case GSRendererType::OGL_HW: renderer = GSRendererType::OGL_SW; break;
+			default: renderer = GSRendererType::DX1011_SW; break; // If wasn't using one of the above mentioned ones, use best SW renderer.
 		}
 
 #endif
@@ -552,7 +532,7 @@ EXPORT_C_(int) GSopen(void** dsp, const char* title, int mt)
 
 #ifdef _WIN32
 
-		renderer = GSUtil::CheckDirect3D11Level() >= D3D_FEATURE_LEVEL_10_0 ? GSRendererType::DX1011_SW : GSRendererType::DX9_SW;
+		renderer = GSRendererType::DX1011_SW;
 
 #endif
 
