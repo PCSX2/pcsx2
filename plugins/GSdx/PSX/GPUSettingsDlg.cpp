ea6e445a8dccbd584e1cbed0cbd6758b6e8c52f8
lightningterror <lightningterror@yahoo.com>
2018-12-21 16:42:30 +0100

    GSdx: Remove support for D3D9 device on ps1 emu compatibility.
    Note: Resolution scaling is unavailable right now so I disabled the
    option, if someone feels like fixing it they can do so.

--- plugins/GSdx/PSX/GPUSettingsDlg.cpp
+++ plugins/GSdx/PSX/GPUSettingsDlg.cpp
@@ -35,36 +35,8 @@ void GPUSettingsDlg::OnInit()
 {
 	__super::OnInit();
 
-	m_modes.clear();
-
-	{
-		D3DDISPLAYMODE mode;
-		memset(&mode, 0, sizeof(mode));
-		m_modes.push_back(mode);
-
-		ComboBoxAppend(IDC_RESOLUTION, "Please select...", (LPARAM)&m_modes.back(), true);
-
-		if(CComPtr<IDirect3D9> d3d = Direct3DCreate9(D3D_SDK_VERSION))
-		{
-			uint32 w = theApp.GetConfigI("ModeWidth");
-			uint32 h = theApp.GetConfigI("ModeHeight");
-			uint32 hz = theApp.GetConfigI("ModeRefreshRate");
-
-			uint32 n = d3d->GetAdapterModeCount(D3DADAPTER_DEFAULT, D3DFMT_X8R8G8B8);
-
-			for(uint32 i = 0; i < n; i++)
-			{
-				if(S_OK == d3d->EnumAdapterModes(D3DADAPTER_DEFAULT, D3DFMT_X8R8G8B8, i, &mode))
-				{
-					m_modes.push_back(mode);
-
-					std::string str = format("%dx%d %dHz", mode.Width, mode.Height, mode.RefreshRate);
-
-					ComboBoxAppend(IDC_RESOLUTION, str.c_str(), (LPARAM)&m_modes.back(), w == mode.Width && h == mode.Height && hz == mode.RefreshRate);
-				}
-			}
-		}
-	}
+	// FIXME: Someone can fix this to use D3D11 or OGL Device, D3D9 is no longer supported.
+	ComboBoxAppend(IDC_RESOLUTION, "Please select...");
 
 	ComboBoxInit(IDC_RENDERER, theApp.m_gpu_renderers, theApp.GetConfigI("Renderer"));
 	ComboBoxInit(IDC_FILTER, theApp.m_gpu_filter, theApp.GetConfigI("filter"));
@@ -89,15 +61,6 @@ bool GPUSettingsDlg::OnCommand(HWND hWnd, UINT id, UINT code)
 	else if(id == IDOK)
 	{
 		INT_PTR data;
-
-		if(ComboBoxGetSelData(IDC_RESOLUTION, data))
-		{
-			const D3DDISPLAYMODE* mode = (D3DDISPLAYMODE*)data;
-
-			theApp.SetConfig("ModeWidth", (int)mode->Width);
-			theApp.SetConfig("ModeHeight", (int)mode->Height);
-			theApp.SetConfig("ModeRefreshRate", (int)mode->RefreshRate);
-		}
 
 		if(ComboBoxGetSelData(IDC_RENDERER, data))
 		{
@@ -143,6 +106,10 @@ void GPUSettingsDlg::UpdateControls()
 		bool dx11 = renderer == GPURendererType::D3D11_SW;
 		bool null = renderer == GPURendererType::NULL_Renderer;
 		bool sw = !null;
+		bool resscalenotsupported = true;
+
+		EnableWindow(GetDlgItem(m_hWnd, IDC_RESOLUTION), !resscalenotsupported);
+		EnableWindow(GetDlgItem(m_hWnd, IDC_RESOLUTION_TEXT), !resscalenotsupported);
 
 		ShowWindow(GetDlgItem(m_hWnd, IDC_PSX_LOGO11), dx11 ? SW_SHOW : SW_HIDE);
 		ShowWindow(GetDlgItem(m_hWnd, IDC_PSX_NULL), null ? SW_SHOW : SW_HIDE);
