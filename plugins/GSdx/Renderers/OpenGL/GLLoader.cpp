535b0d4aace1e055270916b8c2b98b18e8f78cdb
lightningterror <lightningterror@yahoo.com>
2019-02-15 22:56:32 +0100

    gsdx-ogl: Disable sparse depth on amd.
    * Bad driver as usual.
    
    * Driver reports a compatible sparse format for depth texture but it isn't attachable to a frame buffer.

--- plugins/GSdx/Renderers/OpenGL/GLLoader.cpp
+++ plugins/GSdx/Renderers/OpenGL/GLLoader.cpp
@@ -394,7 +394,9 @@ namespace GLLoader {
 		}
 
 		// Can fit in 128x64 but 128x128 is enough
-		found_compatible_sparse_depth = is_sparse2_compatible("GL_DEPTH32F_STENCIL8", GL_DEPTH32F_STENCIL8, 128, 128);
+		// Disable sparse depth for AMD. Bad driver strikes again.
+		// driver reports a compatible sparse format for depth texture but it isn't attachable to a frame buffer.
+		found_compatible_sparse_depth = !vendor_id_amd && is_sparse2_compatible("GL_DEPTH32F_STENCIL8", GL_DEPTH32F_STENCIL8, 128, 128);
 
 		fprintf_once(stdout, "INFO sparse color texture is %s\n", found_compatible_GL_ARB_sparse_texture2 ? "available" : "NOT SUPPORTED");
 		fprintf_once(stdout, "INFO sparse depth texture is %s\n", found_compatible_sparse_depth ? "available" : "NOT SUPPORTED");
