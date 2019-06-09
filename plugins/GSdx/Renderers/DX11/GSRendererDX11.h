60cf62fea13c20548dbb7ddd5572077272fdf4c0
hibye8313 <49872607+hibye8313@users.noreply.github.com>
2019-06-09 12:38:11 -0400

    GSdx-d3d11: Partial port of EmulateBlending() from OGL renderer to DX11 renderer and ps_blend() from OGL shader to DX11 shader (only accumulation blend).

--- plugins/GSdx/Renderers/DX11/GSRendererDX11.h
+++ plugins/GSdx/Renderers/DX11/GSRendererDX11.h
@@ -43,6 +43,7 @@ private:
 	inline void SetupIA(const float& sx, const float& sy);
 	inline void EmulateAtst(const int pass, const GSTextureCache::Source* tex);
 	inline void EmulateZbuffer();
+	inline void EmulateBlending();
 	inline void EmulateTextureShuffleAndFbmask();
 	inline void EmulateChannelShuffle(GSTexture** rt, const GSTextureCache::Source* tex);
 	inline void EmulateTextureSampler(const GSTextureCache::Source* tex);
