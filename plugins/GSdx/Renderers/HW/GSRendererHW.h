370e4e627232f27a48cd48db995ba82ef5bed249
Alessandro Vetere <iminelink@gmail.com>
2019-06-24 15:22:52 +0200

    GSRendererHW: New SW sprite renderer, used in Jak
    Developed a new SW sprite render function, which is used to fix
    Jak and Daxter colors in Jak games and to remove the old Jak games hack
    which yielded wrong colors and clashed with TC search texture in RT function.
    
    - Remove OO_JakGames hack for palette readback from framebuffer (slow,
    clashes with texture in RT TC function, clashes with Target readback logic)
    - Developed new method SwSpriteRender with CPU rendering logic
    - Add OI_JakGames method using SwSpriteRender for CPU palette rendering (fast,
    does not clash with texture in RT TC function, no readback required)

--- plugins/GSdx/Renderers/HW/GSRendererHW.h
+++ plugins/GSdx/Renderers/HW/GSRendererHW.h
@@ -62,10 +62,10 @@ private:
 	bool OI_SuperManReturns(GSTexture* rt, GSTexture* ds, GSTextureCache::Source* t);
 	bool OI_ArTonelico2(GSTexture* rt, GSTexture* ds, GSTextureCache::Source* t);
 	bool OI_ItadakiStreet(GSTexture* rt, GSTexture* ds, GSTextureCache::Source* t);
+	bool OI_JakGames(GSTexture* rt, GSTexture* ds, GSTextureCache::Source* t);
 
 	void OO_DBZBT2();
 	void OO_MajokkoALaMode2();
-	void OO_JakGames();
 
 	bool CU_DBZBT2();
 	bool CU_MajokkoALaMode2();
@@ -136,6 +136,7 @@ private:
 	uint16 Interpolate_UV(float alpha, int t0, int t1);
 	float alpha0(int L, int X0, int X1);
 	float alpha1(int L, int X0, int X1);
+	void SwSpriteRender();
 
 	template <bool linear> void RoundSpriteOffset();
 
