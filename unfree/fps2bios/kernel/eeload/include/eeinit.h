dbfbc333f4a664f425719e8063ea97c7bcab7047
lightningterror <lightningterror@yahoo.com>
2019-11-16 22:06:46 +0100

    fps2bios: Remove fps2bios from master branch.
    It's an abandoned project pretty much, if someone needs to go back and
    take a look at it we have 1.4 branches and older.

--- unfree/fps2bios/kernel/eeload/include/eeinit.h
+++
@@ -1,8 +0,0 @@
-#ifndef __EEINIT_H__
-#define __EEINIT_H__
-
-#include <tamtypes.h>
-
-void TlbInit();
-
-#endif /* __EEINIT_H__ */
--- unfree/fps2bios/kernel/eeload/include/eeload.h
+++
@@ -1,4 +0,0 @@
-#ifndef __EELOAD_H__
-#define __EELOAD_H__
-
-#endif /* __EELOAD_H__ */
