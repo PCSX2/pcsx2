ffe7f9d46949e92507e0c543add9008854f28fe2
Jonathan Li <jonathan.li@hotmail.co.uk>
2019-11-25 15:50:37 +0000

    gui: Fix about dialog logo DPI scaling
    Also dynamically allocate the wxStaticBitmap control since its parent
    window will delete it, which can cause issues.

--- pcsx2/gui/Dialogs/ModalPopups.h
+++ pcsx2/gui/Dialogs/ModalPopups.h
@@ -60,9 +60,6 @@ namespace Dialogs
 {
 	class AboutBoxDialog: public wxDialogWithHelpers
 	{
-	protected:
-		wxStaticBitmap m_bitmap_logo;
-
 	public:
 		AboutBoxDialog( wxWindow* parent=NULL );
 		virtual ~AboutBoxDialog() = default;
