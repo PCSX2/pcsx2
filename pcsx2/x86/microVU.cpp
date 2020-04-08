763611ddb6b48b3bbc831e258a5e8ae3dc40954d
chaoticgd <chaoticgd@protonmail.com>
2020-04-08 19:29:08 +0100

    VU: Log the index of VU microprograms upon execution

--- pcsx2/x86/microVU.cpp
+++ pcsx2/x86/microVU.cpp
@@ -350,7 +350,9 @@ void recMicroVU1::Reset() {
 
 void recMicroVU0::Execute(u32 cycles) {
 	pxAssert(m_Reserved); // please allocate me first! :|
-
+	
+	VUM_LOG("VU0 Microprogram Index: %d", microVU0.prog.cur->idx);
+	
 	if(!(VU0.VI[REG_VPU_STAT].UL & 1)) return;
 
 	// Sometimes games spin on vu0, so be careful with this value
@@ -366,7 +368,9 @@ void recMicroVU0::Execute(u32 cycles) {
 }
 void recMicroVU1::Execute(u32 cycles) {
 	pxAssert(m_Reserved); // please allocate me first! :|
-
+	
+	VUM_LOG("VU1 Microprogram Index: %d", microVU1.prog.cur->idx);
+	
 	if (!THREAD_VU1) {
 		if(!(VU0.VI[REG_VPU_STAT].UL & 0x100)) return;
 	}
