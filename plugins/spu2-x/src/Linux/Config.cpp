17591fb99c93a54234ea1049588d82225d10703e
Tellow Krinkle <tellowkrinkle@gmail.com>
2020-05-05 01:56:10 -0500

    Fix spu2-x configuration crashes

--- plugins/spu2-x/src/Linux/Config.cpp
+++ plugins/spu2-x/src/Linux/Config.cpp
@@ -23,6 +23,9 @@
 #include <SDL.h>
 #include <SDL_audio.h>
 #endif
+#ifdef __APPLE__
+#include <dispatch/dispatch.h>
+#endif
 
 #ifdef PCSX2_DEVBUILD
 static const int LATENCY_MAX = 3000;
@@ -270,11 +273,6 @@ void DisplayDialog()
 {
     int return_value;
 
-#ifdef __APPLE__
-    // Rest of macOS UI doesn't use GTK so we need to init it now
-    gtk_init(nullptr, nullptr);
-#endif
-
     GtkWidget *dialog;
     GtkWidget *main_box;
 
@@ -510,10 +508,23 @@ void DisplayDialog()
 
 void configure()
 {
+#ifdef __APPLE__
+    // Rest of macOS UI doesn't use GTK so we need to init it now
+    gtk_init(nullptr, nullptr);
+    // GTK expects us to be using its event loop, rather than Cocoa's
+    // If we call its stuff right now, it'll attempt to drain a static autorelease pool that was already drained by Cocoa (see https://github.com/GNOME/gtk/blob/8c1072fad1cb6a2e292fce2441b4a571f173ce0f/gdk/quartz/gdkeventloop-quartz.c#L640-L646)
+    // We can convince it that touching that pool would be unsafe by running all GTK calls within a CFRunLoop
+    // (Blocks submitted to the main queue by dispatch_async are run by its CFRunLoop)
+    dispatch_async(dispatch_get_main_queue(), ^{
+#endif
     initIni();
     ReadSettings();
     DisplayDialog();
     WriteSettings();
     delete spuConfig;
     spuConfig = NULL;
+#ifdef __APPLE__
+    // End of `dispatch_async(...` above
+    });
+#endif
 }
