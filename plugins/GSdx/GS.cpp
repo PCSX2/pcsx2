9f41f33e861c27f07c2650ba71396b5c3e6d4b8a
Tyler Wilding <xtvaser@gmail.com>
2020-07-28 20:16:45 -0400

    GSdx: Save PNG compressed screenshots as `.png` files

--- plugins/GSdx/GS.cpp
+++ plugins/GSdx/GS.cpp
@@ -676,9 +676,13 @@ EXPORT_C_(uint32) GSmakeSnapshot(char* path)
 	{
 		std::string s{path};
 
-		if(!s.empty() && s[s.length() - 1] != DIRECTORY_SEPARATOR)
+		if (!s.empty())
 		{
-			s = s + DIRECTORY_SEPARATOR;
+			// Allows for providing a complete path
+			if (s.substr(s.size() - 4, 4) == ".png")
+				return s_gs->MakeSnapshot(s);
+			else if (s[s.length() - 1] != DIRECTORY_SEPARATOR)
+				s = s + DIRECTORY_SEPARATOR;
 		}
 
 		return s_gs->MakeSnapshot(s + "gsdx");
