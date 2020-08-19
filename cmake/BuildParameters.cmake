91326e1d93b9e1d06fe6e91346d2138cd3eaa92e
TellowKrinkle <tellowkrinkle@gmail.com>
2020-08-19 04:16:49 -0500

    cmake: Remove x86-64 warnings
    It's supported now

--- cmake/BuildParameters.cmake
+++ cmake/BuildParameters.cmake
@@ -175,25 +175,9 @@ endif()
 include(TargetArch)
 target_architecture(PCSX2_TARGET_ARCHITECTURES)
 if(${PCSX2_TARGET_ARCHITECTURES} MATCHES "x86_64" OR ${PCSX2_TARGET_ARCHITECTURES} MATCHES "i386")
-	if(${PCSX2_TARGET_ARCHITECTURES} MATCHES "x86_64" AND (CMAKE_BUILD_TYPE MATCHES "Release" OR PACKAGE_MODE))
-		message(FATAL_ERROR "
-        The code for ${PCSX2_TARGET_ARCHITECTURES} support is not ready yet.
-        For now compile with -DCMAKE_TOOLCHAIN_FILE=cmake/linux-compiler-i386-multilib.cmake
-        or with
-        --cross-multilib passed to build.sh")
-	endif()
 	message(STATUS "Compiling a ${PCSX2_TARGET_ARCHITECTURES} build on a ${CMAKE_HOST_SYSTEM_PROCESSOR} host.")
 else()
 	message(FATAL_ERROR "Unsupported architecture: ${PCSX2_TARGET_ARCHITECTURES}")
-endif()
-
-# Print a clear message that most architectures are not supported
-if(NOT (${PCSX2_TARGET_ARCHITECTURES} MATCHES "i386"))
-    message(WARNING "
-    PCSX2 does not support the ${PCSX2_TARGET_ARCHITECTURES} architecture and has no plans yet to support it.
-    It would need a complete rewrite of the core emulator and a lot of time.
-
-    You can still run a i386 binary if you install all the i386 libraries (runtime and dev).")
 endif()
 
 if(${PCSX2_TARGET_ARCHITECTURES} MATCHES "i386")
