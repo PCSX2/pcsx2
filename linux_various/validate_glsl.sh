13ca9b8a5c17baca620242c197cbd8b61c165171
lightningterror <18107717+lightningterror@users.noreply.github.com>
2020-08-21 13:13:29 +0200

    linux various: Remove zzogl code from validate_glsl.sh

--- linux_various/validate_glsl.sh
+++ linux_various/validate_glsl.sh
@@ -38,7 +38,6 @@ Help:
     --input <file>         : input glsl file (mandatory)
     --macro <name> <value> : set a macro. Can be repeated
     --entry <name>         : set an entry point. Note: print the ASM output of the program
-    --test_ZZ              : test of zzogl glsl file
     --test_dx              : test of gsdx glsl file
     --nofrag               : disable fragment processing
     --novert               : disable vertex processing
@@ -52,7 +51,6 @@ case $1 in
     --input|--i) INPUT=$2; shift 2;;
     --macro|--m) MACRO="${MACRO}#define $2 $3\n"; shift 3;;
     --entry|--e) ENTRY="-entry $2";shift 2;;
-    --test_ZZ )  TEST_ZZOGL=1; shift 1;;
     --test_dx )  TEST_GSDX=1; shift 1;;
     --nofrag)    NOFRAG=1; shift 1;;
     --novert)    NOVERT=1; shift 1;;
@@ -116,13 +114,7 @@ fragment_test()
 # Main
 ######################################################
 
-if [ "$TEST_ZZOGL" = '1' ] ; then
-    ./validate_glsl.sh --input $INPUT --novert --m TEST_AEM 1
-    ./validate_glsl.sh --input $INPUT --novert --m REGION_REPEAT 1
-    ./validate_glsl.sh --input $INPUT --novert --m EXACT_COLOR 1
-    ./validate_glsl.sh --input $INPUT --m WRITE_DEPTH 1
-    ./validate_glsl.sh --input $INPUT
-elif [ "$TEST_GSDX" = '1' ] ; then
+if [ "$TEST_GSDX" = '1' ] ; then
     echo "not yet implemented"
     # A very big shader example (124 instructions!)
     ./validate_glsl.sh --input $INPUT --novert --entry ps_main --macro PS_TCC 0 --macro PS_TFX 0 --macro PS_IIP 1 --macro PS_ATST 4 --macro PS_FST 1 --macro PS_BLEND 4 --macro PS_COLCLIP 3 --macro PS_SHUFFLE 1 --macro PS_LTF 1 --macro PS_FMT 6 --macro PS_AEM 0 --macro PS_FBMASK 1 --macro PS_FOG 1 --macro PS_WMS 2 --macro PS_WMT 3
