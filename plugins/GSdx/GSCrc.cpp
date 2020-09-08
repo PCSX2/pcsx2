27598fecce8d22dec04cf4a8d68b14735d581997
Mrlinkwii <Mrlinkwii@users.noreply.github.com>
2020-09-08 21:18:27 +0100

    GSdx: Console printf CRC fix for starter character 0. (#3678)

--- plugins/GSdx/GSCrc.cpp
+++ plugins/GSdx/GSCrc.cpp
@@ -575,19 +575,18 @@ bool IsCrcExcluded(std::string exclusionList, uint32 crc)
 
 CRC::Game CRC::Lookup(uint32 crc)
 {
-	printf("GSdx Lookup CRC:%X\n", crc);
+	printf("GSdx Lookup CRC:%08X\n", crc);
 	if(m_map.empty())
 	{
 		std::string exclusions = theApp.GetConfigS("CrcHacksExclusions");
 		if (exclusions.length() != 0)
-			printf( "GSdx: CrcHacksExclusions: %s\n", exclusions.c_str() );
-
+ 			printf( "GSdx: CrcHacksExclusions: %s\n", exclusions.c_str() ); 
 		int crcDups = 0;
 		for(size_t i = 0; i < countof(m_games); i++)
 		{
 			if( !IsCrcExcluded( exclusions, m_games[i].crc ) ){
 				if(m_map[m_games[i].crc]){
-					printf("[FIXME] GSdx: Duplicate CRC: 0x%x: (game-id/region-id) %d/%d overrides %d/%d\n"
+					printf("[FIXME] GSdx: Duplicate CRC: 0x%08X: (game-id/region-id) %d/%d overrides %d/%d\n"
 						, m_games[i].crc, m_games[i].title, m_games[i].region, m_map[m_games[i].crc]->title, m_map[m_games[i].crc]->region);
 					crcDups++;
 				}
