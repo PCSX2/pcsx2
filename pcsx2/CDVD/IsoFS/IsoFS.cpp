4c88c00eb4e5b4623a0e523f0cf9ff20fd7945ef
Tyler Wilding <xtvaser@gmail.com>
2020-09-18 16:12:13 -0400

    format: clang-format'ed 'pcsx2/CDVD/IsoFS/IsoFS.cpp'

--- pcsx2/CDVD/IsoFS/IsoFS.cpp
+++ pcsx2/CDVD/IsoFS/IsoFS.cpp
@@ -30,13 +30,17 @@
 
 wxString IsoDirectory::FStype_ToString() const
 {
-	switch( m_fstype )
+	switch (m_fstype)
 	{
-		case FStype_ISO9660:	return L"ISO9660";		break;
-		case FStype_Joliet:		return L"Joliet";		break;
+		case FStype_ISO9660:
+			return L"ISO9660";
+			break;
+		case FStype_Joliet:
+			return L"Joliet";
+			break;
 	}
 
-	return wxsFormat( L"Unrecognized Code (0x%x)", m_fstype );
+	return wxsFormat(L"Unrecognized Code (0x%x)", m_fstype);
 }
 
 // Used to load the Root directory from an image
@@ -50,56 +54,56 @@ IsoDirectory::IsoDirectory(SectorSource& r)
 
 	m_fstype = FStype_ISO9660;
 
-	while( !done )
+	while (!done)
 	{
 		u8 sector[2048];
-		internalReader.readSector(sector,i);
-		if( memcmp( &sector[1], "CD001", 5 ) == 0 )
+		internalReader.readSector(sector, i);
+		if (memcmp(&sector[1], "CD001", 5) == 0)
 		{
-		    switch (sector[0])
-		    {
-		        case 0:
-                    DevCon.WriteLn( Color_Green, "(IsoFS) Block 0x%x: Boot partition info.", i );
-                    break;
+			switch (sector[0])
+			{
+				case 0:
+					DevCon.WriteLn(Color_Green, "(IsoFS) Block 0x%x: Boot partition info.", i);
+					break;
 
-                case 1:
-                    DevCon.WriteLn( "(IsoFS) Block 0x%x: Primary partition info.", i );
-                    rootDirEntry.Load( sector+156, 38 );
-                    isValid = true;
-                    break;
+				case 1:
+					DevCon.WriteLn("(IsoFS) Block 0x%x: Primary partition info.", i);
+					rootDirEntry.Load(sector + 156, 38);
+					isValid = true;
+					break;
 
-                case 2:
-                    // Probably means Joliet (long filenames support), which PCSX2 doesn't care about.
-                    DevCon.WriteLn( Color_Green, "(IsoFS) Block 0x%x: Extended partition info.", i );
-                    m_fstype = FStype_Joliet;
-                    break;
+				case 2:
+					// Probably means Joliet (long filenames support), which PCSX2 doesn't care about.
+					DevCon.WriteLn(Color_Green, "(IsoFS) Block 0x%x: Extended partition info.", i);
+					m_fstype = FStype_Joliet;
+					break;
 
 				case 0xff:
 					// Null terminator.  End of partition information.
 					done = true;
-				break;
+					break;
 
 				default:
-					Console.Error( "(IsoFS) Unknown partition type ID=%d, encountered at block 0x%x", sector[0], i );
-				break;
+					Console.Error("(IsoFS) Unknown partition type ID=%d, encountered at block 0x%x", sector[0], i);
+					break;
 			}
 		}
 		else
 		{
 			sector[9] = 0;
-			Console.Error( "(IsoFS) Invalid partition descriptor encountered at block 0x%x: '%s'", i, &sector[1] );
-			break;		// if no valid root partition was found, an exception will be thrown below.
+			Console.Error("(IsoFS) Invalid partition descriptor encountered at block 0x%x: '%s'", i, &sector[1]);
+			break; // if no valid root partition was found, an exception will be thrown below.
 		}
 
 		++i;
 	}
 
-	if( !isValid )
-		throw Exception::FileNotFound(L"IsoFileSystem")	// FIXME: Should report the name of the ISO here...
+	if (!isValid)
+		throw Exception::FileNotFound(L"IsoFileSystem") // FIXME: Should report the name of the ISO here...
 			.SetDiagMsg(L"IsoFS could not find the root directory on the ISO image.");
 
-	DevCon.WriteLn( L"(IsoFS) Filesystem is " + FStype_ToString() );
-	Init( rootDirEntry );
+	DevCon.WriteLn(L"(IsoFS) Filesystem is " + FStype_ToString());
+	Init(rootDirEntry);
 }
 
 // Used to load a specific directory from a file descriptor
@@ -113,7 +117,7 @@ IsoDirectory::IsoDirectory(SectorSource& r, IsoFileDescriptor directoryEntry)
 void IsoDirectory::Init(const IsoFileDescriptor& directoryEntry)
 {
 	// parse directory sector
-	IsoFile dataStream (internalReader, directoryEntry);
+	IsoFile dataStream(internalReader, directoryEntry);
 
 	files.clear();
 
@@ -121,18 +125,18 @@ void IsoDirectory::Init(const IsoFileDescriptor& directoryEntry)
 
 	u8 b[257];
 
-	while(remainingSize>=4) // hm hack :P
+	while (remainingSize >= 4) // hm hack :P
 	{
 		b[0] = dataStream.read<u8>();
 
-		if(b[0]==0)
+		if (b[0] == 0)
 		{
 			break; // or continue?
 		}
 
 		remainingSize -= b[0];
 
-		dataStream.read(b+1, b[0]-1);
+		dataStream.read(b + 1, b[0] - 1);
 
 		files.push_back(IsoFileDescriptor(b, b[0]));
 	}
@@ -147,9 +151,10 @@ const IsoFileDescriptor& IsoDirectory::GetEntry(int index) const
 
 int IsoDirectory::GetIndexOf(const wxString& fileName) const
 {
-	for(unsigned int i=0;i<files.size();i++)
+	for (unsigned int i = 0; i < files.size(); i++)
 	{
-		if(files[i].name == fileName) return i;
+		if (files[i].name == fileName)
+			return i;
 	}
 
 	throw Exception::FileNotFound(fileName);
@@ -162,11 +167,11 @@ const IsoFileDescriptor& IsoDirectory::GetEntry(const wxString& fileName) const
 
 IsoFileDescriptor IsoDirectory::FindFile(const wxString& filePath) const
 {
-	pxAssert( !filePath.IsEmpty() );
+	pxAssert(!filePath.IsEmpty());
 
 	// wxWidgets DOS-style parser should work fine for ISO 9660 path names.  Only practical difference
 	// is case sensitivity, and that won't matter for path splitting.
-	wxFileName parts( filePath, wxPATH_DOS );
+	wxFileName parts(filePath, wxPATH_DOS);
 	IsoFileDescriptor info;
 	const IsoDirectory* dir = this;
 	std::unique_ptr<IsoDirectory> deleteme;
@@ -174,16 +179,17 @@ IsoFileDescriptor IsoDirectory::FindFile(const wxString& filePath) const
 	// walk through path ("." and ".." entries are in the directories themselves, so even if the
 	// path included . and/or .., it still works)
 
-	for(uint i=0; i<parts.GetDirCount(); ++i)
+	for (uint i = 0; i < parts.GetDirCount(); ++i)
 	{
 		info = dir->GetEntry(parts.GetDirs()[i]);
-		if(info.IsFile()) throw Exception::FileNotFound( filePath );
+		if (info.IsFile())
+			throw Exception::FileNotFound(filePath);
 
 		deleteme.reset(new IsoDirectory(internalReader, info));
 		dir = deleteme.get();
 	}
 
-	if( !parts.GetFullName().IsEmpty() )
+	if (!parts.GetFullName().IsEmpty())
 		info = dir->GetEntry(parts.GetFullName());
 
 	return info;
@@ -191,70 +197,77 @@ IsoFileDescriptor IsoDirectory::FindFile(const wxString& filePath) const
 
 bool IsoDirectory::IsFile(const wxString& filePath) const
 {
-	if( filePath.IsEmpty() ) return false;
-	return (FindFile(filePath).flags&2) != 2;
+	if (filePath.IsEmpty())
+		return false;
+	return (FindFile(filePath).flags & 2) != 2;
 }
 
 bool IsoDirectory::IsDir(const wxString& filePath) const
 {
-	if( filePath.IsEmpty() ) return false;
-	return (FindFile(filePath).flags&2) == 2;
+	if (filePath.IsEmpty())
+		return false;
+	return (FindFile(filePath).flags & 2) == 2;
 }
 
-u32 IsoDirectory::GetFileSize( const wxString& filePath ) const
+u32 IsoDirectory::GetFileSize(const wxString& filePath) const
 {
-	return FindFile( filePath ).size;
+	return FindFile(filePath).size;
 }
 
 IsoFileDescriptor::IsoFileDescriptor()
 {
-	lba   = 0;
-	size  = 0;
+	lba = 0;
+	size = 0;
 	flags = 0;
 	memset(&date, 0, sizeof(date));
 }
 
 IsoFileDescriptor::IsoFileDescriptor(const u8* data, int length)
 {
-	Load( data, length );
+	Load(data, length);
 }
 
-void IsoFileDescriptor::Load( const u8* data, int length )
+void IsoFileDescriptor::Load(const u8* data, int length)
 {
-	lba		= (u32&)data[2];
-	size	= (u32&)data[10];
+	lba = (u32&)data[2];
+	size = (u32&)data[10];
 
-	date.year      = data[18] + 1900;
-	date.month     = data[19];
-	date.day       = data[20];
-	date.hour      = data[21];
-	date.minute    = data[22];
-	date.second    = data[23];
+	date.year = data[18] + 1900;
+	date.month = data[19];
+	date.day = data[20];
+	date.hour = data[21];
+	date.minute = data[22];
+	date.second = data[23];
 	date.gmtOffset = data[24];
 
 	flags = data[25];
 
 	int fileNameLength = data[32];
 
-	if(fileNameLength==1)
+	if (fileNameLength == 1)
 	{
 		u8 c = data[33];
 
-		switch(c)
+		switch (c)
 		{
-			case 0:	name = L"."; break;
-			case 1: name = L".."; break;
-			default: name = (wxChar)c;
+			case 0:
+				name = L".";
+				break;
+			case 1:
+				name = L"..";
+				break;
+			default:
+				name = (wxChar)c;
 		}
 	}
 	else
 	{
 		// copy string and up-convert from ascii to wxChar
 
-		const u8* fnsrc = data+33;
-		const u8* fnend = fnsrc+fileNameLength;
+		const u8* fnsrc = data + 33;
+		const u8* fnend = fnsrc + fileNameLength;
 
-		while( fnsrc != fnend )
+		while (fnsrc != fnend)
 		{
 			name += (wxChar)*fnsrc;
 			++fnsrc;
