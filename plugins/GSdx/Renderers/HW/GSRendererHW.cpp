1ad02e0431cb2bdefea1b7fe3ca9f16af3af0961
lightningterror <18107717+lightningterror@users.noreply.github.com>
2020-10-13 21:36:47 +0200

    gsdx-hw: Invert Automatic mipmapping code.
    Always enable Basic mipmapping except for the following games that have
    issues with it.
    
    This way we rely on less crc and more games get to use it which need it
    with adding less crc.

--- plugins/GSdx/Renderers/HW/GSRendererHW.cpp
+++ plugins/GSdx/Renderers/HW/GSRendererHW.cpp
@@ -190,53 +190,26 @@ void GSRendererHW::SetGameCRC(uint32 crc, int options)
 
 	m_hacks.SetGameCRC(m_game);
 
-	// Code for Automatic Mipmapping. Relies on game CRCs.
+	// Code for Automatic Mipmapping.
 	if (theApp.GetConfigT<HWMipmapLevel>("mipmap_hw") == HWMipmapLevel::Automatic)
 	{
+		// FIXME: Investigate the following games and find out why mipmapping doesn't work on them.
+		// By default Basic level is enabled except for the following games below.
 		switch (CRC::Lookup(crc).title)
 		{
-			case CRC::AceCombatZero:
-			case CRC::AceCombat4:
-			case CRC::AceCombat5:
-			case CRC::ApeEscape2:
-			case CRC::Barnyard:
-			case CRC::BrianLaraInternationalCricket:
-			case CRC::DarkCloud:
-			case CRC::DestroyAllHumans:
-			case CRC::DestroyAllHumans2:
-			case CRC::FIFA03:
-			case CRC::FIFA04:
-			case CRC::FIFA05:
-			case CRC::HarryPotterATCOS:
-			case CRC::HarryPotterATGOF:
-			case CRC::HarryPotterATHBP:
-			case CRC::HarryPotterATPOA:
-			case CRC::HarryPotterOOTP:
-			case CRC::Jak1:
-			case CRC::Jak3:
-			case CRC::LegacyOfKainDefiance:
-			case CRC::NicktoonsUnite:
-			case CRC::Persona3:
-			case CRC::ProjectSnowblind:
-			case CRC::Quake3Revolution:
-			case CRC::RatchetAndClank:
-			case CRC::RatchetAndClank2:
-			case CRC::RatchetAndClank3:
-			case CRC::RatchetAndClank4:
-			case CRC::RatchetAndClank5:
-			case CRC::RickyPontingInternationalCricket:
-			case CRC::Shox:
-			case CRC::SoulReaver2:
-			case CRC::TheIncredibleHulkUD:
-			case CRC::TombRaiderAnniversary:
-			case CRC::TribesAerialAssault:
-			case CRC::Whiplash:
-				m_mipmap = static_cast<int>(HWMipmapLevel::Basic);
-				break;
-			default:
+			case CRC::GT3:
+			case CRC::GT4:
+			case CRC::GTConcept:
+			case CRC::Jak2:
+			case CRC::JakX:
+			case CRC::TouristTrophy:
 				m_mipmap = static_cast<int>(HWMipmapLevel::Off);
 				break;
+			default:
+				m_mipmap = static_cast<int>(HWMipmapLevel::Basic);
+				break;
 		}
+		// END OF FIXME
 	}
 }
 
