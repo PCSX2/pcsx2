db5bdb9f5556f23dd9b0b7945f50a0f15b7548cd
Gauvain 'GovanifY' Roussel-Tarbouriech <gauvain@govanify.com>
2020-11-03 12:53:29 +0100

    USB: savestate bump + initial sstate implem for usb

--- pcsx2/USB/USB.h
+++ pcsx2/USB/USB.h
@@ -49,6 +49,9 @@ void USBwrite8(u32 addr, u8 value);
 void USBwrite16(u32 addr, u16 value);
 void USBwrite32(u32 addr, u32 value);
 
+void USBDoFreezeOut(void* dest);
+void USBDoFreezeIn(pxInputStream& infp);
+
 
 void USBsetRAM(void* mem);
 
