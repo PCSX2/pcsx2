449b62c47911445bed0fcfd2feae5d41fc715442
lightningterror <18107717+lightningterror@users.noreply.github.com>
2020-11-04 21:08:39 +0100

    x86emitter: Comment out unused variables.
    cpudetect.cpp -> x86_64_8BITBRANDID and x86_64_12BITBRANDID.

--- common/src/x86emitter/cpudetect.cpp
+++ common/src/x86emitter/cpudetect.cpp
@@ -193,12 +193,6 @@ void x86capabilities::Identify()
     s32 regs[4];
     u32 cmds;
 
-//AMD 64 STUFF
-#ifdef __M_X86_64
-    u32 x86_64_8BITBRANDID;
-    u32 x86_64_12BITBRANDID;
-#endif
-
     memzero(VendorName);
     cpuid(regs, 0);
 
@@ -227,7 +221,7 @@ void x86capabilities::Identify()
         FamilyID = (regs[0] >> 8) & 0xf;
         TypeID = (regs[0] >> 12) & 0x3;
 #ifdef __M_X86_64
-        x86_64_8BITBRANDID = regs[1] & 0xff;
+        //u32 x86_64_8BITBRANDID = regs[1] & 0xff;
 #endif
         Flags = regs[3];
         Flags2 = regs[2];
@@ -246,7 +240,7 @@ void x86capabilities::Identify()
         cpuid(regs, 0x80000001);
 
 #ifdef __M_X86_64
-        x86_64_12BITBRANDID = regs[1] & 0xfff;
+        //u32 x86_64_12BITBRANDID = regs[1] & 0xfff;
 #endif
         EFlags2 = regs[2];
         EFlags = regs[3];
