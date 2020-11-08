13bb815c0e61d6844c09f5385aab9f81e582dc59
lightningterror <18107717+lightningterror@users.noreply.github.com>
2020-11-08 04:36:15 +0100

    spu2: Rename SPU2 Config -> Audio Settings.
    We can have a better user friendly name as the plugin is merged in to
    core.

--- pcsx2/SPU2/wx/wxConfig.cpp
+++ pcsx2/SPU2/wx/wxConfig.cpp
@@ -350,7 +350,7 @@ void DebugTab::CallUpdate(wxCommandEvent& /*event*/)
 }
 
 Dialog::Dialog()
-	: wxDialog(nullptr, wxID_ANY, "SPU2 Config", wxDefaultPosition, wxDefaultSize, wxCAPTION | wxCLOSE_BOX)
+	: wxDialog(nullptr, wxID_ANY, "Audio Settings", wxDefaultPosition, wxDefaultSize, wxCAPTION | wxCLOSE_BOX)
 {
 	m_top_box = new wxBoxSizer(wxVERTICAL);
 	auto* module_box = new wxBoxSizer(wxVERTICAL);
