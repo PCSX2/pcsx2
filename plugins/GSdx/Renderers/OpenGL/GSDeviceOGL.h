51de65b19fe18c4938e377923d73e921f00f2634
lightningterror <18107717+lightningterror@users.noreply.github.com>
2020-11-17 17:32:28 +0100

    gsdx-ogl: Check MinMax for PSConstantBuffer.

--- plugins/GSdx/Renderers/OpenGL/GSDeviceOGL.h
+++ plugins/GSdx/Renderers/OpenGL/GSDeviceOGL.h
@@ -226,8 +226,7 @@ public:
 			GSVector4i* b = (GSVector4i*)cb;
 
 			// if WH matches both HalfTexel and TC_OH_TS do too
-			// MinMax depends on WH and MskFix so no need to check it too
-			if(!((a[0] == b[0]) & (a[1] == b[1]) & (a[2] == b[2]) & (a[3] == b[3]) & (a[4] == b[4])
+			if (!((a[0] == b[0]) & (a[1] == b[1]) & (a[2] == b[2]) & (a[3] == b[3]) & (a[4] == b[4]) & (a[6] == b[6])
 				& (a[8] == b[8]) & (a[9] == b[9]) & (a[10] == b[10]) & (a[11] == b[11]) & (a[12] == b[12])).alltrue())
 			{
 				// Note previous check uses SSE already, a plain copy will be faster than any memcpy
@@ -237,6 +236,7 @@ public:
 				a[3] = b[3];
 				a[4] = b[4];
 				a[5] = b[5];
+				a[6] = b[6];
 
 				a[8] = b[8];
 
