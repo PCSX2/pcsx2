1de96a4801600dea83336721ed59f2c406e686bf
Gauvain 'GovanifY' Roussel-Tarbouriech <gauvain@govanify.com>
2020-12-11 20:32:42 +0100

    PAD: windows sstates support

--- pcsx2/PAD/Windows/PAD.cpp
+++ pcsx2/PAD/Windows/PAD.cpp
@@ -39,6 +39,7 @@
 #include "svnrev.h"
 #include "DualShock3.h"
 #include <timeapi.h>
+#include "Utilities/pxStreams.h"
 
 #define WMA_FORCE_UPDATE (WM_APP + 0x537)
 #define FORCE_UPDATE_WPARAM ((WPARAM)0x74328943)
@@ -1595,4 +1596,50 @@ s32 PADsetSlot(u8 port, u8 slot)
     // First slot always allowed.
     // return pads[port][slot].enabled | !slot;
     return 1;
+}
+
+void PADDoFreezeOut(void* dest)
+{
+	freezeData fP = { 0, (s8*)dest };
+	if (PADfreeze(FREEZE_SIZE, &fP) != 0)
+		return;
+	if (!fP.size)
+		return;
+
+	Console.Indent().WriteLn("Saving PAD");
+
+	if (PADfreeze(FREEZE_SAVE, &fP) != 0)
+		throw std::runtime_error(" * PAD: Error saving state!\n");
+}
+
+
+void PADDoFreezeIn(pxInputStream& infp)
+{
+	freezeData fP = { 0, nullptr };
+	if (PADfreeze(FREEZE_SIZE, &fP) != 0)
+		fP.size = 0;
+
+	Console.Indent().WriteLn("Loading PAD");
+
+	if (!infp.IsOk() || !infp.Length())
+	{
+		// no state data to read, but PAD expects some state data?
+		// Issue a warning to console...
+		if (fP.size != 0)
+			Console.Indent().Warning("Warning: No data for PAD found. Status may be unpredictable.");
+
+		return;
+
+		// Note: Size mismatch check could also be done here on loading, but
+		// some plugins may have built-in version support for non-native formats or
+		// older versions of a different size... or could give different sizes depending
+		// on the status of the plugin when loading, so let's ignore it.
+	}
+
+	ScopedAlloc<s8> data(fP.size);
+	fP.data = data.GetPtr();
+
+	infp.Read(fP.data, fP.size);
+	if (PADfreeze(FREEZE_LOAD, &fP) != 0)
+		throw std::runtime_error(" * PAD: Error loading state!\n");
 }
