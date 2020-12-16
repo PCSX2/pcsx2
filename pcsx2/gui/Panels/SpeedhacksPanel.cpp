600c7dcd12fb52b2aead50d39a70b30e1de4aace
Kojin <kojin@protonmail.com>
2020-12-16 14:57:48 -0500

    core: move button to grid in speedhacks

--- pcsx2/gui/Panels/SpeedhacksPanel.cpp
+++ pcsx2/gui/Panels/SpeedhacksPanel.cpp
@@ -224,17 +224,18 @@ Panels::SpeedHacksPanel::SpeedHacksPanel( wxWindow* parent )
 	*miscHacksPanel	+= m_check_waitloop | StdExpand();
 	*miscHacksPanel	+= m_check_fastCDVD | StdExpand();
 
-	s_table = new wxFlexGridSizer( 2, 2, 0, 0 );
+	s_table = new wxFlexGridSizer( 3, 2, 0, 0 );
 	s_table->Add(m_eeRateSliderPanel, StdExpand());
 	s_table->Add(m_eeSkipSliderPanel, StdExpand());
 	s_table->Add(miscHacksPanel, StdExpand());
 	s_table->Add(vuHacksPanel, StdExpand());
+	s_table->Add(new wxStaticText(this, -1, ""), StdExpand());
+	s_table->Add(m_button_Defaults, StdButton());
 
 	m_sizer = new wxBoxSizer(wxVERTICAL);
 	m_sizer->Add(m_check_Enable, StdExpand());
 	m_sizer->Add(new wxStaticLine(this), StdExpand());
 	m_sizer->Add(s_table, StdExpand());
-	m_sizer->Add(m_button_Defaults, StdButton());
 
 	SetSizer(m_sizer);
 	// ------------------------------------------------------------------------
