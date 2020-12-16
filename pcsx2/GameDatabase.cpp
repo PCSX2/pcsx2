74b9dae8f502ca3aca57b815fff4287f92d1374e
Tyler Wilding <xtvaser@gmail.com>
2020-12-16 17:05:00 -0500

    GameDB: Move the documentation file out of /bin

--- pcsx2/GameDatabase.cpp
+++ pcsx2/GameDatabase.cpp
@@ -95,7 +95,7 @@ GameDatabaseSchema::GameEntry YamlGameDatabaseImpl::entryFromYaml(const std::str
 		gameEntry.name = node["name"].as<std::string>("");
 		gameEntry.region = node["region"].as<std::string>("");
 		gameEntry.compat = static_cast<GameDatabaseSchema::Compatibility>(node["compat"].as<int>(enum_cast(gameEntry.compat)));
-		// Safely grab round mode and clamp modes from the YAML, otherwise default t
+		// Safely grab round mode and clamp modes from the YAML, otherwise use defaults
 		if (YAML::Node roundModeNode = node["roundModes"])
 		{
 			gameEntry.eeRoundMode = static_cast<GameDatabaseSchema::RoundMode>(roundModeNode["eeRoundMode"].as<int>(enum_cast(gameEntry.eeRoundMode)));
