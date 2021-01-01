f41e4e52847c887b10ab77916ecfef0b523ab29d
Kojin <kojin@protonmail.com>
2021-01-01 15:25:03 -0500

    Remove ArTonelico2 Retranslation CRCs

--- plugins/GSdx/GSCrc.cpp
+++ plugins/GSdx/GSCrc.cpp
@@ -27,8 +27,6 @@ CRC::Game CRC::m_games[] =
 {
 	// Note: IDs 0x7ACF7E03, 0x7D4EA48F, 0x37C53760 - shouldn't be added as it's from the multiloaders when packing games.
 	{0x00000000, NoTitle, NoRegion, 0},
-	{0xF46142D3, ArTonelico2, NoRegion, 0},
-	{0xC38067F4, ArTonelico2, NoRegion, 0}, // project metafalica 1.0
 	{0xF95F37EE, ArTonelico2, US, 0},
 	{0x68CE6801, ArTonelico2, JP, 0},
 	{0xCE2C1DBF, ArTonelico2, EU, 0},
