d0c2ba8d205bcc052930a6b29a10e649ce04cc47
RedPanda4552 <redpanda4552@gmail.com>
2021-01-02 16:41:23 -0500

    Add comments for everything
    Probably overkill. Whoops.

--- pcsx2/DiscordSDK/DiscordSDK.cpp
+++ pcsx2/DiscordSDK/DiscordSDK.cpp
@@ -19,14 +19,69 @@
 
 #include "discord.h" // Discord's own header for the SDK
 
-#include "App.h"
-#include <ctime>
+#include "App.h" // Access to g_Conf
+#include <ctime> // time(time_t) function
+
+// Discord SDK Implementation!
+// 
+// Discord SDK is callback based. Functions such as
+// discord::ActivityManager::UpdateActivity are non-blocking and their payloads
+// will run asynchronous to the threads they were called from. Discord wasn't
+// lying when they said the SDK was not thread safe.
+// 
+// As such, these asynchronous functions do take callbacks to run after
+// completion. The discord::Core::RunCallbacks function will execute any
+// callbacks of asynchronous functions which have completed. Because obviously
+// there is no guarantee at all with timing on this, discord::Core::RunCallbacks
+// needs to be executed repeatedly in a loop. Currently we do this in
+// SysCoreThread::VsyncInThread, which works beautifully, as long as the
+// emulation threads are running and we are processing vsyncs.
+// 
+// The downfall of this is our UI thread is not constantly awake. It is an event
+// listener; a giant idle loop with a bunch of event handlers bolted on to it.
+// We certainly could plaster "CallbackDiscordSDK" all over every last event
+// handler that works in the UI thread to. Or, we can just accept that for now
+// the implementation will be unresponsive to any kind of "In the Menus"
+// scenario and just reflect BIOS and ingame status. Hopefully with a new UI and
+// removal of wx we may have some better options, time will tell.
+// 
+// Some side notes to the brave soul who finds this file in the future
+// 
+// The whole "are we in BIOS or ingame" thing is kind of interesting, it doesn't
+// seem like that information really persists. We know the current serial if a
+// game is loaded, or none if BIOS is running, that's about it. The title and
+// all is loaded from GameDB on the fly as a bunch of locals, thrown into the
+// console title bar (using an _ApplySettings function? Interesting place but
+// okay) and then that's all, no persistence for referencing later. This too
+// makes it a bit tricky to do "In the Menus" type updates on the fly because it
+// basically means every pause and resume is going to be another GameDB lookup
+// with the serial again. The GameDB in memory appears to be a map, so it should
+// be relatively cheap to perform, but it just doesn't really feel right when it
+// could be cached and a more accurate, "GameChanged" event perhaps could be a
+// proper place to do a singular GameDB lookup.
+// 
+// Then there are PS1 games (yeah yeah we don't officially support them yet blah
+// blah) which don't exist in GameDB so that lookup would fail, but extra extra
+// problem is they don't even invoke 90% of the _ApplySettings function
+// mentioned above. Until either they are added to GameDB or the title bar is
+// set in one unified location, PS1 games will simply set off the PS2 BIOS
+// condition, then never update Discord status again. The console title is
+// updated instead by cdvdReloadElfInfo in CDVD.cpp, with the serial number
+// simply being thrown in on its own.
 
 #ifdef WIN32
 
+// Honestly, if you thought of looking for this, congratulations.
+// Here's the app ID. Don't be a dick, but have fun. :P
 discord::ClientId CLIENT_ID = 739176831936233566;
+
+// Main "Core" instance of the SDK. Everything else stems from here.
 discord::Core* core{};
 
+// Execute any pending callbacks in the Discord SDK. After an asynchronous
+// function in the SDK (e.g. Core::ActivityManager::UpdateActivity) completes,
+// its callback will be enqueued but not executed. This function calls
+// Core::RunCallbacks, which will execute any enqueued callbacks. 
 void CallbackDiscordSDK()
 {
     if (core == nullptr || g_Conf->EmuOptions.DiscordSDK == false)
@@ -35,6 +90,15 @@ void CallbackDiscordSDK()
     ::core->RunCallbacks();
 }
 
+// Update the Discord activity status. The provided state and details will be
+// shown in the activity popout in the client. The call to
+// Core::ActivityManager::UpdateActivity is non-blocking, and the provided
+// callback will be ran on the next call to CallbackDiscordSDK.
+// 
+// It is worth nothing that for extra confusion, Discord decided "state" should
+// render below "details" in the popout. Nevertheless, for code consistency I
+// thought it would be best to still match the variables up with their docs, 
+// "state" first, then "details".
 void UpdateDiscordSDK(std::string state, std::string details)
 {
     if (core == nullptr || g_Conf->EmuOptions.DiscordSDK == false)
@@ -56,6 +120,9 @@ void UpdateDiscordSDK(std::string state, std::string details)
     );
 }
 
+// Initialize the Discord SDK. Magic function that kicks off the SDK in the
+// background. Unclear if discord::Core::Create is blocking or not, but doesn't
+// seem to matter regardless.
 void InitDiscordSDK() 
 {
     if (g_Conf->EmuOptions.DiscordSDK == false)
