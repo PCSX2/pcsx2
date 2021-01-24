1f4f55f0fe91c2018ec8ce4246d6d29097327ccf
refractionpcsx2 <refraction@gmail.com>
2021-01-24 17:56:14 +0000

    SPU2: Minor improvements to ADMA Timing

--- pcsx2/SPU2/spu2.cpp
+++ pcsx2/SPU2/spu2.cpp
@@ -132,7 +132,8 @@ void SPU2writeDMA4Mem(u16* pMem, u32 size) // size now in 16bit units
 void SPU2interruptDMA4()
 {
 	FileLog("[%10d] SPU2 interruptDMA4\n", Cycles);
-	Cores[0].Regs.STATX |= 0x80;
+	if(Cores[0].DmaMode)
+		Cores[0].Regs.STATX |= 0x80;
 	Cores[0].Regs.STATX &= ~0x400;
 	Cores[0].TSA = Cores[0].ActiveTSA;
 }
@@ -140,7 +141,8 @@ void SPU2interruptDMA4()
 void SPU2interruptDMA7()
 {
 	FileLog("[%10d] SPU2 interruptDMA7\n", Cycles);
-	Cores[1].Regs.STATX |= 0x80;
+	if (Cores[1].DmaMode)
+		Cores[1].Regs.STATX |= 0x80;
 	Cores[1].Regs.STATX &= ~0x400;
 	Cores[1].TSA = Cores[1].ActiveTSA;
 }
