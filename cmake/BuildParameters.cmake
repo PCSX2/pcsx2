989c5716c94897606acda9dd43a8b78a6c8890ad
TellowKrinkle <tellowkrinkle@gmail.com>
2021-02-12 22:08:57 -0600

    GS: Add class for swizzle calculations

--- cmake/BuildParameters.cmake
+++ cmake/BuildParameters.cmake
@@ -220,7 +220,9 @@ option(USE_PGO_OPTIMIZE "Enable PGO optimization (use profile)")
 
 # Note1: Builtin strcmp/memcmp was proved to be slower on Mesa than stdlib version.
 # Note2: float operation SSE is impacted by the PCSX2 SSE configuration. In particular, flush to zero denormal.
-if(NOT MSVC)
+if(MSVC)
+	add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:/Zc:externConstexpr>")
+else()
 	add_compile_options(-pipe -fvisibility=hidden -pthread -fno-builtin-strcmp -fno-builtin-memcmp -mfpmath=sse -fno-operator-names)
 endif()
 
--- pcsx2/GS/GSVector.h
+++ pcsx2/GS/GSVector.h
@@ -53,20 +53,14 @@ public:
 		struct { T v[2]; };
 	};
 
-	GSVector2T()
+	GSVector2T() = default;
+
+	constexpr GSVector2T(T x): x(x), y(x)
 	{
 	}
 
-	GSVector2T(T x)
+	constexpr GSVector2T(T x, T y): x(x), y(y)
 	{
-		this->x = x;
-		this->y = x;
-	}
-
-	GSVector2T(T x, T y)
-	{
-		this->x = x;
-		this->y = y;
 	}
 
 	bool operator==(const GSVector2T& v) const
