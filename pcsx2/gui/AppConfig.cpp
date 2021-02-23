9db778a1edabd9f2e794eb34a73f25e28c205e79
KrossX <krossx@yahoo.com>
2021-02-23 12:16:11 -0300

    Avoid presets from resetting VsyncQueueSize

--- pcsx2/gui/AppConfig.cpp
+++ pcsx2/gui/AppConfig.cpp
@@ -1065,7 +1065,8 @@ bool AppConfig::IsOkApplyPreset(int n, bool ignoreMTVU)
 	EmuOptions.GS					= default_Pcsx2Config.GS;
 	EmuOptions.GS.FrameLimitEnable	= original_GS.FrameLimitEnable;	//Frame limiter is not modified by presets
 	EmuOptions.GS.VsyncEnable		= original_GS.VsyncEnable;
-	
+	EmuOptions.GS.VsyncQueueSize	= original_GS.VsyncQueueSize;
+
 	EmuOptions.Cpu					= default_Pcsx2Config.Cpu;
 	EmuOptions.Gamefixes			= default_Pcsx2Config.Gamefixes;
 	EmuOptions.Speedhacks			= default_Pcsx2Config.Speedhacks;
