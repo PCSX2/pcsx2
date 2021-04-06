b2f8062bafc7a072d62b11909f3c5448e18c21ef
jackun <jack.un@gmail.com>
2021-04-06 16:59:35 +0300

    USB: Remove joydev support

--- pcsx2/USB/usb-pad/api_init_linux.cpp
+++ pcsx2/USB/usb-pad/api_init_linux.cpp
@@ -15,11 +15,9 @@
 
 #include "padproxy.h"
 #include "evdev/evdev.h"
-#include "joydev/joydev.h"
 
 void usb_pad::RegisterPad::Register()
 {
 	auto& inst = RegisterPad::instance();
 	inst.Add("evdev", new PadProxy<evdev::EvDevPad>());
-	inst.Add("joydev", new PadProxy<joydev::JoyDevPad>());
 }
