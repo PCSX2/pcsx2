95dd3101711a510c719096811b06a0ee6fa6aead
TellowKrinkle <tellowkrinkle@gmail.com>
2021-04-05 19:51:01 -0500

    GS: Use broadcast loads on AVX2
    Broadcast loads are free on AVX2 processors, might as well use them

--- pcsx2/GS/GSBlock.cpp
+++ pcsx2/GS/GSBlock.cpp
@@ -16,11 +16,7 @@
 #include "PrecompiledHeader.h"
 #include "GSBlock.h"
 
-#if _M_SSE >= 0x501
-CONSTINIT const GSVector8i GSBlock::m_r16mask(0, 1, 4, 5, 2, 3, 6, 7, 8, 9, 12, 13, 10, 11, 14, 15, 0, 1, 4, 5, 2, 3, 6, 7, 8, 9, 12, 13, 10, 11, 14, 15);
-#else
 CONSTINIT const GSVector4i GSBlock::m_r16mask(0, 1, 4, 5, 2, 3, 6, 7, 8, 9, 12, 13, 10, 11, 14, 15);
-#endif
 CONSTINIT const GSVector4i GSBlock::m_r8mask(0, 4, 2, 6, 8, 12, 10, 14, 1, 5, 3, 7, 9, 13, 11, 15);
 CONSTINIT const GSVector4i GSBlock::m_r4mask(0, 1, 4, 5, 8, 9, 12, 13, 2, 3, 6, 7, 10, 11, 14, 15);
 
