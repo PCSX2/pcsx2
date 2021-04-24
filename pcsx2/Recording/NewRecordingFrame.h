70af06c3153557c8fe1357f68e5d74ea4f42cb59
Tyler Wilding <xtvaser@gmail.com>
2021-04-24 00:31:12 -0400

    input-rec: Add warning when creating a save-state recording

--- pcsx2/Recording/NewRecordingFrame.h
+++ pcsx2/Recording/NewRecordingFrame.h
@@ -15,11 +15,11 @@
 
 #pragma once
 
+#ifndef DISABLE_RECORDING
+
 #include <wx/wx.h>
 #include <wx/filepicker.h>
 
-
-#ifndef DISABLE_RECORDING
 enum MenuIds_New_Recording_Frame
 {
 	MenuIds_New_Recording_Frame_File = 0,
@@ -41,9 +41,11 @@ public:
 protected:
 	void OnFileDirChange(wxFileDirPickerEvent& event);
 	void OnFileChanged(wxFileDirPickerEvent& event);
+	void OnRecordingTypeChoiceChanged(wxCommandEvent& event);
 	void EnableOkBox();
 
 private:
+	wxPanel* m_panel;
 	wxStaticText* m_fileLabel;
 	wxFilePickerCtrl* m_filePicker;
 	bool m_fileBrowsed;
@@ -53,5 +55,7 @@ private:
 	wxChoice* m_fromChoice;
 	wxButton* m_startRecording;
 	wxButton* m_cancelRecording;
+	wxString m_savestate_label;
+	wxStaticText* m_warning_label;
 };
 #endif
