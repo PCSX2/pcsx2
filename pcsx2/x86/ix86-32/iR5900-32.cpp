1097e246a4b7c038f7eb8f7f53733b8549f08aa1
Ty Lamontagne <amfobes@gmail.com>
2021-05-25 15:10:54 -0400

    Differentiate Impossible block clearing message from IOP & EE

--- pcsx2/x86/ix86-32/iR5900-32.cpp
+++ pcsx2/x86/ix86-32/iR5900-32.cpp
@@ -847,9 +847,9 @@ void recClear(u32 addr, u32 size)
 		if (pexblock->startpc >= addr && pexblock->startpc < addr + size * 4
 		 || pexblock->startpc < addr && blockend > addr) {
 			if( !IsDevBuild )
-				Console.Error( "Impossible block clearing failure" );
+				Console.Error( "[EE] Impossible block clearing failure" );
 			else
-				pxFailDev( "Impossible block clearing failure" );
+				pxFailDev( "[EE] Impossible block clearing failure" );
 		}
 	}
 
