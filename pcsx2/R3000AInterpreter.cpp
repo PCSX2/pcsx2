8ac99072202e20b17d1b17675b5651b359b9924b
Ty Lamontagne <amfobes@gmail.com>
2021-07-14 21:27:28 -0400

    IOP: Warn on branches to zero

--- pcsx2/R3000AInterpreter.cpp
+++ pcsx2/R3000AInterpreter.cpp
@@ -1,5 +1,5 @@
 /*  PCSX2 - PS2 Emulator for PCs
- *  Copyright (C) 2002-2010  PCSX2 Dev Team
+ *  Copyright (C) 2002-2021  PCSX2 Dev Team
  *
  *  PCSX2 is free software: you can redistribute it and/or modify it under the terms
  *  of the GNU Lesser General Public License as published by the Free Software Found-
@@ -250,6 +250,9 @@ static __fi void execI()
 }
 
 static void doBranch(s32 tar) {
+	if (tar == 0x0)
+		DevCon.Warning("[R3000 Interpreter] Warning: Branch to 0x0!");
+
 	branch2 = iopIsDelaySlot = true;
 	branchPC = tar;
 	execI();
