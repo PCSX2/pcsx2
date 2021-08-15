f510f212e6f09300934c647b443c0d7eea7db533
Mrlinkwii <Mrlinkwii@users.noreply.github.com>
2021-08-15 15:49:13 +0100

    Utilities : Return  distro info instead of kernel version
    Utilities: Return  distro info instead of kernel version
    Co-Authored-By: Ty <29295048+F0bes@users.noreply.github.com>

--- common/src/Utilities/Linux/LnxMisc.cpp
+++ common/src/Utilities/Linux/LnxMisc.cpp
@@ -1,5 +1,5 @@
 /*  PCSX2 - PS2 Emulator for PCs
- *  Copyright (C) 2002-2010  PCSX2 Dev Team
+ *  Copyright (C) 2002-2021  PCSX2 Dev Team
  *
  *  PCSX2 is free software: you can redistribute it and/or modify it under the terms
  *  of the GNU Lesser General Public License as published by the Free Software Found-
@@ -52,7 +52,7 @@ u64 GetCPUTicks()
 
 wxString GetOSVersionString()
 {
-    return wxGetOsDescription();
+    return wxGetLinuxDistributionInfo().Description;
 }
 
 void ScreensaverAllow(bool allow)
