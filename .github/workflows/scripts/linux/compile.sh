c93692a779674fa2a458dbeaf41164a2d7262552
TellowKrinkle <tellowkrinkle@gmail.com>
2021-08-18 22:19:26 -0500

    GHActions:Linux: Make ccache config global
    New testing step was missing the config

--- .github/workflows/scripts/linux/compile.sh
+++ .github/workflows/scripts/linux/compile.sh
@@ -6,12 +6,6 @@ if [ -n "${GITHUB_ACTIONS}" ]; then
     echo "Warning: Running this script outside of GitHub Actions isn't recommended."
 fi
 
-export CCACHE_BASEDIR=${GITHUB_WORKSPACE}
-export CCACHE_DIR=${GITHUB_WORKSPACE}/.ccache
-export CCACHE_COMPRESS="true"
-export CCACHE_COMPRESSLEVEL="6"
-export CCACHE_MAXSIZE="400M"
-
 # Prepare the Cache
 ccache -p
 ccache -z
