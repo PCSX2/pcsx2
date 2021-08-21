90a198e3f1776dc10c59bc662dbbc14867f24872
Ziemas <ziemas@ziemas.se>
2021-08-21 22:35:50 +0200

    Cmake: Update vtune paths

--- cmake/FindVtune.cmake
+++ cmake/FindVtune.cmake
@@ -6,6 +6,7 @@
 # VTUNE_LIBRARIES    path to vtune libs
 
 find_path(VTUNE_INCLUDE_DIRS NAMES jitprofiling.h PATHS
+	/opt/intel/oneapi/vtune/latest/include
 	/opt/intel/vtune_amplifier_xe_2018/include
 	/opt/intel/vtune_amplifier_xe_2017/include
 	/opt/intel/vtune_amplifier_xe_2016/include
@@ -13,12 +14,14 @@ find_path(VTUNE_INCLUDE_DIRS NAMES jitprofiling.h PATHS
 
 if(${PCSX2_TARGET_ARCHITECTURES} MATCHES "i386")
 	find_library(VTUNE_LIBRARIES NAMES libjitprofiling.a PATHS
+		/opt/intel/oneapi/vtune/latest/lib32
 		/opt/intel/vtune_amplifier_xe_2018/lib32
 		/opt/intel/vtune_amplifier_xe_2017/lib32
 		/opt/intel/vtune_amplifier_xe_2016/lib32
 	)
 else()
 	find_library(VTUNE_LIBRARIES NAMES libjitprofiling.a PATHS
+		/opt/intel/oneapi/vtune/latest/lib64
 		/opt/intel/vtune_amplifier_xe_2018/lib64
 		/opt/intel/vtune_amplifier_xe_2017/lib64
 		/opt/intel/vtune_amplifier_xe_2016/lib64
