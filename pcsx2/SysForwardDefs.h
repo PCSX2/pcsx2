bb5bfda5c6cc92649716ff39290dbda3a3a82a86
refractionpcsx2 <refraction@gmail.com>
2021-08-31 18:09:38 +0100

    App: Link in version number information to exe version information

--- pcsx2/SysForwardDefs.h
+++ pcsx2/SysForwardDefs.h
@@ -15,9 +15,27 @@
 
 #pragma once
 
-static const int PCSX2_VersionHi	= 1;
-static const int PCSX2_VersionMid	= 7;
-static const int PCSX2_VersionLo	= 0;
+#define PCSX2_VersionHi     1
+#define PCSX2_VersionMid    7
+#define PCSX2_VersionLo     0
+
+#define STRINGIZE2(s) #s
+#define STRINGIZE(s) STRINGIZE2(s)
+
+#define VER_FILE_DESCRIPTION_STR    "PCSX2 PS2 Emulator"
+#define VER_FILE_VERSION            PCSX2_VersionHi, PCSX2_VersionMid, PCSX2_VersionLo, 0
+#define VER_FILE_VERSION_STR        STRINGIZE(PCSX2_VersionHi)        \
+                                    "." STRINGIZE(PCSX2_VersionMid)    \
+                                    "." STRINGIZE(PCSX2_VersionLo) \
+                                    "." STRINGIZE(0)    \
+
+#define VER_PRODUCTNAME_STR         "PCSX2"
+#define VER_PRODUCT_VERSION         VER_FILE_VERSION
+#define VER_PRODUCT_VERSION_STR     VER_FILE_VERSION_STR
+#define VER_ORIGINAL_FILENAME_STR   VER_PRODUCTNAME_STR ".exe"
+#define VER_INTERNAL_NAME_STR       VER_ORIGINAL_FILENAME_STR
+#define VER_COPYRIGHT_STR           "Copyright (C) 2021"
+
 static const bool PCSX2_isReleaseVersion = 0;
 
 class SysCoreThread;
