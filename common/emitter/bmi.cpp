d87cc5224e7fd263539111101050e2b047a6198c
kojin <kojin@protonmail.com>
2021-09-05 09:17:22 -0400

    common: format bmi.cpp

--- common/emitter/bmi.cpp
+++ common/emitter/bmi.cpp
@@ -19,17 +19,17 @@
 namespace x86Emitter
 {
 
-const xImplBMI_RVM xMULX = {0xF2, 0x38, 0xF6};
-const xImplBMI_RVM xPDEP = {0xF2, 0x38, 0xF5};
-const xImplBMI_RVM xPEXT = {0xF3, 0x38, 0xF5};
-const xImplBMI_RVM xANDN_S = {0x00, 0x38, 0xF2};
+	const xImplBMI_RVM xMULX = {0xF2, 0x38, 0xF6};
+	const xImplBMI_RVM xPDEP = {0xF2, 0x38, 0xF5};
+	const xImplBMI_RVM xPEXT = {0xF3, 0x38, 0xF5};
+	const xImplBMI_RVM xANDN_S = {0x00, 0x38, 0xF2};
 
-void xImplBMI_RVM::operator()(const xRegisterInt &to, const xRegisterInt &from1, const xRegisterInt &from2) const
-{
-    xOpWriteC4(Prefix, MbPrefix, Opcode, to, from1, from2);
-}
-void xImplBMI_RVM::operator()(const xRegisterInt &to, const xRegisterInt &from1, const xIndirectVoid &from2) const
-{
-    xOpWriteC4(Prefix, MbPrefix, Opcode, to, from1, from2);
-}
-}
+	void xImplBMI_RVM::operator()(const xRegisterInt& to, const xRegisterInt& from1, const xRegisterInt& from2) const
+	{
+		xOpWriteC4(Prefix, MbPrefix, Opcode, to, from1, from2);
+	}
+	void xImplBMI_RVM::operator()(const xRegisterInt& to, const xRegisterInt& from1, const xIndirectVoid& from2) const
+	{
+		xOpWriteC4(Prefix, MbPrefix, Opcode, to, from1, from2);
+	}
+} // namespace x86Emitter
