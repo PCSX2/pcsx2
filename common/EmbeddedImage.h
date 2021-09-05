7d0bc7409bcb9d47c831e4c067244d63da993fef
kojin <kojin@protonmail.com>
2021-09-05 07:52:14 -0400

    common: format EmbeddedImage

--- common/EmbeddedImage.h
+++ common/EmbeddedImage.h
@@ -25,8 +25,8 @@
 class IEmbeddedImage
 {
 public:
-    virtual const wxImage &Get() = 0;
-    virtual wxImage Scale(int width, int height) = 0;
+	virtual const wxImage& Get() = 0;
+	virtual wxImage Scale(int width, int height) = 0;
 };
 
 //////////////////////////////////////////////////////////////////////////////////////////
@@ -42,79 +42,80 @@ template <typename ImageType>
 class EmbeddedImage : public IEmbeddedImage
 {
 protected:
-    wxImage m_Image;
-    const wxSize m_ResampleTo;
+	wxImage m_Image;
+	const wxSize m_ResampleTo;
 
 protected:
-    // ------------------------------------------------------------------------
-    // Internal function used to ensure the image is loaded before returning the image
-    // handle (called from all methods that return an image handle).
-    //
-    void _loadImage()
-    {
-        if (!m_Image.Ok()) {
-            wxMemoryInputStream joe(ImageType::Data, ImageType::Length);
-            m_Image.LoadFile(joe, ImageType::GetFormat());
+	// ------------------------------------------------------------------------
+	// Internal function used to ensure the image is loaded before returning the image
+	// handle (called from all methods that return an image handle).
+	//
+	void _loadImage()
+	{
+		if (!m_Image.Ok())
+		{
+			wxMemoryInputStream joe(ImageType::Data, ImageType::Length);
+			m_Image.LoadFile(joe, ImageType::GetFormat());
 
-            if (m_ResampleTo.IsFullySpecified() && (m_ResampleTo.GetWidth() != m_Image.GetWidth() || m_ResampleTo.GetHeight() != m_Image.GetHeight()))
-                m_Image.Rescale(m_ResampleTo.GetWidth(), m_ResampleTo.GetHeight(), wxIMAGE_QUALITY_HIGH);
-        }
-    }
+			if (m_ResampleTo.IsFullySpecified() && (m_ResampleTo.GetWidth() != m_Image.GetWidth() || m_ResampleTo.GetHeight() != m_Image.GetHeight()))
+				m_Image.Rescale(m_ResampleTo.GetWidth(), m_ResampleTo.GetHeight(), wxIMAGE_QUALITY_HIGH);
+		}
+	}
 
 public:
-    EmbeddedImage()
-        : m_Image()
-        , m_ResampleTo(wxDefaultSize)
-    {
-    }
+	EmbeddedImage()
+		: m_Image()
+		, m_ResampleTo(wxDefaultSize)
+	{
+	}
 
-    // ------------------------------------------------------------------------
-    // Constructor for creating an embedded image that gets resampled to the specified size when
-    // loaded.
-    //
-    // Implementation Note: This function uses wxWidgets ResamplBox method to resize the image.
-    // wxWidgets ResampleBicubic appears to be buggy and produces fairly poor results when down-
-    // sampling images (basically resembles a pixel resize).  ResampleBox produces much cleaner
-    // results.
-    //
-    EmbeddedImage(int newWidth, int newHeight)
-        : m_Image()
-        , m_ResampleTo(newWidth, newHeight)
-    {
-    }
+	// ------------------------------------------------------------------------
+	// Constructor for creating an embedded image that gets resampled to the specified size when
+	// loaded.
+	//
+	// Implementation Note: This function uses wxWidgets ResamplBox method to resize the image.
+	// wxWidgets ResampleBicubic appears to be buggy and produces fairly poor results when down-
+	// sampling images (basically resembles a pixel resize).  ResampleBox produces much cleaner
+	// results.
+	//
+	EmbeddedImage(int newWidth, int newHeight)
+		: m_Image()
+		, m_ResampleTo(newWidth, newHeight)
+	{
+	}
 
-    // ------------------------------------------------------------------------
-    // Loads and retrieves the embedded image.  The embedded image is only loaded from its em-
-    // bedded format once.  Any subsequent calls to Get(), Rescale(), or Resample() will use
-    // the pre-loaded copy.  Translation: the png/jpeg decoding overhead happens only once,
-    // and only happens when the image is actually fetched.  Simply creating an instance
-    // of an EmbeddedImage object uses no excess memory nor cpu overhead. :)
-    //
-    const wxImage &Get()
-    {
-        _loadImage();
-        return m_Image;
-    }
+	// ------------------------------------------------------------------------
+	// Loads and retrieves the embedded image.  The embedded image is only loaded from its em-
+	// bedded format once.  Any subsequent calls to Get(), Rescale(), or Resample() will use
+	// the pre-loaded copy.  Translation: the png/jpeg decoding overhead happens only once,
+	// and only happens when the image is actually fetched.  Simply creating an instance
+	// of an EmbeddedImage object uses no excess memory nor cpu overhead. :)
+	//
+	const wxImage& Get()
+	{
+		_loadImage();
+		return m_Image;
+	}
 
-    wxIcon GetIcon()
-    {
-        wxIcon retval;
-        retval.CopyFromBitmap(Get());
-        return retval;
-    }
+	wxIcon GetIcon()
+	{
+		wxIcon retval;
+		retval.CopyFromBitmap(Get());
+		return retval;
+	}
 
-    // ------------------------------------------------------------------------
-    // Performs a pixel resize of the loaded image and returns a new image handle (EmbeddedImage
-    // is left unmodified).
-    //
+	// ------------------------------------------------------------------------
+	// Performs a pixel resize of the loaded image and returns a new image handle (EmbeddedImage
+	// is left unmodified).
+	//
 
-    wxImage Scale(int width, int height)
-    {
-        _loadImage();
-        // Not strictly necessary - wxWidgets does the dimensions check anyway.
-        if (width != m_Image.GetWidth() || height != m_Image.GetHeight())
-            return m_Image.Scale(width, height, wxIMAGE_QUALITY_HIGH);
-        else
-            return m_Image;
-    }
+	wxImage Scale(int width, int height)
+	{
+		_loadImage();
+		// Not strictly necessary - wxWidgets does the dimensions check anyway.
+		if (width != m_Image.GetWidth() || height != m_Image.GetHeight())
+			return m_Image.Scale(width, height, wxIMAGE_QUALITY_HIGH);
+		else
+			return m_Image;
+	}
 };
