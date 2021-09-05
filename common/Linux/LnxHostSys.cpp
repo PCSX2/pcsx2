ae97a079575053bcfdfd51a44fcfbec4d8da857f
kojin <kojin@protonmail.com>
2021-09-05 09:12:25 -0400

    common: format Linux directory

--- common/Linux/LnxHostSys.cpp
+++ common/Linux/LnxHostSys.cpp
@@ -33,195 +33,198 @@ extern void SignalExit(int sig);
 static const uptr m_pagemask = getpagesize() - 1;
 
 // Linux implementation of SIGSEGV handler.  Bind it using sigaction().
-static void SysPageFaultSignalFilter(int signal, siginfo_t *siginfo, void *)
+static void SysPageFaultSignalFilter(int signal, siginfo_t* siginfo, void*)
 {
-    // [TODO] : Add a thread ID filter to the Linux Signal handler here.
-    // Rationale: On windows, the __try/__except model allows per-thread specific behavior
-    // for page fault handling.  On linux, there is a single signal handler for the whole
-    // process, but the handler is executed by the thread that caused the exception.
+	// [TODO] : Add a thread ID filter to the Linux Signal handler here.
+	// Rationale: On windows, the __try/__except model allows per-thread specific behavior
+	// for page fault handling.  On linux, there is a single signal handler for the whole
+	// process, but the handler is executed by the thread that caused the exception.
 
 
-    // Stdio Usage note: SIGSEGV handling is a synchronous in-thread signal.  It is done
-    // from the context of the current thread and stackframe.  So long as the thread is not
-    // the main/ui thread, use of the px assertion system should be safe.  Use of stdio should
-    // be safe even on the main thread.
-    //  (in other words, stdio limitations only really apply to process-level asynchronous
-    //   signals)
+	// Stdio Usage note: SIGSEGV handling is a synchronous in-thread signal.  It is done
+	// from the context of the current thread and stackframe.  So long as the thread is not
+	// the main/ui thread, use of the px assertion system should be safe.  Use of stdio should
+	// be safe even on the main thread.
+	//  (in other words, stdio limitations only really apply to process-level asynchronous
+	//   signals)
 
-    // Note: Use of stdio functions isn't safe here.  Avoid console logs,
-    // assertions, file logs, or just about anything else useful.
+	// Note: Use of stdio functions isn't safe here.  Avoid console logs,
+	// assertions, file logs, or just about anything else useful.
 
 
-    // Note: This signal can be accessed by the EE or MTVU thread
-    // Source_PageFault is a global variable with its own state information
-    // so for now we lock this exception code unless someone can fix this better...
-    Threading::ScopedLock lock(PageFault_Mutex);
+	// Note: This signal can be accessed by the EE or MTVU thread
+	// Source_PageFault is a global variable with its own state information
+	// so for now we lock this exception code unless someone can fix this better...
+	Threading::ScopedLock lock(PageFault_Mutex);
 
-    Source_PageFault->Dispatch(PageFaultInfo((uptr)siginfo->si_addr & ~m_pagemask));
+	Source_PageFault->Dispatch(PageFaultInfo((uptr)siginfo->si_addr & ~m_pagemask));
 
-    // resumes execution right where we left off (re-executes instruction that
-    // caused the SIGSEGV).
-    if (Source_PageFault->WasHandled())
-        return;
+	// resumes execution right where we left off (re-executes instruction that
+	// caused the SIGSEGV).
+	if (Source_PageFault->WasHandled())
+		return;
 
-    if (!wxThread::IsMain()) {
-        pxFailRel(pxsFmt("Unhandled page fault @ 0x%08x", siginfo->si_addr));
-    }
+	if (!wxThread::IsMain())
+	{
+		pxFailRel(pxsFmt("Unhandled page fault @ 0x%08x", siginfo->si_addr));
+	}
 
-    // Bad mojo!  Completely invalid address.
-    // Instigate a trap if we're in a debugger, and if not then do a SIGKILL.
+	// Bad mojo!  Completely invalid address.
+	// Instigate a trap if we're in a debugger, and if not then do a SIGKILL.
 
-    pxTrap();
-    if (!IsDebugBuild)
-        raise(SIGKILL);
+	pxTrap();
+	if (!IsDebugBuild)
+		raise(SIGKILL);
 }
 
 void _platform_InstallSignalHandler()
 {
-    Console.WriteLn("Installing POSIX SIGSEGV handler...");
-    struct sigaction sa;
+	Console.WriteLn("Installing POSIX SIGSEGV handler...");
+	struct sigaction sa;
 
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_SIGINFO;
-    sa.sa_sigaction = SysPageFaultSignalFilter;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = SysPageFaultSignalFilter;
 #ifdef __APPLE__
-    // MacOS uses SIGBUS for memory permission violations
-    sigaction(SIGBUS, &sa, NULL);
+	// MacOS uses SIGBUS for memory permission violations
+	sigaction(SIGBUS, &sa, NULL);
 #else
-    sigaction(SIGSEGV, &sa, NULL);
+	sigaction(SIGSEGV, &sa, NULL);
 #endif
 }
 
 static __ri void PageSizeAssertionTest(size_t size)
 {
-    pxAssertMsg((__pagesize == getpagesize()), pxsFmt(
-                                                   "Internal system error: Operating system pagesize does not match compiled pagesize.\n\t"
-                                                   L"\tOS Page Size: 0x%x (%d), Compiled Page Size: 0x%x (%u)",
-                                                   getpagesize(), getpagesize(), __pagesize, __pagesize));
+	pxAssertMsg((__pagesize == getpagesize()), pxsFmt(
+												   "Internal system error: Operating system pagesize does not match compiled pagesize.\n\t"
+												   L"\tOS Page Size: 0x%x (%d), Compiled Page Size: 0x%x (%u)",
+												   getpagesize(), getpagesize(), __pagesize, __pagesize));
 
-    pxAssertDev((size & (__pagesize - 1)) == 0, pxsFmt(
-                                                    L"Memory block size must be a multiple of the target platform's page size.\n"
-                                                    L"\tPage Size: 0x%x (%u), Block Size: 0x%x (%u)",
-                                                    __pagesize, __pagesize, size, size));
+	pxAssertDev((size & (__pagesize - 1)) == 0, pxsFmt(
+													L"Memory block size must be a multiple of the target platform's page size.\n"
+													L"\tPage Size: 0x%x (%u), Block Size: 0x%x (%u)",
+													__pagesize, __pagesize, size, size));
 }
 
 // returns FALSE if the mprotect call fails with an ENOMEM.
 // Raises assertions on other types of POSIX errors (since those typically reflect invalid object
 // or memory states).
-static bool _memprotect(void *baseaddr, size_t size, const PageProtectionMode &mode)
+static bool _memprotect(void* baseaddr, size_t size, const PageProtectionMode& mode)
 {
-    PageSizeAssertionTest(size);
+	PageSizeAssertionTest(size);
 
-    uint lnxmode = 0;
+	uint lnxmode = 0;
 
-    if (mode.CanWrite())
-        lnxmode |= PROT_WRITE;
-    if (mode.CanRead())
-        lnxmode |= PROT_READ;
-    if (mode.CanExecute())
-        lnxmode |= PROT_EXEC | PROT_READ;
+	if (mode.CanWrite())
+		lnxmode |= PROT_WRITE;
+	if (mode.CanRead())
+		lnxmode |= PROT_READ;
+	if (mode.CanExecute())
+		lnxmode |= PROT_EXEC | PROT_READ;
 
-    const int result = mprotect(baseaddr, size, lnxmode);
+	const int result = mprotect(baseaddr, size, lnxmode);
 
-    if (result == 0)
-        return true;
+	if (result == 0)
+		return true;
 
-    switch (errno) {
-        case EINVAL:
-            pxFailDev(pxsFmt(L"mprotect returned EINVAL @ 0x%08X -> 0x%08X  (mode=%s)",
-                             baseaddr, (uptr)baseaddr + size, WX_STR(mode.ToString())));
-            break;
+	switch (errno)
+	{
+		case EINVAL:
+			pxFailDev(pxsFmt(L"mprotect returned EINVAL @ 0x%08X -> 0x%08X  (mode=%s)",
+				baseaddr, (uptr)baseaddr + size, WX_STR(mode.ToString())));
+			break;
 
-        case EACCES:
-            pxFailDev(pxsFmt(L"mprotect returned EACCES @ 0x%08X -> 0x%08X  (mode=%s)",
-                             baseaddr, (uptr)baseaddr + size, WX_STR(mode.ToString())));
-            break;
+		case EACCES:
+			pxFailDev(pxsFmt(L"mprotect returned EACCES @ 0x%08X -> 0x%08X  (mode=%s)",
+				baseaddr, (uptr)baseaddr + size, WX_STR(mode.ToString())));
+			break;
 
-        case ENOMEM:
-            // caller handles assertion or exception, or whatever.
-            break;
-    }
-    return false;
+		case ENOMEM:
+			// caller handles assertion or exception, or whatever.
+			break;
+	}
+	return false;
 }
 
-void *HostSys::MmapReservePtr(void *base, size_t size)
+void* HostSys::MmapReservePtr(void* base, size_t size)
 {
-    PageSizeAssertionTest(size);
+	PageSizeAssertionTest(size);
 
-    // On linux a reserve-without-commit is performed by using mmap on a read-only
-    // or anonymous source, with PROT_NONE (no-access) permission.  Since the mapping
-    // is completely inaccessible, the OS will simply reserve it and will not put it
-    // against the commit table.
-    return mmap(base, size, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	// On linux a reserve-without-commit is performed by using mmap on a read-only
+	// or anonymous source, with PROT_NONE (no-access) permission.  Since the mapping
+	// is completely inaccessible, the OS will simply reserve it and will not put it
+	// against the commit table.
+	return mmap(base, size, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 }
 
-bool HostSys::MmapCommitPtr(void *base, size_t size, const PageProtectionMode &mode)
+bool HostSys::MmapCommitPtr(void* base, size_t size, const PageProtectionMode& mode)
 {
-    // In linux, reserved memory is automatically committed when its permissions are
-    // changed to something other than PROT_NONE.  If the user is committing memory
-    // as PROT_NONE, then just ignore this call (memory will be committed automatically
-    // later when the user changes permissions to something useful via calls to MemProtect).
+	// In linux, reserved memory is automatically committed when its permissions are
+	// changed to something other than PROT_NONE.  If the user is committing memory
+	// as PROT_NONE, then just ignore this call (memory will be committed automatically
+	// later when the user changes permissions to something useful via calls to MemProtect).
 
-    if (mode.IsNone())
-        return false;
+	if (mode.IsNone())
+		return false;
 
-    if (_memprotect(base, size, mode))
-        return true;
+	if (_memprotect(base, size, mode))
+		return true;
 
-    if (!pxDoOutOfMemory)
-        return false;
-    pxDoOutOfMemory(size);
-    return _memprotect(base, size, mode);
+	if (!pxDoOutOfMemory)
+		return false;
+	pxDoOutOfMemory(size);
+	return _memprotect(base, size, mode);
 }
 
-void HostSys::MmapResetPtr(void *base, size_t size)
+void HostSys::MmapResetPtr(void* base, size_t size)
 {
-    PageSizeAssertionTest(size);
+	PageSizeAssertionTest(size);
 
-    void *result = mmap(base, size, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
+	void* result = mmap(base, size, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
 
-    pxAssertRel((uptr)result == (uptr)base, pxsFmt(
-                                                "Virtual memory decommit failed: memory at 0x%08X -> 0x%08X could not be remapped.",
-                                                base, (uptr)base + size));
+	pxAssertRel((uptr)result == (uptr)base, pxsFmt(
+												"Virtual memory decommit failed: memory at 0x%08X -> 0x%08X could not be remapped.",
+												base, (uptr)base + size));
 }
 
-void *HostSys::MmapReserve(uptr base, size_t size)
+void* HostSys::MmapReserve(uptr base, size_t size)
 {
-    return MmapReservePtr((void *)base, size);
+	return MmapReservePtr((void*)base, size);
 }
 
-bool HostSys::MmapCommit(uptr base, size_t size, const PageProtectionMode &mode)
+bool HostSys::MmapCommit(uptr base, size_t size, const PageProtectionMode& mode)
 {
-    return MmapCommitPtr((void *)base, size, mode);
+	return MmapCommitPtr((void*)base, size, mode);
 }
 
 void HostSys::MmapReset(uptr base, size_t size)
 {
-    MmapResetPtr((void *)base, size);
+	MmapResetPtr((void*)base, size);
 }
 
-void *HostSys::Mmap(uptr base, size_t size)
+void* HostSys::Mmap(uptr base, size_t size)
 {
-    PageSizeAssertionTest(size);
+	PageSizeAssertionTest(size);
 
-    // MAP_ANONYMOUS - means we have no associated file handle (or device).
+	// MAP_ANONYMOUS - means we have no associated file handle (or device).
 
-    return mmap((void *)base, size, PROT_EXEC | PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	return mmap((void*)base, size, PROT_EXEC | PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 }
 
 void HostSys::Munmap(uptr base, size_t size)
 {
-    if (!base)
-        return;
-    munmap((void *)base, size);
+	if (!base)
+		return;
+	munmap((void*)base, size);
 }
 
-void HostSys::MemProtect(void *baseaddr, size_t size, const PageProtectionMode &mode)
+void HostSys::MemProtect(void* baseaddr, size_t size, const PageProtectionMode& mode)
 {
-    if (!_memprotect(baseaddr, size, mode)) {
-        throw Exception::OutOfMemory(L"MemProtect")
-            .SetDiagMsg(pxsFmt(L"mprotect failed @ 0x%08X -> 0x%08X  (mode=%s)",
-                               baseaddr, (uptr)baseaddr + size, WX_STR(mode.ToString())));
-    }
+	if (!_memprotect(baseaddr, size, mode))
+	{
+		throw Exception::OutOfMemory(L"MemProtect")
+			.SetDiagMsg(pxsFmt(L"mprotect failed @ 0x%08X -> 0x%08X  (mode=%s)",
+				baseaddr, (uptr)baseaddr + size, WX_STR(mode.ToString())));
+	}
 }
 #endif
--- common/Linux/LnxThreads.cpp
+++ common/Linux/LnxThreads.cpp
@@ -39,21 +39,21 @@
 
 __forceinline void Threading::Sleep(int ms)
 {
-    usleep(1000 * ms);
+	usleep(1000 * ms);
 }
 
 // For use in spin/wait loops,  Acts as a hint to Intel CPUs and should, in theory
 // improve performance and reduce cpu power consumption.
 __forceinline void Threading::SpinWait()
 {
-    // If this doesn't compile you can just comment it out (it only serves as a
-    // performance hint and isn't required).
-    __asm__("pause");
+	// If this doesn't compile you can just comment it out (it only serves as a
+	// performance hint and isn't required).
+	__asm__("pause");
 }
 
 __forceinline void Threading::EnableHiresScheduler()
 {
-    // Don't know if linux has a customizable scheduler resolution like Windows (doubtful)
+	// Don't know if linux has a customizable scheduler resolution like Windows (doubtful)
 }
 
 __forceinline void Threading::DisableHiresScheduler()
@@ -63,69 +63,72 @@ __forceinline void Threading::DisableHiresScheduler()
 // Unit of time of GetThreadCpuTime/GetCpuTime
 u64 Threading::GetThreadTicksPerSecond()
 {
-    return 1000000;
+	return 1000000;
 }
 
 // Helper function to get either either the current cpu usage
 // in called thread or in id thread
 static u64 get_thread_time(uptr id = 0)
 {
-    clockid_t cid;
-    if (id) {
-        int err = pthread_getcpuclockid((pthread_t)id, &cid);
-        if (err)
-            return 0;
-    } else {
-        cid = CLOCK_THREAD_CPUTIME_ID;
-    }
+	clockid_t cid;
+	if (id)
+	{
+		int err = pthread_getcpuclockid((pthread_t)id, &cid);
+		if (err)
+			return 0;
+	}
+	else
+	{
+		cid = CLOCK_THREAD_CPUTIME_ID;
+	}
 
-    struct timespec ts;
-    int err = clock_gettime(cid, &ts);
-    if (err)
-        return 0;
+	struct timespec ts;
+	int err = clock_gettime(cid, &ts);
+	if (err)
+		return 0;
 
-    return (u64)ts.tv_sec * (u64)1e6 + (u64)ts.tv_nsec / (u64)1e3;
+	return (u64)ts.tv_sec * (u64)1e6 + (u64)ts.tv_nsec / (u64)1e3;
 }
 
 // Returns the current timestamp (not relative to a real world clock)
 u64 Threading::GetThreadCpuTime()
 {
-    return get_thread_time();
+	return get_thread_time();
 }
 
 u64 Threading::pxThread::GetCpuTime() const
 {
-    // Get the cpu time for the thread belonging to this object.  Use m_native_id and/or
-    // m_native_handle to implement it. Return value should be a measure of total time the
-    // thread has used on the CPU (scaled by the value returned by GetThreadTicksPerSecond(),
-    // which typically would be an OS-provided scalar or some sort).
+	// Get the cpu time for the thread belonging to this object.  Use m_native_id and/or
+	// m_native_handle to implement it. Return value should be a measure of total time the
+	// thread has used on the CPU (scaled by the value returned by GetThreadTicksPerSecond(),
+	// which typically would be an OS-provided scalar or some sort).
 
-    if (!m_native_id)
-        return 0;
+	if (!m_native_id)
+		return 0;
 
-    return get_thread_time(m_native_id);
+	return get_thread_time(m_native_id);
 }
 
 void Threading::pxThread::_platform_specific_OnStartInThread()
 {
-    // Obtain linux-specific thread IDs or Handles here, which can be used to query
-    // kernel scheduler performance information.
-    m_native_id = (uptr)pthread_self();
+	// Obtain linux-specific thread IDs or Handles here, which can be used to query
+	// kernel scheduler performance information.
+	m_native_id = (uptr)pthread_self();
 }
 
 void Threading::pxThread::_platform_specific_OnCleanupInThread()
 {
-    // Cleanup handles here, which were opened above.
+	// Cleanup handles here, which were opened above.
 }
 
-void Threading::SetNameOfCurrentThread(const char *name)
+void Threading::SetNameOfCurrentThread(const char* name)
 {
 #if defined(__linux__)
-    // Extract of manpage: "The name can be up to 16 bytes long, and should be
-    //						null-terminated if it contains fewer bytes."
-    prctl(PR_SET_NAME, name, 0, 0, 0);
+	// Extract of manpage: "The name can be up to 16 bytes long, and should be
+	//						null-terminated if it contains fewer bytes."
+	prctl(PR_SET_NAME, name, 0, 0, 0);
 #elif defined(__unix__)
-    pthread_set_name_np(pthread_self(), name);
+	pthread_set_name_np(pthread_self(), name);
 #endif
 }
 
