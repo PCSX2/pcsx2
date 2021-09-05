0b23d886b20a8a118aedb5555a5cac5586c2b77e
lightningterror <18107717+lightningterror@users.noreply.github.com>
2021-09-05 06:02:58 +0200

    gs: GSCapture Replace format with fmt format.

--- pcsx2/GS/GSCapture.cpp
+++ pcsx2/GS/GSCapture.cpp
@@ -548,7 +548,7 @@ bool GSCapture::DeliverFrame(const void* bits, int pitch, bool rgba)
 
 #elif defined(__unix__)
 
-	std::string out_file = m_out_dir + format("/frame.%010d.png", m_frame);
+	std::string out_file = m_out_dir + fmt::format("/frame.{:010d}.png", m_frame);
 	//GSPng::Save(GSPng::RGB_PNG, out_file, (uint8*)bits, m_size.x, m_size.y, pitch, m_compression_level);
 	m_workers[m_frame % m_threads]->Push(std::make_shared<GSPng::Transaction>(GSPng::RGB_PNG, out_file, static_cast<const uint8*>(bits), m_size.x, m_size.y, pitch, m_compression_level));
 
