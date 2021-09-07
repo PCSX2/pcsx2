8e3aabcc659f13bc0a61d75a0710967ee03ff2ab
kojin <kojin@protonmail.com>
2021-09-07 11:14:26 -0400

    core: port hidapi to DynamicLibrary

--- pcsx2/USB/shared/hidapi.cpp
+++ pcsx2/USB/shared/hidapi.cpp
@@ -14,62 +14,87 @@
  */
 
 #include "PrecompiledHeader.h"
+#include "common/DynamicLibrary.h"
 #include <windows.h>
 #include <setupapi.h>
 #include "hidapi.h"
 
-_HidD_GetHidGuid HidD_GetHidGuid = NULL;
-_HidD_GetAttributes HidD_GetAttributes = NULL;
-_HidD_GetPreparsedData HidD_GetPreparsedData = NULL;
-_HidP_GetCaps HidP_GetCaps = NULL;
-_HidD_FreePreparsedData HidD_FreePreparsedData = NULL;
-_HidD_GetFeature HidD_GetFeature = NULL;
-_HidD_SetFeature HidD_SetFeature = NULL;
-_HidP_GetSpecificButtonCaps HidP_GetSpecificButtonCaps = NULL;
-_HidP_GetButtonCaps HidP_GetButtonCaps = NULL;
-_HidP_GetUsages HidP_GetUsages = NULL;
-_HidP_GetValueCaps HidP_GetValueCaps = NULL;
-_HidP_GetUsageValue HidP_GetUsageValue = NULL;
-_HidD_GetProductString HidD_GetProductString = NULL;
+_HidD_GetHidGuid HidD_GetHidGuid = nullptr;
+_HidD_GetAttributes HidD_GetAttributes = nullptr;
+_HidD_GetPreparsedData HidD_GetPreparsedData = nullptr;
+_HidP_GetCaps HidP_GetCaps = nullptr;
+_HidD_FreePreparsedData HidD_FreePreparsedData = nullptr;
+_HidD_GetFeature HidD_GetFeature = nullptr;
+_HidD_SetFeature HidD_SetFeature = nullptr;
+_HidP_GetSpecificButtonCaps HidP_GetSpecificButtonCaps = nullptr;
+_HidP_GetButtonCaps HidP_GetButtonCaps = nullptr;
+_HidP_GetUsages HidP_GetUsages = nullptr;
+_HidP_GetValueCaps HidP_GetValueCaps = nullptr;
+_HidP_GetUsageValue HidP_GetUsageValue = nullptr;
+_HidD_GetProductString HidD_GetProductString = nullptr;
 
-static HMODULE hModHid = 0;
+static Common::DynamicLibrary s_hid_lib;
 
 int InitHid()
 {
-	if (hModHid)
-	{
+	if (s_hid_lib.IsOpen())
 		return 1;
-	}
-	hModHid = LoadLibraryA("hid.dll");
-	if (hModHid)
-	{
-		if ((HidD_GetHidGuid = (_HidD_GetHidGuid)GetProcAddress(hModHid, "HidD_GetHidGuid")) &&
-			(HidD_GetAttributes = (_HidD_GetAttributes)GetProcAddress(hModHid, "HidD_GetAttributes")) &&
-			(HidD_GetPreparsedData = (_HidD_GetPreparsedData)GetProcAddress(hModHid, "HidD_GetPreparsedData")) &&
-			(HidP_GetCaps = (_HidP_GetCaps)GetProcAddress(hModHid, "HidP_GetCaps")) &&
-			(HidD_FreePreparsedData = (_HidD_FreePreparsedData)GetProcAddress(hModHid, "HidD_FreePreparsedData")) &&
-			(HidP_GetSpecificButtonCaps = (_HidP_GetSpecificButtonCaps)GetProcAddress(hModHid, "HidP_GetSpecificButtonCaps")) &&
-			(HidP_GetButtonCaps = (_HidP_GetButtonCaps)GetProcAddress(hModHid, "HidP_GetButtonCaps")) &&
-			(HidP_GetUsages = (_HidP_GetUsages)GetProcAddress(hModHid, "HidP_GetUsages")) &&
-			(HidP_GetValueCaps = (_HidP_GetValueCaps)GetProcAddress(hModHid, "HidP_GetValueCaps")) &&
-			(HidP_GetUsageValue = (_HidP_GetUsageValue)GetProcAddress(hModHid, "HidP_GetUsageValue")) &&
-			(HidD_GetProductString = (_HidD_GetProductString)GetProcAddress(hModHid, "HidD_GetProductString")) &&
-			(HidD_GetFeature = (_HidD_GetFeature)GetProcAddress(hModHid, "HidD_GetFeature")) &&
-			(HidD_SetFeature = (_HidD_SetFeature)GetProcAddress(hModHid, "HidD_SetFeature")))
-		{
-			//pHidD_GetHidGuid(&GUID_DEVINTERFACE_HID);
-			return 1;
-		}
-		UninitHid();
-	}
+
+	if (!s_hid_lib.Open("hid"))
+		return 0;
+
+	HidP_GetUsages = s_hid_lib.GetSymbol<_HidP_GetUsages>("HidP_GetUsages");
+	HidP_GetCaps   = s_hid_lib.GetSymbol<_HidP_GetCaps>("HidP_GetCaps");
+
+	HidD_GetHidGuid     = s_hid_lib.GetSymbol<_HidD_GetHidGuid>("HidD_GetHidGuid");
+	HidD_GetAttributes  = s_hid_lib.GetSymbol<_HidD_GetAttributes>("HidD_GetAttributes");
+
+	HidP_GetButtonCaps = s_hid_lib.GetSymbol<_HidP_GetButtonCaps>("HidP_GetButtonCaps");
+	HidP_GetValueCaps = s_hid_lib.GetSymbol<_HidP_GetValueCaps>("HidP_GetValueCaps");
+
+	HidD_GetPreparsedData  = s_hid_lib.GetSymbol<_HidD_GetPreparsedData>("HidD_GetPreparsedData");
+	HidD_FreePreparsedData = s_hid_lib.GetSymbol<_HidD_FreePreparsedData>("HidD_FreePreparsedData");
+
+	HidP_GetSpecificButtonCaps = s_hid_lib.GetSymbol<_HidP_GetSpecificButtonCaps>("HidP_GetSpecificButtonCaps");
+
+	HidP_GetUsageValue    = s_hid_lib.GetSymbol<_HidP_GetUsageValue>("HidP_GetUsageValue");
+	HidD_GetProductString = s_hid_lib.GetSymbol<_HidD_GetProductString>("HidD_GetProductString");
+
+	HidD_GetFeature = s_hid_lib.GetSymbol<_HidD_GetFeature>("HidD_GetFeature");
+	HidD_SetFeature = s_hid_lib.GetSymbol<_HidD_SetFeature>("HidD_SetFeature");
+
+	const bool loaded =
+		HidP_GetUsages && HidP_GetCaps && HidD_GetHidGuid &&
+		HidD_GetAttributes && HidP_GetButtonCaps && HidP_GetValueCaps &&
+		HidD_GetPreparsedData && HidD_FreePreparsedData && HidP_GetSpecificButtonCaps &&
+		HidP_GetUsageValue && HidD_GetProductString && HidD_GetFeature && HidD_SetFeature;
+
+	if(loaded)
+		return 1;
+
+	UninitHid();
+
 	return 0;
 }
 
 void UninitHid()
 {
-	if (hModHid)
-	{
-		FreeLibrary(hModHid);
-		hModHid = 0;
-	}
+	HidP_GetUsages = nullptr;
+	HidP_GetCaps = nullptr;
+	HidD_GetHidGuid = nullptr;
+
+	HidD_GetAttributes = nullptr;
+	HidP_GetButtonCaps = nullptr;
+	HidP_GetValueCaps = nullptr;
+	HidD_GetPreparsedData = nullptr;
+
+	HidD_FreePreparsedData = nullptr;
+	HidP_GetSpecificButtonCaps = nullptr;
+	HidP_GetUsageValue = nullptr;
+	HidD_GetProductString = nullptr;
+
+	HidD_GetFeature = nullptr;
+	HidD_SetFeature = nullptr;
+
+	s_hid_lib.Close();
 }
