204bbd52b763eb82395793a442b92ae13968da1d
Ty Lamontagne <amfobes@gmail.com>
2021-09-26 10:54:53 -0400

    Perf: Fix missing include

--- common/Perf.cpp
+++ common/Perf.cpp
@@ -1,5 +1,5 @@
 /*  PCSX2 - PS2 Emulator for PCs
- *  Copyright (C) 2002-2015  PCSX2 Dev Team
+ *  Copyright (C) 2002-2021  PCSX2 Dev Team
  *
  *  PCSX2 is free software: you can redistribute it and/or modify it under the terms
  *  of the GNU Lesser General Public License as published by the Free Software Found-
@@ -15,6 +15,7 @@
 
 #include "common/Perf.h"
 #include "common/Pcsx2Types.h"
+#include "common/Dependencies.h"
 #ifdef __unix__
 #include <unistd.h>
 #endif
