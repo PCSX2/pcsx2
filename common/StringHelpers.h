b05668ff591ef3cbeac190efad94c03427f15530
Connor McLaughlin <stenzek@gmail.com>
2021-10-01 10:13:22 +1000

    Common/StringHelpers: Fix printing of 64-bit pointers

--- common/StringHelpers.h
+++ common/StringHelpers.h
@@ -15,6 +15,7 @@
 
 #pragma once
 
+#include <cinttypes>
 #include <wx/tokenzr.h>
 #include "common/Dependencies.h"
 #include "common/SafeArray.h"
@@ -225,7 +226,11 @@ extern bool pxParseAssignmentString(const wxString& src, wxString& ldest, wxStri
 #define pxsFmt FastFormatUnicode().Write
 #define pxsFmtV FastFormatUnicode().WriteV
 
-#define pxsPtr(ptr) pxsFmt("0x%08X", (ptr)).c_str()
+#ifdef _M_X86_64
+#define pxsPtr(ptr) pxsFmt("0x%016" PRIXPTR, (ptr)).c_str()
+#else
+#define pxsPtr(ptr) pxsFmt("0x%08" PRIXPTR, (ptr)).c_str()
+#endif
 
 extern wxString& operator+=(wxString& str1, const FastFormatUnicode& str2);
 extern wxString operator+(const wxString& str1, const FastFormatUnicode& str2);
