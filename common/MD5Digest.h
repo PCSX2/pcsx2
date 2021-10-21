cdb0068dcc4492a066a7383ef06844450bec7b80
Connor McLaughlin <stenzek@gmail.com>
2021-10-21 16:57:10 +1000

    Common: Add MD5Digest

---
+++ common/MD5Digest.h
@@ -0,0 +1,32 @@
+/*  PCSX2 - PS2 Emulator for PCs
+ *  Copyright (C) 2002-2021  PCSX2 Dev Team
+ *
+ *  PCSX2 is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU Lesser General Public License as published by the Free Software Found-
+ *  ation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  PCSX2 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *  PURPOSE.  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with PCSX2.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#include "Pcsx2Types.h"
+
+class MD5Digest
+{
+public:
+  MD5Digest();
+
+  void Update(const void* pData, u32 cbData);
+  void Final(u8 Digest[16]);
+  void Reset();
+
+private:
+  u32 buf[4];
+  u32 bits[2];
+  u8 in[64];
+};
