b23a4b894eb345265bdf7297cb752a24bda8b50b
TellowKrinkle <tellowkrinkle@gmail.com>
2021-10-23 23:36:11 -0500

    Common: Add non-constant offsetof macro

--- common/Pcsx2Defs.h
+++ common/Pcsx2Defs.h
@@ -31,6 +31,10 @@
 
 #include "common/emitter/x86_intrin.h"
 
+// The C++ standard doesn't allow `offsetof` to be used on non-constant values (e.g. `offsetof(class, field[i])`)
+// Use this in those situations
+#define OFFSETOF(a, b) (reinterpret_cast<size_t>(&(static_cast<a*>(0)->b)))
+
 // Renamed ARRAYSIZE to ArraySize -- looks nice and gets rid of Windows.h conflicts (air)
 // Notes: I'd have used ARRAY_SIZE instead but ran into cross-platform lib conflicts with
 // that as well.  >_<
