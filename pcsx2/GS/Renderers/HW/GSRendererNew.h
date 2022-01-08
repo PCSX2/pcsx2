bd0e4d5be334eb9b64bb2d9ac3fb02291c8a1c30
lightningterror <18107717+lightningterror@users.noreply.github.com>
2022-01-08 07:09:16 +0100

    GS-hw: Rename DATE_GL42 DATE_GL45.
    DATE_GL42 -> DATE_PRIMID
    DATE_GL45 -> DATE_BARRIER

--- pcsx2/GS/Renderers/HW/GSRendererNew.h
+++ pcsx2/GS/Renderers/HW/GSRendererNew.h
@@ -38,7 +38,7 @@ private:
 	inline void SetupIA(const float& sx, const float& sy);
 	inline void EmulateTextureShuffleAndFbmask();
 	inline void EmulateChannelShuffle(GSTexture** rt, const GSTextureCache::Source* tex);
-	inline void EmulateBlending(bool& DATE_GL42, bool& DATE_GL45);
+	inline void EmulateBlending(bool& DATE_PRIMID, bool& DATE_BARRIER);
 	inline void EmulateTextureSampler(const GSTextureCache::Source* tex);
 	inline void EmulateZbuffer();
 	inline void EmulateATST(float& AREF, GSHWDrawConfig::PSSelector& ps, bool pass_2);
