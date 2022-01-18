38a5431ceb9364933d8269ee38b07aedc0aced9b
TheLastRar <alastairbarnes123@hotmail.com>
2022-01-18 22:42:49 +0000

    Build: Update file copyright to 2022
    Also update the copyright year of SysForwardDefs.h

--- pcsx2/SysForwardDefs.h
+++ pcsx2/SysForwardDefs.h
@@ -1,5 +1,5 @@
 /*  PCSX2 - PS2 Emulator for PCs
- *  Copyright (C) 2002-2010  PCSX2 Dev Team
+ *  Copyright (C) 2002-2022  PCSX2 Dev Team
  *
  *  PCSX2 is free software: you can redistribute it and/or modify it under the terms
  *  of the GNU Lesser General Public License as published by the Free Software Found-
@@ -43,7 +43,7 @@
 #define VER_PRODUCT_VERSION_STR     VER_FILE_VERSION_STR
 #define VER_ORIGINAL_FILENAME_STR   VER_PRODUCTNAME_STR ".exe"
 #define VER_INTERNAL_NAME_STR       VER_ORIGINAL_FILENAME_STR
-#define VER_COPYRIGHT_STR           "Copyright (C) 2021"
+#define VER_COPYRIGHT_STR           "Copyright (C) 2022"
 
 class SysCoreThread;
 
