fdc3ca1227ad80ed7a09e554f99a678eea99b321
Connor McLaughlin <stenzek@gmail.com>
2022-03-25 22:16:08 +1000

    PAD: Add automatic mapping functions

--- pcsx2/PAD/Host/PAD.h
+++ pcsx2/PAD/Host/PAD.h
@@ -16,6 +16,7 @@
 #pragma once
 
 #include <string>
+#include <utility>
 #include <vector>
 
 #include "PAD/Host/Global.h"
@@ -23,6 +24,7 @@
 
 class SettingsInterface;
 struct WindowInfo;
+enum class GenericInputBinding : u8;
 
 s32 PADinit();
 void PADshutdown();
@@ -63,6 +65,10 @@ namespace PAD
 
 	/// Returns the vibration configuration for the specified controller type.
 	VibrationCapabilities GetControllerVibrationCapabilities(const std::string_view& type);
+
+	/// Performs automatic controller mapping with the provided list of generic mappings.
+	bool MapController(SettingsInterface& si, u32 controller,
+		const std::vector<std::pair<GenericInputBinding, std::string>>& mapping);
 
 	/// Sets the specified bind on a controller to the specified pressure (normalized to 0..1).
 	void SetControllerState(u32 controller, u32 bind, float value);
