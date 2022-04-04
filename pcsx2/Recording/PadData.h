9aecf79d7f65f6d44390f93c15b26080c1595e37
Tyler Wilding <xtvaser@gmail.com>
2022-04-03 20:07:29 -0400

    input-rec: refactor core code, freeze wxWidgets version

--- pcsx2/Recording/PadData.h
+++ pcsx2/Recording/PadData.h
@@ -1,5 +1,5 @@
 /*  PCSX2 - PS2 Emulator for PCs
- *  Copyright (C) 2002-2020  PCSX2 Dev Team
+ *  Copyright (C) 2002-2022  PCSX2 Dev Team
  *
  *  PCSX2 is free software: you can redistribute it and/or modify it under the terms
  *  of the GNU Lesser General Public License as published by the Free Software Found-
@@ -14,8 +14,6 @@
  */
 
 #pragma once
-
-#ifndef DISABLE_RECORDING
 
 class PadData
 {
@@ -122,7 +120,9 @@ private:
 	bool IsButtonPressed(ButtonResolver buttonResolver, u8 const& bufVal);
 	u8 BitmaskOrZero(bool pressed, ButtonResolver buttonInfo);
 
+#ifndef PCSX2_CORE
 	wxString RawPadBytesToString(int start, int end);
-};
-
+#else
+	std::string RawPadBytesToString(int start, int end);
 #endif
+};
