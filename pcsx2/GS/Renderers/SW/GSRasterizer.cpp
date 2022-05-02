0a34ff5be6f1515347671da9d2938efeb1253718
Connor McLaughlin <stenzek@gmail.com>
2022-05-02 16:05:24 +1000

    PerformanceMetrics: Switch to ThreadHandle

--- pcsx2/GS/Renderers/SW/GSRasterizer.cpp
+++ pcsx2/GS/Renderers/SW/GSRasterizer.cpp
@@ -1204,7 +1204,7 @@ GSRasterizerList::~GSRasterizerList()
 void GSRasterizerList::OnWorkerStartup(int i)
 {
 	Threading::SetNameOfCurrentThread(StringUtil::StdStringFromFormat("GS-SW-%d", i).c_str());
-	PerformanceMetrics::SetGSSWThreadTimer(i, Common::ThreadCPUTimer::GetForCallingThread());
+	PerformanceMetrics::SetGSSWThread(i, Threading::ThreadHandle::GetForCallingThread());
 }
 
 void GSRasterizerList::OnWorkerShutdown(int i)
