437b6f3f0e485e94046ff1348cfd4f8c8cdc17a4
Connor McLaughlin <stenzek@gmail.com>
2022-05-16 22:43:01 +1000

    Host: Reset imgui state when there's no frame to present
    Prevents things getting into a messed up state internally where several
    frames are buffered.

--- pcsx2/gui/AppHost.cpp
+++ pcsx2/gui/AppHost.cpp
@@ -153,7 +153,16 @@ HostDisplay* Host::GetHostDisplay()
 bool Host::BeginPresentFrame(bool frame_skip)
 {
 	CheckForGSWindowResize();
-	return s_host_display->BeginPresent(frame_skip);
+
+	if (!s_host_display->BeginPresent(frame_skip))
+	{
+		// if we're skipping a frame, we need to reset imgui's state, since
+		// we won't be calling EndPresentFrame().
+		ImGuiManager::NewFrame();
+		return false;
+	}
+
+	return true;
 }
 
 void Host::EndPresentFrame()
