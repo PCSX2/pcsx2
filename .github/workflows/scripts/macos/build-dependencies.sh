db4bf0847578f457c804af1f64842001334b6219
TellowKrinkle <tellowkrinkle@gmail.com>
2022-05-26 00:30:00 -0500

    GHActions:macOS: Add Qt build

--- .github/workflows/scripts/macos/build-dependencies.sh
+++ .github/workflows/scripts/macos/build-dependencies.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-export MACOSX_DEPLOYMENT_TARGET=10.13
+if [ "$GUI" == "Qt" ]; then
+	export MACOSX_DEPLOYMENT_TARGET=10.14
+else
+	export MACOSX_DEPLOYMENT_TARGET=10.13
+fi
 INSTALLDIR="$HOME/deps"
 NPROCS="$(getconf _NPROCESSORS_ONLN)"
 SDL=SDL2-2.0.22
@@ -12,6 +16,7 @@ SAMPLERATE=libsamplerate-0.1.9
 PORTAUDIO=pa_stable_v190700_20210406
 SOUNDTOUCH=soundtouch-2.3.1
 WXWIDGETS=3.1.6
+QT=6.2.4
 
 mkdir deps-build
 cd deps-build
@@ -29,6 +34,9 @@ fe7cbf3127882e3fc7259a75a0cb585620272c51745d3852ab9dd87960697f2e  $SDL.tar.gz
 47efbf42c77c19a05d22e627d42873e991ec0c1357219c0d74ce6a2948cb2def  $PORTAUDIO.tgz
 6900996607258496ce126924a19fe9d598af9d892cf3f33d1e4daaa9b42ae0b1  $SOUNDTOUCH.tar.gz
 4980e86c6494adcd527a41fc0a4e436777ba41d1893717d7b7176c59c2061c25  wxWidgets-$WXWIDGETS.tar.bz2
+d9924d6fd4fa5f8e24458c87f73ef3dfc1e7c9b877a5407c040d89e6736e2634  qtbase-everywhere-src-$QT.tar.xz
+17f40689c4a1706a1b7db22fa92f6ab79f7b698a89e100cab4d10e19335f8267  qttools-everywhere-src-$QT.tar.xz
+bd1aac74a892c60b2f147b6d53bb5b55ab7a6409e63097d38198933f8024fa51  qttranslations-everywhere-src-$QT.tar.xz
 EOF
 
 curl -L \
@@ -39,6 +47,9 @@ curl -L \
 	-O "http://files.portaudio.com/archives/$PORTAUDIO.tgz" \
 	-O "https://www.surina.net/soundtouch/$SOUNDTOUCH.tar.gz" \
 	-O "https://github.com/wxWidgets/wxWidgets/releases/download/v$WXWIDGETS/wxWidgets-$WXWIDGETS.tar.bz2" \
+	-O "https://download.qt.io/official_releases/qt/${QT%.*}/$QT/submodules/qtbase-everywhere-src-$QT.tar.xz" \
+	-O "https://download.qt.io/official_releases/qt/${QT%.*}/$QT/submodules/qttools-everywhere-src-$QT.tar.xz" \
+	-O "https://download.qt.io/official_releases/qt/${QT%.*}/$QT/submodules/qttranslations-everywhere-src-$QT.tar.xz" \
 
 shasum -a 256 --check SHASUMS
 
@@ -91,13 +102,53 @@ make -C build "-j$NPROCS"
 make -C build install
 cd ..
 
-echo "Installing wx..."
-tar xf "wxWidgets-$WXWIDGETS.tar.bz2"
-cd "wxWidgets-$WXWIDGETS"
-./configure --prefix "$INSTALLDIR" --with-macosx-version-min="$MACOSX_DEPLOYMENT_TARGET" --enable-clipboard --enable-dnd --enable-std_string --with-cocoa --with-libiconv --with-libjpeg --with-libpng --with-zlib --without-libtiff --without-regex
-make "-j$NPROCS"
-make install
-cd ..
+if [ "$GUI" == "wxWidgets" ]; then
+	echo "Installing wx..."
+	tar xf "wxWidgets-$WXWIDGETS.tar.bz2"
+	cd "wxWidgets-$WXWIDGETS"
+	./configure --prefix "$INSTALLDIR" --with-macosx-version-min="$MACOSX_DEPLOYMENT_TARGET" --enable-clipboard --enable-dnd --enable-std_string --with-cocoa --with-libiconv --with-libjpeg --with-libpng --with-zlib --without-libtiff --without-regex
+	make "-j$NPROCS"
+	make install
+	cd ..
+fi
+
+if [ "$GUI" == "Qt" ]; then
+	echo "Installing Qt Base..."
+	tar xf "qtbase-everywhere-src-$QT.tar.xz"
+	cd "qtbase-everywhere-src-$QT"
+	cmake -B build -DCMAKE_PREFIX_PATH="$INSTALLDIR" -DCMAKE_INSTALL_PREFIX="$INSTALLDIR" -DCMAKE_BUILD_TYPE=Release -DFEATURE_optimize_size=ON -DFEATURE_dbus=OFF -DFEATURE_framework=OFF -DFEATURE_icu=OFF -DFEATURE_opengl=OFF -DFEATURE_printsupport=OFF -DFEATURE_sql=OFF
+	make -C build "-j$NPROCS"
+	make -C build install
+	cd ..
+	echo "Installing Qt Tools..."
+	tar xf "qttools-everywhere-src-$QT.tar.xz"
+	cd "qttools-everywhere-src-$QT"
+	# Linguist relies on a library in the Designer target, which takes 5-7 minutes to build on the CI
+	# Avoid it by not building Linguist, since we only need the tools that come with it
+	patch -u src/linguist/CMakeLists.txt <<EOF
+--- src/linguist/CMakeLists.txt
++++ src/linguist/CMakeLists.txt
+@@ -14,7 +14,7 @@
+ add_subdirectory(lrelease-pro)
+ add_subdirectory(lupdate)
+ add_subdirectory(lupdate-pro)
+-if(QT_FEATURE_process AND QT_FEATURE_pushbutton AND QT_FEATURE_toolbutton AND TARGET Qt::Widgets AND NOT no-png)
++if(QT_FEATURE_process AND QT_FEATURE_pushbutton AND QT_FEATURE_toolbutton AND TARGET Qt::Widgets AND TARGET Qt::PrintSupport AND NOT no-png)
+     add_subdirectory(linguist)
+ endif()
+EOF
+	cmake -B build -DCMAKE_PREFIX_PATH="$INSTALLDIR" -DCMAKE_INSTALL_PREFIX="$INSTALLDIR" -DCMAKE_BUILD_TYPE=Release -DFEATURE_assistant=OFF -DFEATURE_clang=OFF -DFEATURE_designer=OFF -DFEATURE_kmap2qmap=OFF -DFEATURE_pixeltool=OFF -DFEATURE_pkg_config=OFF -DFEATURE_qev=OFF -DFEATURE_qtattributionsscanner=OFF -DFEATURE_qtdiag=OFF -DFEATURE_qtplugininfo=OFF
+	make -C build "-j$NPROCS"
+	make -C build install
+	cd ..
+	echo "Installing Qt Translations..."
+	tar xf "qttranslations-everywhere-src-$QT.tar.xz"
+	cd "qttranslations-everywhere-src-$QT"
+	cmake -B build -DCMAKE_PREFIX_PATH="$INSTALLDIR" -DCMAKE_INSTALL_PREFIX="$INSTALLDIR" -DCMAKE_BUILD_TYPE=Release
+	make -C build "-j$NPROCS"
+	make -C build install
+	cd ..
+fi
 
 echo "Cleaning up..."
 cd ..
