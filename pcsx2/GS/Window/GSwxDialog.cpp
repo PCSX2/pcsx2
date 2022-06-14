063d02a9cb7ea2085e87469ba2ec40f2f6ab2b7c
lightningterror <18107717+lightningterror@users.noreply.github.com>
2022-06-14 14:33:24 +0200

    GS-wx: Disable TX x y offsets on native res:
    We already gray out the option on Qt, and it should be an upscaling hack only.

--- pcsx2/GS/Window/GSwxDialog.cpp
+++ pcsx2/GS/Window/GSwxDialog.cpp
@@ -395,11 +395,11 @@ HacksTab::HacksTab(wxWindow* parent)
 	auto* tex_off_box = new wxBoxSizer(wxHORIZONTAL);
 	add_label(this, tex_off_box, "X:", IDC_TCOFFSETX, wxSizerFlags().Centre());
 	tex_off_box->AddSpacer(space);
-	m_ui.addSpin(tex_off_box, "UserHacks_TCOffsetX", 0, 10000, 0, IDC_TCOFFSETX, hacks_prereq);
+	m_ui.addSpin(tex_off_box, "UserHacks_TCOffsetX", 0, 10000, 0, IDC_TCOFFSETX, upscale_hacks_prereq);
 	tex_off_box->AddSpacer(space);
 	add_label(this, tex_off_box, "Y:", IDC_TCOFFSETY, wxSizerFlags().Centre());
 	tex_off_box->AddSpacer(space);
-	m_ui.addSpin(tex_off_box, "UserHacks_TCOffsetY", 0, 10000, 0, IDC_TCOFFSETY, hacks_prereq);
+	m_ui.addSpin(tex_off_box, "UserHacks_TCOffsetY", 0, 10000, 0, IDC_TCOFFSETY, upscale_hacks_prereq);
 
 	upscale_hack_choice_grid->Add(tex_off_box, wxSizerFlags().Expand());
 
