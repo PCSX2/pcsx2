f55570b8c2c79a60d2cf2cb9de2e29a078480ba1
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2022-06-29 14:06:11 -0700

    Qt 6.3.0 dependencies

--- .github/workflows/scripts/linux/build-dependencies.sh
+++ .github/workflows/scripts/linux/build-dependencies.sh
@@ -4,7 +4,7 @@ set -e
 SRCDIR="$HOME"
 INSTALLDIR="$HOME/Depends"
 
-QT=6.2.4
+QT=6.3.0
 SDL=SDL2-2.0.22
 PATCHELF_VERSION=0.14.5
 
@@ -22,15 +22,15 @@ cd "$SRCDIR"
 cat > SHASUMS <<EOF
 fe7cbf3127882e3fc7259a75a0cb585620272c51745d3852ab9dd87960697f2e  $SDL.tar.gz
 b9a46f2989322eb89fa4f6237e20836c57b455aa43a32545ea093b431d982f5c  patchelf-$PATCHELF_VERSION.tar.bz2
-d9924d6fd4fa5f8e24458c87f73ef3dfc1e7c9b877a5407c040d89e6736e2634  qtbase-everywhere-src-$QT.tar.xz
-23ec4c14259d799bb6aaf1a07559d6b1bd2cf6d0da3ac439221ebf9e46ff3fd2  qtsvg-everywhere-src-$QT.tar.xz
-17f40689c4a1706a1b7db22fa92f6ab79f7b698a89e100cab4d10e19335f8267  qttools-everywhere-src-$QT.tar.xz
-bd1aac74a892c60b2f147b6d53bb5b55ab7a6409e63097d38198933f8024fa51  qttranslations-everywhere-src-$QT.tar.xz
-ef7217d25608b6f3f3cd92fa783c8bfca50821906729887fe9d392db7e74920e qtwayland-everywhere-src-$QT.tar.xz
-fa6a8b5bfe43203daf022b7949c7874240ec28b9c4b7ebc52b2e5ea440e6e94f qtdeclarative-everywhere-src-$QT.tar.xz
+b865aae43357f792b3b0a162899d9bf6a1393a55c4e5e4ede5316b157b1a0f99  qtbase-everywhere-src-$QT.tar.xz
+d294b029dc2b2d4f65da516fdc3b8088d32643eb7ff77db135a8b9ce904caa37  qtdeclarative-everywhere-src-$QT.tar.xz
+3164504d7e3f640439308235739b112605ab5fc9cc517ca0b28f9fb93a8db0e3  qtsvg-everywhere-src-$QT.tar.xz
+fce94688ea925782a2879347584991f854630daadba6c52aed6d93e33cd0b19c  qttools-everywhere-src-$QT.tar.xz
+e4dd4ef892a34a9514a19238f189a33ed85c76f31dcad6599ced93b1e33440b3  qttranslations-everywhere-src-$QT.tar.xz
+e7b567f6e43ffc5918d4aa825ce1eced66a00cb0a87133b2912ba5c1b2a02190  qtwayland-everywhere-src-$QT.tar.xz
 EOF
 
-wget https://libsdl.org/release/$SDL.tar.gz \
+wget -q https://libsdl.org/release/$SDL.tar.gz \
 https://github.com/NixOS/patchelf/releases/download/"$PATCHELF_VERSION/patchelf-$PATCHELF_VERSION.tar.bz2" \
 https://download.qt.io/official_releases/qt/"${QT%.*}/$QT/submodules/qtbase-everywhere-src-$QT.tar.xz" \
 https://download.qt.io/official_releases/qt/"${QT%.*}/$QT/submodules/qtsvg-everywhere-src-$QT.tar.xz" \
@@ -40,7 +40,10 @@ https://download.qt.io/official_releases/qt/"${QT%.*}/$QT/submodules/qtdeclarati
 https://download.qt.io/official_releases/qt/"${QT%.*}/$QT/submodules/qttranslations-everywhere-src-$QT.tar.xz" 
 sha256sum -c SHASUMS
 
-tar -xvf "$SDL.tar.gz"
+#find sha256sums
+sha256sum *.tar*
+
+tar -xf "$SDL.tar.gz"
 
 cd "$SDL"
 
@@ -56,7 +59,7 @@ ninja -j$(nproc) && ninja install
 
 cd ../..
 
-tar xvf patchelf-$PATCHELF_VERSION.tar.bz2
+tar xf patchelf-$PATCHELF_VERSION.tar.bz2
 
 cd patchelf-$PATCHELF_VERSION
 
@@ -66,13 +69,13 @@ cd ..
 
     for f in qt*.tar*; do 
         mkdir "${f/-*/}"
-        tar -xvf "$f" -C "${f/-*/}" --strip-components=1; 
+        tar -xf "$f" -C "${f/-*/}" --strip-components=1; 
     done
 
 
 	echo "Installing Qt Base..."
 	cd "qtbase"
-	cmake -B build -DCMAKE_PREFIX_PATH="$INSTALLDIR" -GNinja -DCMAKE_INSTALL_PREFIX="$INSTALLDIR" -DCMAKE_BUILD_TYPE=Release -DFEATURE_optimize_size=ON -DFEATURE_framework=OFF  -DFEATURE_icu=OFF -DFEATURE_printsupport=OFF -DFEATURE_sql=OFF
+	cmake -B build -DCMAKE_PREFIX_PATH="$INSTALLDIR" -GNinja -DCMAKE_INSTALL_PREFIX="$INSTALLDIR" -DCMAKE_BUILD_TYPE=Release -DFEATURE_optimize_size=ON -DFEATURE_framework=OFF -DFEATURE_icu=OFF -DFEATURE_printsupport=OFF -DFEATURE_sql=OFF
 	ninja -C build -j$(nproc)
 	ninja -C build install
 	cd ..
@@ -102,14 +105,14 @@ EOF
 	ninja -C build -j$(nproc)
 	ninja -C build install
 	cd ..
-	echo "Installing Qt Wayland..."
-	cd "qtwayland"
+	echo "Installing Qt Declarative..."
+	cd "qtdeclarative"
 	cmake -B build -DCMAKE_INSTALL_PREFIX="$INSTALLDIR" -DCMAKE_PREFIX_PATH="$INSTALLDIR" -DCMAKE_BUILD_TYPE=Release -GNinja
 	ninja -C build -j$(nproc) 
 	ninja -C build install
 	cd ..
-	echo "Installing Qt Declarative..."
-	cd "qtdeclarative"
+	echo "Installing Qt Wayland..."
+	cd "qtwayland"
 	cmake -B build -DCMAKE_INSTALL_PREFIX="$INSTALLDIR" -DCMAKE_PREFIX_PATH="$INSTALLDIR" -DCMAKE_BUILD_TYPE=Release -GNinja
 	ninja -C build -j$(nproc) 
 	ninja -C build install
