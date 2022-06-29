aa8fffe87512189470e98bafda16d61edf7fc784
Connor McLaughlin <stenzek@gmail.com>
2022-06-29 19:31:44 +1000

    Qt: Fix drag/drop of blockdumps

--- pcsx2/VMManager.h
+++ pcsx2/VMManager.h
@@ -140,6 +140,9 @@ namespace VMManager
 	/// Returns true if the specified path is an ELF.
 	bool IsElfFileName(const std::string_view& path);
 
+	/// Returns true if the specified path is a blockdump.
+	bool IsBlockDumpFileName(const std::string_view& path);
+
 	/// Returns true if the specified path is a GS Dump.
 	bool IsGSDumpFileName(const std::string_view& path);
 
