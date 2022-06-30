fa99137c79077fa69fa209102ce861752beaf6fa
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2022-06-30 09:43:56 -0700

    Resolve Conflicts

--- .github/workflows/scripts/linux/generate-cmake.sh
+++ .github/workflows/scripts/linux/generate-cmake.sh
@@ -2,19 +2,25 @@
 
 set -e
 
+if [ "${COMPILER}" = "gcc" ]; then
+  export CC=gcc-10; export CXX=g++-10
+else
+  export CC=clang; export CXX=clang++
+fi
+
 echo "Additional CMake Args - ${ADDITIONAL_CMAKE_ARGS}"
 
 # Generate CMake into ./build
-cmake                                     \
--DCMAKE_BUILD_TYPE=RelWithDebInfo          \
+cmake                                       \
+-DCMAKE_BUILD_TYPE=Release                  \
 -DCMAKE_INSTALL_PREFIX="squashfs-root/usr/" \
--DCMAKE_PREFIX_PATH="$HOME/Depends"          \
--DWAYLAND_API=ON                              \
--DQT_BUILD=ON                                  \
--DXDG_STD=TRUE                                  \
--DDISABLE_ADVANCE_SIMD=TRUE						           \
--DUSE_VULKAN=ON                                   \
--DPACKAGE_MODE=ON                                  \
--DDISABLE_SETCAP=ON                                 \
--GNinja                                              \
+-DCMAKE_PREFIX_PATH="$HOME/Depends"         \
+-DWAYLAND_API=ON                            \
+-DXDG_STD=TRUE                              \
+-DDISABLE_ADVANCE_SIMD=TRUE                 \
+-DUSE_VULKAN=ON                             \
+-DPACKAGE_MODE=ON                           \
+-DDISABLE_SETCAP=ON                         \
+"$ADDITIONAL_CMAKE_ARGS"                    \
+-GNinja                                     \
 -B build
