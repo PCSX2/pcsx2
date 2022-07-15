498eb9330b1b76391eec5c0de889ee22f443b91f
lightningterror <18107717+lightningterror@users.noreply.github.com>
2022-07-15 02:39:20 +0200

    USB: Fix array index 'i' is used before limits check warning.
    Codacy.

--- pcsx2/USB/usb-eyetoy/jo_mpeg.cpp
+++ pcsx2/USB/usb-eyetoy/jo_mpeg.cpp
@@ -148,7 +148,7 @@ static int jo_processDU(jo_bits_t *bits, float A[64], const unsigned char htdc[9
 	for(; (endpos>0)&&(Q[endpos]==0); --endpos) { /* do nothing */ }
 	for(int i = 1; i <= endpos;) {
 		int run = 0;
-		while (Q[i]==0 && i<endpos) {
+		while (i < endpos && Q[i] == 0) {
 			++run;
 			++i;
 		}
