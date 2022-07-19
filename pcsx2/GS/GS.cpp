6e8d667c6a82277b442c51254f7559881625491c
lightningterror <18107717+lightningterror@users.noreply.github.com>
2022-07-19 22:29:50 +0200

    Wx: Remove AA1 option from the gui.

--- pcsx2/GS/GS.cpp
+++ pcsx2/GS/GS.cpp
@@ -1420,7 +1420,6 @@ void GSApp::Init()
 #else
 	m_default_configuration["linux_replay"]                               = "1";
 #endif
-	m_default_configuration["aa1"]                                        = "1";
 	m_default_configuration["accurate_date"]                              = "1";
 	m_default_configuration["accurate_blending_unit"]                     = "1";
 	m_default_configuration["AspectRatio"]                                = "1";
