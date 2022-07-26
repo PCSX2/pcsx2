10528da4615adf3d796b279db1eda92c91ee7986
Florin9doi <Florin9doi@users.noreply.github.com>
2022-07-26 16:05:36 +0300

    BIOS: Map the entire ROM1 file to PS2 memory
    EROM is part of ROM1, its exact location vary and can't be predicted

--- pcsx2/IopMem.cpp
+++ pcsx2/IopMem.cpp
@@ -99,12 +99,12 @@ void iopMemoryReserve::Reset()
 	//psxMemWLUT[0xbf80] = (uptr)iopHw;
 
 	// Read-only memory areas, so don't map WLUT for these...
-	for (int i=0; i<0x0040; i++)
+	for (int i = 0; i < 0x0040; i++)
 	{
 		psxMemWLUT[i + 0x2000 + 0x1fc0] = (uptr)&eeMem->ROM[i << 16];
 	}
 
-	for (int i=0; i<0x0004; i++)
+	for (int i = 0; i < 0x0040; i++)
 	{
 		psxMemWLUT[i + 0x2000 + 0x1e00] = (uptr)&eeMem->ROM1[i << 16];
 	}
--- pcsx2/ps2/BiosTools.cpp
+++ pcsx2/ps2/BiosTools.cpp
@@ -161,7 +161,7 @@ void ChecksumIt(u32& result, const u8 (&srcdata)[_size])
 // the base.
 //
 // Parameters:
-//   ext - extension of the sub-component to load.  Valid options are rom1, rom2, AND erom.
+//   ext - extension of the sub-component to load. Valid options are rom1 and rom2.
 //
 template <size_t _size>
 static void LoadExtraRom(const char* ext, u8 (&dest)[_size])
@@ -187,7 +187,7 @@ static void LoadExtraRom(const char* ext, u8 (&dest)[_size])
 		Console.Warning("BIOS Warning: %s could not be read (permission denied?)", ext);
 		return;
 	}
-	// Checksum for ROM1, ROM2, EROM?  Rama says no, Gigaherz says yes.  I'm not sure either way.  --air
+	// Checksum for ROM1, ROM2?  Rama says no, Gigaherz says yes.  I'm not sure either way.  --air
 	//ChecksumIt( BiosChecksum, dest );
 }
 
@@ -236,7 +236,7 @@ static std::string FindBiosImage()
 // this method being called.
 //
 // Remarks:
-//   This function does not fail if rom1, rom2, or erom files are missing, since none are
+//   This function does not fail if rom1 or rom2 files are missing, since none are
 //   explicitly required for most emulation tasks.
 //
 // Exceptions:
@@ -296,7 +296,6 @@ bool LoadBIOS()
 
 	LoadExtraRom("rom1", eeMem->ROM1);
 	LoadExtraRom("rom2", eeMem->ROM2);
-	LoadExtraRom("erom", eeMem->EROM);
 
 	if (EmuConfig.CurrentIRX.length() > 3)
 		LoadIrx(EmuConfig.CurrentIRX, &eeMem->ROM[0x3C0000], sizeof(eeMem->ROM) - 0x3C0000);
