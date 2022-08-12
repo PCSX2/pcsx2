4a2fcea77879257267390d1bd26026668a0087c4
JordanTheToaster <jordanh48@ymail.com>
2022-08-12 09:29:11 +0100

    Config: Change default dump compression
    Changes default dump compression from uncompressed to LZMA (xy)

--- pcsx2/Config.h
+++ pcsx2/Config.h
@@ -531,7 +531,7 @@ struct Pcsx2Config
 		CRCHackLevel CRCHack{CRCHackLevel::Automatic};
 		BiFiltering TextureFiltering{BiFiltering::PS2};
 		TexturePreloadingLevel TexturePreloading{TexturePreloadingLevel::Full};
-		GSDumpCompressionMethod GSDumpCompression{GSDumpCompressionMethod::Uncompressed};
+		GSDumpCompressionMethod GSDumpCompression{GSDumpCompressionMethod::LZMA};
 		int Dithering{2};
 		int MaxAnisotropy{0};
 		int SWExtraThreads{2};
