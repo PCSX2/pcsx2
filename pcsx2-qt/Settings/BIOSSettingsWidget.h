9b22913f8dc2b3c5b08806f0ab05305f312c89ca
TellowKrinkle <tellowkrinkle@gmail.com>
2022-08-22 17:17:01 -0500

    Qt: no-pch support

--- pcsx2-qt/Settings/BIOSSettingsWidget.h
+++ pcsx2-qt/Settings/BIOSSettingsWidget.h
@@ -15,8 +15,9 @@
 
 #pragma once
 #include <QtCore/QDir>
-#include <QtCore/QString>
 #include <QtCore/QPair>
+#include <QtCore/QString>
+#include <QtCore/QThread>
 #include <QtCore/QVector>
 #include <QtWidgets/QWidget>
 #include <string>
