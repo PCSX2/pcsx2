c332bd198a18bea694412e5779398b03f0182650
lightningterror <18107717+lightningterror@users.noreply.github.com>
2022-08-24 01:42:28 +0200

    GS-wx: Remove unused FMVSoftwareRendererSwitch config.

--- pcsx2/GS/GS.cpp
+++ pcsx2/GS/GS.cpp
@@ -1444,7 +1444,6 @@ void GSApp::Init()
 	m_default_configuration["extrathreads"]                               = "2";
 	m_default_configuration["extrathreads_height"]                        = "4";
 	m_default_configuration["filter"]                                     = std::to_string(static_cast<s8>(BiFiltering::PS2));
-	m_default_configuration["FMVSoftwareRendererSwitch"]                  = "0";
 	m_default_configuration["FullscreenMode"]                             = "";
 	m_default_configuration["fxaa"]                                       = "0";
 	m_default_configuration["GSDumpCompression"]                          = "0";
