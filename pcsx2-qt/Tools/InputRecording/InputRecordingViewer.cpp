6288f945cd014b6f778c8b5f8389a68da255990e
sonicfind <sonicfind2@gmail.com>
2022-09-22 10:19:56 -0500

    input-rec: refactor PadData button state control

--- pcsx2-qt/Tools/InputRecording/InputRecordingViewer.cpp
+++ pcsx2-qt/Tools/InputRecording/InputRecordingViewer.cpp
@@ -50,24 +50,24 @@ void InputRecordingViewer::loadTable()
 	for (const auto& frame : data)
 	{
 		// TODO - disgusting, clean it up
-		m_ui.tableWidget->setItem(frameNum, 0, new QTableWidgetItem(tr("%1 %2").arg(frame.m_leftAnalogX).arg(frame.m_leftAnalogY)));
-		m_ui.tableWidget->setItem(frameNum, 1, new QTableWidgetItem(tr("%1 %2").arg(frame.m_rightAnalogX).arg(frame.m_rightAnalogY)));
-		m_ui.tableWidget->setItem(frameNum, 2, new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_crossPressed).arg(frame.m_crossPressure)));
-		m_ui.tableWidget->setItem(frameNum, 3, new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_squarePressed).arg(frame.m_squarePressure)));
-		m_ui.tableWidget->setItem(frameNum, 4, new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_trianglePressed).arg(frame.m_trianglePressure)));
-		m_ui.tableWidget->setItem(frameNum, 5, new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_circlePressed).arg(frame.m_circlePressure)));
-		m_ui.tableWidget->setItem(frameNum, 6, new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_l1Pressed).arg(frame.m_l1Pressure)));
-		m_ui.tableWidget->setItem(frameNum, 7, new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_l2Pressed).arg(frame.m_l2Pressure)));
-		m_ui.tableWidget->setItem(frameNum, 8, new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_r1Pressed).arg(frame.m_r1Pressure)));
-		m_ui.tableWidget->setItem(frameNum, 9, new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_r1Pressed).arg(frame.m_r2Pressure)));
-		m_ui.tableWidget->setItem(frameNum, 10, new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_downPressed).arg(frame.m_downPressure)));
-		m_ui.tableWidget->setItem(frameNum, 11, new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_rightPressed).arg(frame.m_rightPressure)));
-		m_ui.tableWidget->setItem(frameNum, 12, new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_upPressed).arg(frame.m_upPressure)));
-		m_ui.tableWidget->setItem(frameNum, 13, new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_leftPressed).arg(frame.m_leftPressure)));
-		m_ui.tableWidget->setItem(frameNum, 14, new QTableWidgetItem(tr("%1").arg(frame.m_l3)));
-		m_ui.tableWidget->setItem(frameNum, 15, new QTableWidgetItem(tr("%1").arg(frame.m_r3)));
-		m_ui.tableWidget->setItem(frameNum, 16, new QTableWidgetItem(tr("%1").arg(frame.m_select)));
-		m_ui.tableWidget->setItem(frameNum, 17, new QTableWidgetItem(tr("%1").arg(frame.m_start)));
+		m_ui.tableWidget->setItem(frameNum, 0,  new QTableWidgetItem(tr("%1 %2")  .arg(frame.m_leftAnalogX)              .arg(frame.m_leftAnalogY)));
+		m_ui.tableWidget->setItem(frameNum, 1,  new QTableWidgetItem(tr("%1 %2")  .arg(frame.m_rightAnalogX)             .arg(frame.m_rightAnalogY)));
+		m_ui.tableWidget->setItem(frameNum, 2,  new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_crossPressed.m_pressed)   .arg(frame.m_crossPressure)));
+		m_ui.tableWidget->setItem(frameNum, 3,  new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_squarePressed.m_pressed)  .arg(frame.m_squarePressure)));
+		m_ui.tableWidget->setItem(frameNum, 4,  new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_trianglePressed.m_pressed).arg(frame.m_trianglePressure)));
+		m_ui.tableWidget->setItem(frameNum, 5,  new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_circlePressed.m_pressed)  .arg(frame.m_circlePressure)));
+		m_ui.tableWidget->setItem(frameNum, 6,  new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_l1Pressed.m_pressed)      .arg(frame.m_l1Pressure)));
+		m_ui.tableWidget->setItem(frameNum, 7,  new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_l2Pressed.m_pressed)      .arg(frame.m_l2Pressure)));
+		m_ui.tableWidget->setItem(frameNum, 8,  new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_r1Pressed.m_pressed)      .arg(frame.m_r1Pressure)));
+		m_ui.tableWidget->setItem(frameNum, 9,  new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_r1Pressed.m_pressed)      .arg(frame.m_r2Pressure)));
+		m_ui.tableWidget->setItem(frameNum, 10, new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_downPressed.m_pressed)    .arg(frame.m_downPressure)));
+		m_ui.tableWidget->setItem(frameNum, 11, new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_rightPressed.m_pressed)   .arg(frame.m_rightPressure)));
+		m_ui.tableWidget->setItem(frameNum, 12, new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_upPressed.m_pressed)      .arg(frame.m_upPressure)));
+		m_ui.tableWidget->setItem(frameNum, 13, new QTableWidgetItem(tr("%1 [%2]").arg(frame.m_leftPressed.m_pressed)    .arg(frame.m_leftPressure)));
+		m_ui.tableWidget->setItem(frameNum, 14, new QTableWidgetItem(tr("%1")     .arg(frame.m_l3.m_pressed)));
+		m_ui.tableWidget->setItem(frameNum, 15, new QTableWidgetItem(tr("%1")     .arg(frame.m_r3.m_pressed)));
+		m_ui.tableWidget->setItem(frameNum, 16, new QTableWidgetItem(tr("%1")     .arg(frame.m_select.m_pressed)));
+		m_ui.tableWidget->setItem(frameNum, 17, new QTableWidgetItem(tr("%1")     .arg(frame.m_start.m_pressed)));
 		frameNum++;
 	}
 }
