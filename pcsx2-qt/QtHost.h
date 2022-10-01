ea19514b53e40a51e126045989d28e399280d3e0
Connor McLaughlin <stenzek@gmail.com>
2022-10-02 01:14:39 +1000

    Qt: Fix main window hiding with EFS bug

--- pcsx2-qt/QtHost.h
+++ pcsx2-qt/QtHost.h
@@ -67,6 +67,7 @@ public:
 	__fi bool isRunningFullscreenUI() const { return m_run_fullscreen_ui; }
 
 	bool isOnEmuThread() const;
+	bool shouldRenderToMain() const;
 
 	/// Called back from the GS thread when the display state changes (e.g. fullscreen, render to main).
 	bool acquireHostDisplay(HostDisplay::RenderAPI api);
@@ -166,7 +167,6 @@ private:
 
 	void destroyVM();
 	void executeVM();
-	bool shouldRenderToMain() const;
 
 	void createBackgroundControllerPollTimer();
 	void destroyBackgroundControllerPollTimer();
