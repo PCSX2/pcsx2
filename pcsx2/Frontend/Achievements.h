49b172c9f3e64ce39dea53ee60e996d14de76c85
Connor McLaughlin <stenzek@gmail.com>
2022-10-06 19:59:51 +1000

    FullscreenUI: Display primed achievement list in pause menu

--- pcsx2/Frontend/Achievements.h
+++ pcsx2/Frontend/Achievements.h
@@ -146,7 +146,8 @@ namespace Achievements
 	const Achievement* GetAchievementByID(u32 id);
 	std::pair<u32, u32> GetAchievementProgress(const Achievement& achievement);
 	std::string GetAchievementProgressText(const Achievement& achievement);
-	const std::string& GetAchievementBadgePath(const Achievement& achievement, bool download_if_missing = true);
+	const std::string& GetAchievementBadgePath(
+		const Achievement& achievement, bool download_if_missing = true, bool force_unlocked_icon = false);
 	std::string GetAchievementBadgeURL(const Achievement& achievement);
 	u32 GetPrimedAchievementCount();
 
