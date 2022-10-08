f0ebe4c3f5421c4a263686b058a584d2b1936548
TellowKrinkle <tellowkrinkle@gmail.com>
2022-10-08 01:05:24 -0500

    GHActions:macOS: Remove unused dependencies

--- .github/workflows/scripts/macos/build-dependencies.sh
+++ .github/workflows/scripts/macos/build-dependencies.sh
@@ -12,8 +12,6 @@ NPROCS="$(getconf _NPROCESSORS_ONLN)"
 SDL=SDL2-2.24.0
 PNG=1.6.37
 JPG=9e
-SAMPLERATE=libsamplerate-0.1.9
-PORTAUDIO=pa_stable_v190700_20210406
 SOUNDTOUCH=soundtouch-2.3.1
 WXWIDGETS=3.1.6
 QT=6.3.1
@@ -30,8 +28,6 @@ cat > SHASUMS <<EOF
 91e4c34b1768f92d399b078e171448c6af18cafda743987ed2064a28954d6d97  $SDL.tar.gz
 505e70834d35383537b6491e7ae8641f1a4bed1876dbfe361201fc80868d88ca  libpng-$PNG.tar.xz
 4077d6a6a75aeb01884f708919d25934c93305e49f7e3f36db9129320e6f4f3d  jpegsrc.v$JPG.tar.gz
-0a7eb168e2f21353fb6d84da152e4512126f7dc48ccb0be80578c565413444c1  $SAMPLERATE.tar.gz
-47efbf42c77c19a05d22e627d42873e991ec0c1357219c0d74ce6a2948cb2def  $PORTAUDIO.tgz
 6900996607258496ce126924a19fe9d598af9d892cf3f33d1e4daaa9b42ae0b1  $SOUNDTOUCH.tar.gz
 4980e86c6494adcd527a41fc0a4e436777ba41d1893717d7b7176c59c2061c25  wxWidgets-$WXWIDGETS.tar.bz2
 0a64421d9c2469c2c48490a032ab91d547017c9cc171f3f8070bc31888f24e03  qtbase-everywhere-src-$QT.tar.xz
@@ -44,8 +40,6 @@ curl -L \
 	-O "https://libsdl.org/release/$SDL.tar.gz" \
 	-O "https://downloads.sourceforge.net/project/libpng/libpng16/$PNG/libpng-$PNG.tar.xz" \
 	-O "https://www.ijg.org/files/jpegsrc.v$JPG.tar.gz" \
-	-O "http://www.mega-nerd.com/SRC/$SAMPLERATE.tar.gz" \
-	-O "http://files.portaudio.com/archives/$PORTAUDIO.tgz" \
 	-O "https://www.surina.net/soundtouch/$SOUNDTOUCH.tar.gz" \
 	-O "https://github.com/wxWidgets/wxWidgets/releases/download/v$WXWIDGETS/wxWidgets-$WXWIDGETS.tar.bz2" \
 	-O "https://download.qt.io/official_releases/qt/${QT%.*}/$QT/submodules/qtbase-everywhere-src-$QT.tar.xz" \
@@ -75,23 +69,6 @@ echo "Installing libjpeg..."
 tar xf "jpegsrc.v$JPG.tar.gz"
 cd "jpeg-$JPG"
 ./configure --prefix "$INSTALLDIR" --disable-dependency-tracking
-make "-j$NPROCS"
-make install
-cd ..
-
-echo "Installing libsamplerate..."
-tar xf "$SAMPLERATE.tar.gz"
-cd "$SAMPLERATE"
-sed -i "" "s/Carbon.h/Carbon\\/Carbon.h/" examples/audio_out.c
-./configure --prefix "$INSTALLDIR" --disable-dependency-tracking --disable-sndfile
-make "-j$NPROCS"
-make install
-cd ..
-
-echo "Installing portaudio..."
-tar xf "$PORTAUDIO.tgz"
-cd portaudio
-./configure --prefix "$INSTALLDIR" --enable-mac-universal=no
 make "-j$NPROCS"
 make install
 cd ..
