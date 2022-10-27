62a64f5042dae5bbdffedb301ed585c54da6adac
Ganael Laplanche <ganael.laplanche@martymac.org>
2022-10-27 16:56:17 +0200

    build: Fix build on FreeBSD
    - ftruncate64() does not exist on FreeBSD
    - add missing environ(7) declaration

--- common/Linux/LnxMisc.cpp
+++ common/Linux/LnxMisc.cpp
@@ -72,6 +72,8 @@ std::string GetOSVersionString()
 
 static bool SetScreensaverInhibitX11(const WindowInfo& wi, bool inhibit)
 {
+	extern char **environ;
+
 	const char* command = "xdg-screensaver";
 	const char* operation = inhibit ? "suspend" : "resume";
 	std::string id = fmt::format("0x{:X}", static_cast<u64>(reinterpret_cast<uintptr_t>(wi.window_handle)));
