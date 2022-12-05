8925da94e1a2e66965469d18d65c75463b5daf5c
TellowKrinkle <tellowkrinkle@gmail.com>
2022-12-05 00:55:17 -0600

    Updater: MacOS support

--- pcsx2-qt/AutoUpdaterDialog.h
+++ pcsx2-qt/AutoUpdaterDialog.h
@@ -22,6 +22,7 @@
 
 class QNetworkAccessManager;
 class QNetworkReply;
+class QProgressDialog;
 
 class AutoUpdaterDialog final : public QDialog
 {
@@ -58,13 +59,9 @@ private:
 	void checkIfUpdateNeeded();
 	QString getCurrentUpdateTag() const;
 
+	bool processUpdate(const QByteArray& update_data, QProgressDialog& progress);
 #if defined(_WIN32)
-	bool processUpdate(const QByteArray& update_data);
 	bool doUpdate(const QString& zip_path, const QString& updater_path, const QString& destination_path);
-#elif defined(__linux__)
-	bool processUpdate(const QByteArray& update_data);
-#else
-	bool processUpdate(const QByteArray& update_data);
 #endif
 
 	Ui::AutoUpdaterDialog m_ui;
