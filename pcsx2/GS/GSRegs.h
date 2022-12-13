227c2563c5bfac9eeaa3a442b9af8455ba6abff8
refractionpcsx2 <refraction@gmail.com>
2022-12-13 20:46:16 +0000

    GS-HW: Disable texture mapping if not required

--- pcsx2/GS/GSRegs.h
+++ pcsx2/GS/GSRegs.h
@@ -540,7 +540,7 @@ REG_END2
 	__forceinline bool IsOpaque() const { return ((A == B || (C == 2 && FIX == 0)) && D == 0) || (A == 0 && B == D && C == 2 && FIX == 0x80) || (C == 2 && D != 1 && FIX == 0x00); }
 	__forceinline bool IsOpaque(int amin, int amax) const { return ((A == B || amax == 0) && D == 0) || (A == 0 && B == D && amin == 0x80 && amax == 0x80) || (C == 2 && D != 1 && FIX == 0x00); }
 	__forceinline bool IsCd() { return (A == B) && (D == 1); }
-	__forceinline bool IsBlack() { return ((C == 2 && FIX == 0) || (A == 2 && A == B)) && D == 2; }
+	__forceinline bool IsBlack() const { return ((C == 2 && FIX == 0) || (A == 2 && A == B)) && D == 2; }
 REG_END2
 
 REG64_(GIFReg, BITBLTBUF)
