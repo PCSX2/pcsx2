a42143690c6e05db901f4074bd53556430ca51ce
Stenzek <stenzek@gmail.com>
2022-12-25 22:47:05 +1000

    USB: Only update when devices plugged

--- pcsx2/USB/USB.cpp
+++ pcsx2/USB/USB.cpp
@@ -490,6 +490,9 @@ bool USB::DoState(StateWrapper& sw)
 
 void USBasync(u32 cycles)
 {
+	if (!s_usb_device[0] && !s_usb_device[1])
+		return;
+
 	s_usb_remaining += cycles;
 	s_usb_clocks += s_usb_remaining;
 	if (s_qemu_ohci->eof_timer > 0)
