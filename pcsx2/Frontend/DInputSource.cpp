c21df3b7f0920e9acba831be6dad7d0445799938
Florin9doi <Florin9doi@users.noreply.github.com>
2022-12-26 02:30:15 +0200

    Pad: DInput: Fix incorrect Dpad labels

--- pcsx2/Frontend/DInputSource.cpp
+++ pcsx2/Frontend/DInputSource.cpp
@@ -65,7 +65,7 @@ std::string DInputSource::GetDeviceIdentifier(u32 index)
 	return fmt::format("DInput-{}", index);
 }
 
-static constexpr std::array<const char*, DInputSource::NUM_HAT_DIRECTIONS> s_hat_directions = {{"Up", "Right", "Down", "Left"}};
+static constexpr std::array<const char*, DInputSource::NUM_HAT_DIRECTIONS> s_hat_directions = {{"Up", "Down", "Left", "Right"}};
 
 bool DInputSource::Initialize(SettingsInterface& si, std::unique_lock<std::mutex>& settings_lock)
 {
