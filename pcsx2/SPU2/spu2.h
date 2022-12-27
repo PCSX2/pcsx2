89dd34857af35126d701e5f9b936702d740b7895
Stenzek <stenzek@gmail.com>
2022-12-27 23:39:29 +1000

    SPU2: Remove some now-unused functions

--- pcsx2/SPU2/spu2.h
+++ pcsx2/SPU2/spu2.h
@@ -44,8 +44,6 @@ void SPU2endRecording();
 void SPU2async(u32 cycles);
 s32 SPU2freeze(FreezeAction mode, freezeData* data);
 
-void SPU2setSettingsDir(const char* dir);
-void SPU2setLogDir(const char* dir);
 void SPU2readDMA4Mem(u16* pMem, u32 size);
 void SPU2writeDMA4Mem(u16* pMem, u32 size);
 void SPU2interruptDMA4();
