2bc2891c731e46df7e555e8429320da6d104f389
Stenzek <stenzek@gmail.com>
2022-12-30 17:10:57 +1000

    CommonHotkeys: Add volume control hotkeys

--- pcsx2/SPU2/spu2.h
+++ pcsx2/SPU2/spu2.h
@@ -37,6 +37,9 @@ void Reset(bool psxmode);
 /// Identifies any configuration changes and applies them.
 void CheckForConfigChanges(const Pcsx2Config& old_config);
 
+/// Returns the current output volume, irrespective of the configuration.
+s32 GetOutputVolume();
+
 /// Directly updates the output volume without going through the configuration.
 void SetOutputVolume(s32 volume);
 
