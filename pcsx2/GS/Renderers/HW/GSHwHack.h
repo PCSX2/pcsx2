9e301e1aeca12afd5c1a30ef932059cc51e88abd
Stenzek <stenzek@gmail.com>
2023-01-05 18:54:50 +1000

    GS/HW: CRC hack for Barnyard

--- pcsx2/GS/Renderers/HW/GSHwHack.h
+++ pcsx2/GS/Renderers/HW/GSHwHack.h
@@ -55,6 +55,7 @@ public:
 	static bool GSC_RedDeadRevolver(GSRendererHW& r, const GSFrameInfo& fi, int& skip);
 	static bool GSC_ShinOnimusha(GSRendererHW& r, const GSFrameInfo& fi, int& skip);
 	static bool GSC_XenosagaE3(GSRendererHW& r, const GSFrameInfo& fi, int& skip);
+	static bool GSC_Barnyard(GSRendererHW& r, const GSFrameInfo& fi, int& skip);
 
 	static bool OI_PointListPalette(GSRendererHW& r, GSTexture* rt, GSTexture* ds, GSTextureCache::Source* t);
 	static bool OI_BigMuthaTruckers(GSRendererHW& r, GSTexture* rt, GSTexture* ds, GSTextureCache::Source* t);
