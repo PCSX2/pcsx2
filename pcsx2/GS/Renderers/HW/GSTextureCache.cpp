e449ad74721762948197b728865d88904445728a
Stenzek <stenzek@gmail.com>
2023-01-11 22:01:40 +1000

    GS/TextureCache: Check PSM matches before expanding target
    Mercenaries is doing a 128x4095 write in PSMT4, which when upscaling,
    goes beyond the limits of most GPUs.

--- pcsx2/GS/Renderers/HW/GSTextureCache.cpp
+++ pcsx2/GS/Renderers/HW/GSTextureCache.cpp
@@ -781,7 +781,7 @@ void GSTextureCache::ExpandTarget(const GIFRegBITBLTBUF& BITBLTBUF, const GSVect
 	{
 		Target* t = *i;
 
-		if (TEX0.TBP0 == t->m_TEX0.TBP0 && t->Overlaps(TEX0.TBP0, TEX0.TBW, TEX0.PSM, r))
+		if (TEX0.TBP0 == t->m_TEX0.TBP0 && TEX0.PSM == t->m_TEX0.PSM && t->Overlaps(TEX0.TBP0, TEX0.TBW, TEX0.PSM, r))
 		{
 			list.MoveFront(i.Index());
 
