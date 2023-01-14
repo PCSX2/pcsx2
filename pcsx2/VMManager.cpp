432fd4109d02052e5ffa5273725d221021a74860
pgert <pgert@users.noreply.github.com>
2023-01-14 22:12:36 +0000

    Correct Error-messages: "SSE4.1" not "SSE4"

--- pcsx2/VMManager.cpp
+++ pcsx2/VMManager.cpp
@@ -156,8 +156,8 @@ bool VMManager::PerformEarlyHardwareChecks(const char** error)
 
 	if (!temp_x86_caps.hasStreamingSIMD4Extensions)
 	{
-		*error = "PCSX2 requires the Streaming SIMD 4 Extensions instruction set, which your CPU does not support.\n\n"
-				 "SSE4 is now a minimum requirement for PCSX2. You should either upgrade your CPU, or use an older build such as 1.6.0.\n\n" COMMON_DOWNLOAD_MESSAGE;
+		*error = "PCSX2 requires the Streaming SIMD 4.1 Extensions instruction set, which your CPU does not support.\n\n"
+				 "SSE4.1 is now a minimum requirement for PCSX2. You should either upgrade your CPU, or use an older build such as 1.6.0.\n\n" COMMON_DOWNLOAD_MESSAGE;
 		return false;
 	}
 
@@ -165,7 +165,7 @@ bool VMManager::PerformEarlyHardwareChecks(const char** error)
 	if (!temp_x86_caps.hasAVX || !temp_x86_caps.hasAVX2)
 	{
 		*error = "This build of PCSX2 requires the Advanced Vector Extensions 2 instruction set, which your CPU does not support.\n\n"
-				 "You should download and run the SSE4 build of PCSX2 instead, or upgrade to a CPU that supports AVX2 to use this build.\n\n" COMMON_DOWNLOAD_MESSAGE;
+				 "You should download and run the SSE4.1 build of PCSX2 instead, or upgrade to a CPU that supports AVX2 to use this build.\n\n" COMMON_DOWNLOAD_MESSAGE;
 		return false;
 	}
 #endif
