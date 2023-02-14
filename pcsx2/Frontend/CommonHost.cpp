f09662778e443e3cab9e081672b602e197cd619d
Ganael Laplanche <ganael.laplanche@martymac.org>
2023-02-14 22:32:01 +0100

    Fix config dir location on FreeBSD

--- pcsx2/Frontend/CommonHost.cpp
+++ pcsx2/Frontend/CommonHost.cpp
@@ -160,7 +160,7 @@ void CommonHost::SetDataDirectory()
 			EmuFolders::DataRoot = Path::Combine(StringUtil::WideStringToUTF8String(documents_directory), "PCSX2");
 		CoTaskMemFree(documents_directory);
 	}
-#elif defined(__linux__)
+#elif defined(__linux__) || defined(__FreeBSD__)
 	// Use $XDG_CONFIG_HOME/PCSX2 if it exists.
 	const char* xdg_config_home = getenv("XDG_CONFIG_HOME");
 	if (xdg_config_home && Path::IsAbsolute(xdg_config_home))
