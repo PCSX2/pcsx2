8590e390a25b5eee5cff8c1760924b1962500afd
Stenzek <stenzek@gmail.com>
2023-04-15 15:10:44 +1000

    GS/Vulkan: Don't enable geometryShader

--- common/Vulkan/Context.cpp
+++ common/Vulkan/Context.cpp
@@ -490,7 +490,6 @@ namespace Vulkan
 
 		// Enable the features we use.
 		m_device_features.dualSrcBlend = available_features.dualSrcBlend;
-		m_device_features.geometryShader = available_features.geometryShader;
 		m_device_features.largePoints = available_features.largePoints;
 		m_device_features.wideLines = available_features.wideLines;
 		m_device_features.fragmentStoresAndAtomics = available_features.fragmentStoresAndAtomics;
