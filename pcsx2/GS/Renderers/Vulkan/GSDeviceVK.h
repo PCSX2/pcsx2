c57d8980a1bc141f0cbf5df5d911f89051cc924b
Stenzek <stenzek@gmail.com>
2023-05-05 21:47:41 +1000

    GS: Move Vulkan helpers from common to GS

--- pcsx2/GS/Renderers/Vulkan/GSDeviceVK.h
+++ pcsx2/GS/Renderers/Vulkan/GSDeviceVK.h
@@ -15,18 +15,20 @@
 
 #pragma once
 
-#include "GSTextureVK.h"
-#include "GS/GSVector.h"
 #include "GS/Renderers/Common/GSDevice.h"
-#include "common/Vulkan/StreamBuffer.h"
+#include "GS/Renderers/Vulkan/GSTextureVK.h"
+#include "GS/Renderers/Vulkan/VKLoader.h"
+#include "GS/Renderers/Vulkan/VKStreamBuffer.h"
+#include "GS/GSVector.h"
+
 #include "common/HashCombine.h"
-#include "vk_mem_alloc.h"
+
 #include <array>
 #include <unordered_map>
 
 namespace Vulkan
 {
-class SwapChain;
+class VKSwapChain;
 }
 
 class GSDeviceVK final : public GSDevice
@@ -109,7 +111,7 @@ public:
 	};
 
 private:
-	std::unique_ptr<Vulkan::SwapChain> m_swap_chain;
+	std::unique_ptr<VKSwapChain> m_swap_chain;
 
 	VkDescriptorSetLayout m_utility_ds_layout = VK_NULL_HANDLE;
 	VkPipelineLayout m_utility_pipeline_layout = VK_NULL_HANDLE;
@@ -119,10 +121,10 @@ private:
 	VkDescriptorSetLayout m_tfx_rt_texture_ds_layout = VK_NULL_HANDLE;
 	VkPipelineLayout m_tfx_pipeline_layout = VK_NULL_HANDLE;
 
-	Vulkan::StreamBuffer m_vertex_stream_buffer;
-	Vulkan::StreamBuffer m_index_stream_buffer;
-	Vulkan::StreamBuffer m_vertex_uniform_stream_buffer;
-	Vulkan::StreamBuffer m_fragment_uniform_stream_buffer;
+	VKStreamBuffer m_vertex_stream_buffer;
+	VKStreamBuffer m_index_stream_buffer;
+	VKStreamBuffer m_vertex_uniform_stream_buffer;
+	VKStreamBuffer m_fragment_uniform_stream_buffer;
 	VkBuffer m_expand_index_buffer = VK_NULL_HANDLE;
 	VmaAllocation m_expand_index_buffer_allocation = VK_NULL_HANDLE;
 
@@ -410,20 +412,20 @@ private:
 	GSVector4i m_scissor = GSVector4i::zero();
 	u8 m_blend_constant_color = 0;
 
-	std::array<const Vulkan::Texture*, NUM_TFX_TEXTURES> m_tfx_textures{};
+	std::array<const VKTexture*, NUM_TFX_TEXTURES> m_tfx_textures{};
 	VkSampler m_tfx_sampler = VK_NULL_HANDLE;
 	u32 m_tfx_sampler_sel = 0;
 	std::array<VkDescriptorSet, NUM_TFX_DESCRIPTOR_SETS> m_tfx_descriptor_sets{};
 	std::array<u32, NUM_TFX_DYNAMIC_OFFSETS> m_tfx_dynamic_offsets{};
 
-	const Vulkan::Texture* m_utility_texture = nullptr;
+	const VKTexture* m_utility_texture = nullptr;
 	VkSampler m_utility_sampler = VK_NULL_HANDLE;
 	VkDescriptorSet m_utility_descriptor_set = VK_NULL_HANDLE;
 
 	PipelineLayout m_current_pipeline_layout = PipelineLayout::Undefined;
 	VkPipeline m_current_pipeline = VK_NULL_HANDLE;
 
-	Vulkan::Texture m_null_texture;
+	VKTexture m_null_texture;
 
 	// current pipeline selector - we save this in the struct to avoid re-zeroing it every draw
 	PipelineSelector m_pipeline_selector = {};
--- pcsx2/GS/Renderers/Vulkan/vk_mem_alloc.cpp
+++ pcsx2/GS/Renderers/Vulkan/vk_mem_alloc.cpp
@@ -1,5 +1,5 @@
 /*  PCSX2 - PS2 Emulator for PCs
- *  Copyright (C) 2002-2021 PCSX2 Dev Team
+ *  Copyright (C) 2002-2023 PCSX2 Dev Team
  *
  *  PCSX2 is free software: you can redistribute it and/or modify it under the terms
  *  of the GNU Lesser General Public License as published by the Free Software Found-
@@ -13,8 +13,8 @@
  *  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "common/PrecompiledHeader.h"
+#include "PrecompiledHeader.h"
 
 #define VMA_IMPLEMENTATION 1
 
-#include "common/Vulkan/Loader.h"
+#include "GS/Renderers/Vulkan/VKLoader.h"
