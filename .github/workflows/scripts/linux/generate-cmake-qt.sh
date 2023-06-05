089f220c069f33f7f33e27f3658e876f951650e9
Stenzek <stenzek@gmail.com>
2023-06-05 23:38:37 +1000

    CI/Linux: Allow path to clang and build deps to be set at build time

--- .github/workflows/scripts/linux/generate-cmake-qt.sh
+++ .github/workflows/scripts/linux/generate-cmake-qt.sh
@@ -2,16 +2,28 @@
 
 set -e
 
+if [[ -z "${DEPS_PREFIX}" ]]; then
+  echo "DEPS_PREFIX is not set."
+  exit 1
+fi
+
+echo "Using build dependencies from: ${DEPS_PREFIX}"
+
 if [ "${COMPILER}" = "clang" ]; then
+  if [[ -z "${CLANG_PATH}" ]] || [[ -z "${CLANGXX_PATH}" ]]; then
+    echo "CLANG_PATH or CLANGXX_PATH is not set."
+    exit 1
+  fi
+
 	echo "Using clang toolchain"
-	cat > "$HOME/clang-toolchain.cmake" << EOF
-set(CMAKE_C_COMPILER /usr/bin/clang-12)
-set(CMAKE_CXX_COMPILER /usr/bin/clang++-12)
+	cat > "clang-toolchain.cmake" << EOF
+set(CMAKE_C_COMPILER "${CLANG_PATH}")
+set(CMAKE_CXX_COMPILER "${CLANGXX_PATH}")
 set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld")
 set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=lld")
 set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld")
 EOF
-	ADDITIONAL_CMAKE_ARGS="$ADDITIONAL_CMAKE_ARGS -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON -DCMAKE_TOOLCHAIN_FILE=$HOME/clang-toolchain.cmake"
+	ADDITIONAL_CMAKE_ARGS="$ADDITIONAL_CMAKE_ARGS -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON -DCMAKE_TOOLCHAIN_FILE=clang-toolchain.cmake"
 fi
 
 echo "Additional CMake Args - ${ADDITIONAL_CMAKE_ARGS}"
@@ -31,7 +43,7 @@ cmake \
 	-DX11_API=ON \
 	-DWAYLAND_API=ON \
 	-DENABLE_SETCAP=OFF \
-	-DCMAKE_PREFIX_PATH="$HOME/deps" \
+	-DCMAKE_PREFIX_PATH="${DEPS_PREFIX}" \
 	-DUSE_SYSTEM_SDL2=ON \
 	-DUSE_SYSTEM_ZSTD=OFF \
 	-DDISABLE_ADVANCE_SIMD=TRUE
