db642b05c009693e7c23320758259d08879cdcb4
Stenzek <stenzek@gmail.com>
2023-06-30 00:55:39 +1000

    CI/Linux: Add manifest to AppStream XML

--- .github/workflows/scripts/linux/generate-metainfo.sh
+++ .github/workflows/scripts/linux/generate-metainfo.sh
@@ -10,6 +10,7 @@ fi
 OUTFILE=$1
 GIT_DATE=$(git log -1 --pretty=%cd --date=short)
 GIT_VERSION=$(git tag --points-at HEAD)
+GIT_HASH=$(git rev-parse HEAD)
 
 if [[ "${GIT_VERSION}" == "" ]]; then
     GIT_VERSION=$(git rev-parse HEAD)
@@ -17,8 +18,11 @@ fi
 
 echo "GIT_DATE: ${GIT_DATE}"
 echo "GIT_VERSION: ${GIT_VERSION}"
+echo "GIT_HASH: ${GIT_HASH}"
 
 cp "${SCRIPTDIR}"/pcsx2-qt.metainfo.xml.in "${OUTFILE}"
 
 sed -i -e "s/@GIT_VERSION@/${GIT_VERSION}/" "${OUTFILE}"
 sed -i -e "s/@GIT_DATE@/${GIT_DATE}/" "${OUTFILE}"
+sed -i -e "s/@GIT_HASH@/${GIT_HASH}/" "${OUTFILE}"
+
