d46b8390f1f8c68efe6219eafae8aa88a226dbd5
TellowKrinkle <tellowkrinkle@gmail.com>
2023-07-16 19:21:17 -0500

    Qt:Mac: Add Qt mac application menu strings
    The official Qt translations don't match the rest of macOS for some reason

--- pcsx2-qt/Translations.cpp
+++ pcsx2-qt/Translations.cpp
@@ -39,6 +39,17 @@
 #include <ShlObj.h>
 #endif
 
+#if 0
+// Qt internal strings we'd like to have translated
+QT_TRANSLATE_NOOP("MAC_APPLICATION_MENU", "Services")
+QT_TRANSLATE_NOOP("MAC_APPLICATION_MENU", "Hide %1")
+QT_TRANSLATE_NOOP("MAC_APPLICATION_MENU", "Hide Others")
+QT_TRANSLATE_NOOP("MAC_APPLICATION_MENU", "Show All")
+QT_TRANSLATE_NOOP("MAC_APPLICATION_MENU", "Preferences...")
+QT_TRANSLATE_NOOP("MAC_APPLICATION_MENU", "Quit %1")
+QT_TRANSLATE_NOOP("MAC_APPLICATION_MENU", "About %1")
+#endif
+
 namespace QtHost
 {
 	struct GlyphInfo
