460a2dbbd3a0413d71f5ab1e6609f801a839ffa1
Stenzek <stenzek@gmail.com>
2023-07-23 15:35:32 +1000

    Qt: Add custom binding widget for GunCon2

--- pcsx2-qt/Settings/ControllerBindingWidgets.cpp
+++ pcsx2-qt/Settings/ControllerBindingWidgets.cpp
@@ -38,6 +38,7 @@
 
 #include "ui_USBBindingWidget_DrivingForce.h"
 #include "ui_USBBindingWidget_GTForce.h"
+#include "ui_USBBindingWidget_GunCon2.h"
 
 ControllerBindingWidget::ControllerBindingWidget(QWidget* parent, ControllerSettingsDialog* dialog, u32 port)
 	: QWidget(parent)
@@ -1274,6 +1275,11 @@ USBBindingWidget* USBBindingWidget::createInstance(
 			Ui::USBBindingWidget_GTForce().setupUi(widget);
 			has_template = true;
 		}
+	}
+	else if (type == "guncon2")
+	{
+		Ui::USBBindingWidget_GunCon2().setupUi(widget);
+		has_template = true;
 	}
 
 	if (has_template)
