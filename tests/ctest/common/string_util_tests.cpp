3c41c286c7804cd0a50ae9cdf8a22825a946db38
Stenzek <stenzek@gmail.com>
2023-09-22 20:52:35 +1000

    StringUtil: Add Ellipsise

--- tests/ctest/common/string_util_tests.cpp
+++ tests/ctest/common/string_util_tests.cpp
@@ -113,3 +113,32 @@ TEST(StringUtil, FromCharsIsLocaleIndependent)
 }
 
 #endif
+
+TEST(StringUtil, Ellipsise)
+{
+	ASSERT_EQ(StringUtil::Ellipsise("HelloWorld", 6, "..."), "Hel...");
+	ASSERT_EQ(StringUtil::Ellipsise("HelloWorld", 7, ".."), "Hello..");
+	ASSERT_EQ(StringUtil::Ellipsise("HelloWorld", 20, ".."), "HelloWorld");
+	ASSERT_EQ(StringUtil::Ellipsise("", 20, "..."), "");
+	ASSERT_EQ(StringUtil::Ellipsise("Hello", 10, "..."), "Hello");
+}
+
+TEST(StringUtil, EllipsiseInPlace)
+{
+	std::string s;
+	s = "HelloWorld";
+	StringUtil::EllipsiseInPlace(s, 6, "...");
+	ASSERT_EQ(s, "Hel...");
+	s = "HelloWorld";
+	StringUtil::EllipsiseInPlace(s, 7, "..");
+	ASSERT_EQ(s, "Hello..");
+	s = "HelloWorld";
+	StringUtil::EllipsiseInPlace(s, 20, "..");
+	ASSERT_EQ(s, "HelloWorld");
+	s = "";
+	StringUtil::EllipsiseInPlace(s, 20, "...");
+	ASSERT_EQ(s, "");
+	s = "Hello";
+	StringUtil::EllipsiseInPlace(s, 10, "...");
+	ASSERT_EQ(s, "Hello");
+}
