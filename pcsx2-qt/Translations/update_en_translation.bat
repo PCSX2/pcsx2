875e3af7046916975c27ded47d54cf2df094d44b
Stenzek <stenzek@gmail.com>
2023-10-02 15:01:05 +1000

    Qt: Fix update_en_translation.bat script

--- pcsx2-qt/Translations/update_en_translation.bat
+++ pcsx2-qt/Translations/update_en_translation.bat
@@ -1,19 +1,10 @@
 @echo off
 
-set QTBIN=..\..\3rdparty\qt\6.5.0\msvc2022_64\bin
+set QTBIN=..\..\deps\bin
 set SRCDIRS=../ ../../pcsx2/
 
 set OPTS=-tr-function-alias QT_TRANSLATE_NOOP+=TRANSLATE,QT_TRANSLATE_NOOP+=TRANSLATE_SV,QT_TRANSLATE_NOOP+=TRANSLATE_STR,QT_TRANSLATE_NOOP+=TRANSLATE_FS,QT_TRANSLATE_N_NOOP3+=TRANSLATE_FMT,QT_TRANSLATE_NOOP+=TRANSLATE_NOOP
 
 "%QTBIN%\lupdate.exe" %SRCDIRS% %OPTS% -no-obsolete -source-language en -ts pcsx2-qt_en.ts
 
-echo.
-echo ******************************************************************************************************************
-echo *                                                  PLEASE READ                                                   *
-echo ******************************************************************************************************************
-echo.
-echo Make sure you have deleted the build (x64) directory from pcsx2-qt, otherwise you will have polluted the .ts file.
-echo If you did not, then you should reset/checkout the ts file, delete the build directory, and run this script again.
-echo.
-echo.
 pause
