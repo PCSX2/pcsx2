PCSX2/pcsx2

commit fa5aedc906eba9aa326ae0ddf31da6e11a02d389

GS/HW: Don't convert old depth if being overwritten

pcsx2/GS/Renderers/HW/GSTextureCache.cpp
@@ -1920,7 +1920,7 @@ GSTextureCache::Target* GSTextureCache::LookupTarget(GIFRegTEX0 TEX0, const GSVe
 				dst_match = t;
 				break;
 			}
-			else if (t->m_age == 1)
+			else if (t->m_age == 1 && (preserve_rgb || (preserve_alpha && (dst_match->m_valid_alpha_low || dst_match->m_valid_alpha_high))))
 			{
 				dst_match = t;
 			}

commit 09d15c4bce02ac6a8f822b07bc1eaba21294e35e

GS/HW: Fix incorrect alpha valid check in LookupTarget()
Regression from #10254.

pcsx2/GS/Renderers/HW/GSTextureCache.cpp
@@ -1920,7 +1920,7 @@ GSTextureCache::Target* GSTextureCache::LookupTarget(GIFRegTEX0 TEX0, const GSVe
 				dst_match = t;
 				break;
 			}
-			else if (t->m_age == 1 && (preserve_rgb || (preserve_alpha && (dst_match->m_valid_alpha_low || dst_match->m_valid_alpha_high))))
+			else if (t->m_age == 1 && (preserve_rgb || (preserve_alpha && (t->m_valid_alpha_low || t->m_valid_alpha_high))))
 			{
 				dst_match = t;
 			}