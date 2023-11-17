56fa8745de833ede6a55e6e61e339f471473c02f
cyanea-bt <150618858+cyanea-bt@users.noreply.github.com>
2023-11-18 00:48:28 +0100

    MemoryCardFolder: Fix incorrect save timestamps

--- pcsx2/SIO/Memcard/MemoryCardFolder.h
+++ pcsx2/SIO/Memcard/MemoryCardFolder.h
@@ -26,6 +26,10 @@
 
 //#define DEBUG_WRITE_FOLDER_CARD_IN_MEMORY_TO_FILE_ON_CHANGE
 
+#ifdef _WIN32
+#define timegm _mkgmtime
+#endif
+
 // --------------------------------------------------------------------------------------
 //  Superblock Header Struct
 // --------------------------------------------------------------------------------------
