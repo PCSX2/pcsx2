cddd1c58b6aaf897fbf812f5767141b39d6dfb9e
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2023-12-13 18:58:56 +0100

    format code

--- pcsx2/CDVD/CsoFileReader.h
+++ pcsx2/CDVD/CsoFileReader.h
@@ -57,10 +57,10 @@ private:
 	bool DecompressFrame(Bytef* dst, u32 frame, u32 readBufferSize);
 	bool DecompressFrame(u32 frame, u32 readBufferSize);
 
-	bool m_uselz4 = false; // flag to enable LZ4 decompression (ZSO files)
 	u32 m_frameSize = 0;
 	u8 m_frameShift = 0;
 	u8 m_indexShift = 0;
+	bool m_uselz4 = false; // flag to enable LZ4 decompression (ZSO files)
 	std::unique_ptr<u8[]> m_readBuffer;
 
 	std::unique_ptr<u32[]> m_index;
