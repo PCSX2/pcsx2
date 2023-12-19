061ca482adaae3aa4d903521085aa5341bb2a6ad
refractionpcsx2 <refraction@gmail.com>
2023-12-19 18:48:17 +0000

    MCD: Correctly wait 60 frames for eject timeout

--- pcsx2/SIO/Sio.cpp
+++ pcsx2/SIO/Sio.cpp
@@ -100,7 +100,7 @@ void AutoEject::Set(size_t port, size_t slot)
 {
 	if (mcds[port][slot].autoEjectTicks == 0)
 	{
-		mcds[port][slot].autoEjectTicks = 1; // 1 second is enough.
+		mcds[port][slot].autoEjectTicks = 60; // 60 frames is enough.
 		mcds[port][slot].term = 0x55; // Reset terminator to default (0x55), forces the PS2 to recheck the memcard.
 	}
 }
