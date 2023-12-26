4918b1492125f8bfb16d2d842aa52cdef774cb76
Stenzek <stenzek@gmail.com>
2023-12-26 13:48:39 +1000

    GS/Vulkan: Disable threaded presentation when spin is enabled

--- pcsx2/GS/Renderers/Vulkan/GSDeviceVK.cpp
+++ pcsx2/GS/Renderers/Vulkan/GSDeviceVK.cpp
@@ -1113,7 +1113,9 @@ void GSDeviceVK::SubmitCommandBuffer(
 	if (spin_enabled && m_optional_extensions.vk_ext_calibrated_timestamps)
 		resources.submit_timestamp = GetCPUTimestamp();
 
-	if (!submit_on_thread || !m_present_thread.joinable())
+	// Don't use threaded presentation when spinning is enabled. ScanForCommandBufferCompletion()
+	// calls vkGetFenceStatus(), which reads a fence that has been passed off to the thread.
+	if (!submit_on_thread || GSConfig.HWSpinGPUForReadbacks || !m_present_thread.joinable())
 	{
 		DoSubmitCommandBuffer(m_current_frame, present_swap_chain, spin_cycles);
 		if (present_swap_chain)
