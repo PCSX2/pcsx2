1874e7273b2777c42dd2bab00e9668f481a5990f
Tyler Wilding <xtvaser@gmail.com>
2024-01-05 21:29:28 -0500

    ci: simplify things, just explicitly specify the artifact names in the workflows

--- .github/workflows/scripts/common/name-artifacts.sh
+++ .github/workflows/scripts/common/name-artifacts.sh
@@ -1,67 +1,26 @@
 #!/bin/bash
 
 # Artifact Naming Scheme:
-# PCSX2-<OS>-Qt-[ARCH]-[SIMD]-[pr\[PR_NUM\]]-[title|sha\[SHA|PR_TITLE\]
+# PCSX2-<OS>-Qt-[BUILD_SYSTEM]-[ARCH]-[SIMD]-[pr\[PR_NUM\]]-[title|sha\[SHA|PR_TITLE\]
 # -- limited to 200 chars
 # Outputs:
 # - artifact-name
 
+# Example - PCSX2-linux-Qt-x64-flatpak-sse4-sha[e880a2749]
+
 # Inputs as env-vars
-# OS
-# CMAKE_FLAGS
-# BUILD_CONFIGURATION
-# BUILD_SYSTEM
-# ARCH
-# SIMD
+# PREFIX
 # EVENT_NAME
 # PR_TITLE
 # PR_NUM
 # PR_SHA
 
-NAME=""
-
-if [ "${OS}" == "macos" ]; then
-  # MacOS has combined binaries for x64 and ARM64.
-  NAME="PCSX2-${OS}-Qt"
-elif [[ ("${OS}" == "windows" && "$BUILD_SYSTEM" != "cmake") ]]; then
-  NAME="PCSX2-${OS}-Qt-${ARCH}-${SIMD}"
-else
-  NAME="PCSX2-${OS}-Qt-${ARCH}"
+if [[ -z "${PREFIX}" ]]; then
+  echo "PREFIX is not set, can't name artifact without it!"
+  exit 1
 fi
 
-# Add cmake if used to differentate it from msbuild builds
-# Else the two artifacts will have the same name and the files will be merged
-if [[ ! -z "${BUILD_SYSTEM}" ]]; then
-  if [[ "${BUILD_SYSTEM}" == "cmake" ]] || [[ "${BUILD_SYSTEM}" == "flatpak" ]] || [[ "${BUILD_SYSTEM}" == "appimage" ]]; then
-    NAME="${NAME}-${BUILD_SYSTEM}"
-  fi
-fi
-
-# Isolate artifacts produced with different build systems, otherwise they overwrite each other
-# and you have no idea what you got!
-if [[ ! -z "${BUILD_SYSTEM}" ]]; then
-  # differentiate between clang and msvc
-  # edge case for cmake since cmake presets aren't used, instead flags are manually passed in
-  if [[ "${BUILD_SYSTEM}" == "cmake" ]]; then
-    if [[ "${CMAKE_FLAGS,,}" == *"clang"* ]]; then
-      NAME="${NAME}-clang"
-    else
-        NAME="${NAME}-msvc"
-    fi
-  else
-    if [[ "${BUILD_CONFIGURATION,,}" == *"clang"* ]]; then
-      NAME="${NAME}-clang"
-    else
-        NAME="${NAME}-msvc"
-    fi
-  fi
-  # also differentiate between sse4 and avx2
-  if [[ "${BUILD_CONFIGURATION,,}" == *"avx2"* ]]; then
-      NAME="${NAME}-avx2"
-  else
-      NAME="${NAME}-sse4"
-  fi
-fi
+NAME="${PREFIX}"
 
 # Add PR / Commit Metadata
 if [ "$EVENT_NAME" == "pull_request" ]; then
