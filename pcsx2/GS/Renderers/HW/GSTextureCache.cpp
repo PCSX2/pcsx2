d4856aa9a7c37a6e707ce140e99a63a4f86b6de7
lightningterror <18107717+lightningterror@users.noreply.github.com>
2024-01-18 14:20:47 +0100

    GS/HW: Fix compiler warnings.

--- pcsx2/GS/Renderers/HW/GSTextureCache.cpp
+++ pcsx2/GS/Renderers/HW/GSTextureCache.cpp
@@ -3512,7 +3512,7 @@ bool GSTextureCache::Move(u32 SBP, u32 SBW, u32 SPSM, int sx, int sy, u32 DBP, u
 			return false;
 	}
 
-	if (m_expected_src_bp == SBP && m_expected_dst_bp == DBP)
+	if (m_expected_src_bp == static_cast<int>(SBP) && m_expected_dst_bp == static_cast<int>(DBP))
 	{
 		// Get the new position so we can work out the offset.
 		GSVector4i rect_offset = TranslateAlignedRectByPage(m_remembered_src_bp, m_remembered_src_bp + 1, SBW, SPSM, SBP, SPSM, SBW, GSVector4i(sx, sy, sx + w, sy + h), false);
