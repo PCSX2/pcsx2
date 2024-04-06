78c1d6f545c4fc8cea7ecf9421cb81b8e5094ee5
Stenzek <stenzek@gmail.com>
2024-04-06 17:06:10 +1000

    GS/Vulkan: Swap out glslang for shaderc

--- pcsx2/ShaderCacheVersion.h
+++ pcsx2/ShaderCacheVersion.h
@@ -1,6 +1,6 @@
-// SPDX-FileCopyrightText: 2002-2023 PCSX2 Dev Team
+// SPDX-FileCopyrightText: 2002-2024 PCSX2 Dev Team
 // SPDX-License-Identifier: LGPL-3.0+
 
 /// Version number for GS and other shaders. Increment whenever any of the contents of the
 /// shaders change, to invalidate the cache.
-static constexpr u32 SHADER_CACHE_VERSION = 44;
+static constexpr u32 SHADER_CACHE_VERSION = 45;
