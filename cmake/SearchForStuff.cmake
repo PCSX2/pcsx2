ca8a8376141329351e39aac6d93cdfc070e99c89
Stenzek <stenzek@gmail.com>
2024-04-24 22:28:02 +1000

    3rdparty: Add FreeSurround

--- cmake/SearchForStuff.cmake
+++ cmake/SearchForStuff.cmake
@@ -109,6 +109,7 @@ add_subdirectory(3rdparty/libzip EXCLUDE_FROM_ALL)
 add_subdirectory(3rdparty/rcheevos EXCLUDE_FROM_ALL)
 add_subdirectory(3rdparty/rapidjson EXCLUDE_FROM_ALL)
 add_subdirectory(3rdparty/discord-rpc EXCLUDE_FROM_ALL)
+add_subdirectory(3rdparty/freesurround EXCLUDE_FROM_ALL)
 
 if(USE_OPENGL)
 	add_subdirectory(3rdparty/glad EXCLUDE_FROM_ALL)
