73787b1cccf3ff9938b7557066017ebc3ba8571b
refractionpcsx2 <refraction@gmail.com>
2024-06-10 22:46:10 +0100

    GS/HW: Improve detection of upscaling and downscaling

--- pcsx2/GS/Renderers/HW/GSTextureCache.cpp
+++ pcsx2/GS/Renderers/HW/GSTextureCache.cpp
@@ -2595,7 +2595,7 @@ bool GSTextureCache::PreloadTarget(GIFRegTEX0 TEX0, const GSVector2i& size, cons
 								continue;
 							}
 
-							if (!hw_clear && (preserve_target || preload))
+							if (!hw_clear && (preserve_target || preload) && dst->GetScale() == t->GetScale())
 							{
 								const int copy_width = (t->m_texture->GetWidth()) > (dst->m_texture->GetWidth()) ? (dst->m_texture->GetWidth()) : t->m_texture->GetWidth();
 								const int copy_height = overlapping_pages_height * t->m_scale;
