06ff07f0eff78ca15b9bcecaff6cfff017a55609
TheTechnician27 <TheTechnician27@users.noreply.github.com>
2024-07-15 20:42:34 -0500

    Qt: Add Emerald theme and fix palette var names

--- pcsx2-qt/Settings/InterfaceSettingsWidget.cpp
+++ pcsx2-qt/Settings/InterfaceSettingsWidget.cpp
@@ -39,6 +39,8 @@ const char* InterfaceSettingsWidget::THEME_NAMES[] = {
 	QT_TRANSLATE_NOOP("InterfaceSettingsWidget", "Ruby (Black/Red) [Dark]"),
 	//: Ignore what Crowdin says in this string about "[Light]/[Dark]" being untouchable here, these are not variables in this case and must be translated.
 	QT_TRANSLATE_NOOP("InterfaceSettingsWidget", "Sapphire (Black/Blue) [Dark]"),
+	//: Ignore what Crowdin says in this string about "[Light]/[Dark]" being untouchable here, these are not variables in this case and must be translated.
+	QT_TRANSLATE_NOOP("InterfaceSettingsWidget", "Emerald (Black/Green) [Dark]"),
 	//: "Custom.qss" must be kept as-is.
 	QT_TRANSLATE_NOOP("InterfaceSettingsWidget", "Custom.qss [Drop in PCSX2 Folder]"),
 	nullptr};
@@ -61,6 +63,7 @@ const char* InterfaceSettingsWidget::THEME_VALUES[] = {
 	"CobaltSky",
 	"Ruby",
 	"Sapphire",
+	"Emerald",
 	"Custom",
 	nullptr};
 
