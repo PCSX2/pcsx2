d2acb45d58bb3d6274021028a2889417f9ca1788
KamFretoZ <14798312+kamfretoz@users.noreply.github.com>
2024-07-19 21:07:36 +0700

    OSD: Add PCSX2 Version toggle

--- pcsx2/ImGui/ImGuiOverlays.cpp
+++ pcsx2/ImGui/ImGuiOverlays.cpp
@@ -24,6 +24,7 @@
 #include "SIO/Pad/PadBase.h"
 #include "USB/USB.h"
 #include "VMManager.h"
+#include "svnrev.h"
 
 #include "common/BitUtils.h"
 #include "common/FileSystem.h"
@@ -121,6 +122,7 @@ __ri void ImGuiManager::DrawPerformanceOverlay(float& position_y, float scale, f
 	{
 		bool first = true;
 		const float speed = PerformanceMetrics::GetSpeed();
+
 		if (GSConfig.OsdShowFPS)
 		{
 			switch (PerformanceMetrics::GetInternalFPSMethod())
@@ -152,6 +154,16 @@ __ri void ImGuiManager::DrawPerformanceOverlay(float& position_y, float scale, f
 			else
 				text.append_format(" ({:.0f}%)", target_speed * 100.0f);
 		}
+
+		if (GSConfig.OsdShowVersion)
+		{
+			if (GSConfig.OsdShowFPS || GSConfig.OsdShowSpeed)
+			{
+				text.append_format(" | ");
+			}
+			text.append_format("PCSX2 {}", GIT_REV);
+		}
+
 		if (!text.empty())
 		{
 			ImU32 color;
