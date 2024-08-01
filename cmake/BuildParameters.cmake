bc6033d1a5efbc983ffbecb0082f0285a031b992
TheLastRar <TheLastRar@users.noreply.github.com>
2024-08-01 14:41:48 +0100

    Build: Set Page/Cache line sizes on Windows ARM

--- cmake/BuildParameters.cmake
+++ cmake/BuildParameters.cmake
@@ -115,6 +115,13 @@ elseif("${CMAKE_HOST_SYSTEM_PROCESSOR}" STREQUAL "arm64" OR "${CMAKE_HOST_SYSTEM
 		detect_cache_line_size()
 		list(APPEND PCSX2_DEFS OVERRIDE_HOST_CACHE_LINE_SIZE=${HOST_CACHE_LINE_SIZE})
 	endif()
+	
+	# Windows page/cache line size seems to match x68-64 
+	if(WIN32)
+		list(APPEND PCSX2_DEFS OVERRIDE_HOST_PAGE_SIZE=0x1000)
+		# Value of std::hardware_destructive_interference_size for ARM64 on MSVC toolset 14.40.33807
+		list(APPEND PCSX2_DEFS OVERRIDE_HOST_CACHE_LINE_SIZE=64)
+	endif()
 else()
 	message(FATAL_ERROR "Unsupported architecture: ${CMAKE_HOST_SYSTEM_PROCESSOR}")
 endif()
