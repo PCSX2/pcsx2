68678ac4ec5b92963dabdbf0ed69eb1fbc0be959
KamFretoZ <14798312+kamfretoz@users.noreply.github.com>
2024-10-02 13:23:00 +0700

    CI: Add other translations related scripts script to the daily workflow
    [ci skip]

--- .github/workflows/scripts/common/update_base_translation.sh
+++ .github/workflows/scripts/common/update_base_translation.sh
@@ -6,6 +6,9 @@ set -e
 
 # While we use custom Qt builds for our releases, the Qt6 package will be good enough
 # for just updating translations. Saves building it for this action alone.
-"$SCRIPTDIR/../../../../tools/retry.sh" sudo apt-get -y install qt6-l10n-tools
+"$SCRIPTDIR/../../../../tools/retry.sh" sudo apt-get -y install qt6-l10n-tools python3  
 
 PATH=/usr/lib/qt6/bin:$PATH "$SCRIPTDIR/../../../../pcsx2-qt/Translations/update_base_translation.sh"
+PATH=/usr/bin/python3:$PATH "$SCRIPTDIR/../../../../pcsx2-qt/Translations/update_glyph_ranges.py"
+PATH=/usr/bin/python3:$PATH "$SCRIPTDIR/../../../../tools/generate_fullscreen_ui_translation_strings.py"
+PATH=/usr/bin/python3:$PATH "$SCRIPTDIR/../../../../tools/generate_update_fa_glyph_ranges.py"
