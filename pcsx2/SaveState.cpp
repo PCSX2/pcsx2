93652a31158a774f8e35e2f6d51ead0905b008aa
TheLastRar <TheLastRar@users.noreply.github.com>
2024-11-27 17:57:42 +0000

    SaveState: Bounds check FreezeMem loads

--- pcsx2/SaveState.cpp
+++ pcsx2/SaveState.cpp
@@ -315,6 +315,9 @@ memLoadingState::memLoadingState(const VmStateBuffer& load_from)
 // Loading of state data from a memory buffer...
 void memLoadingState::FreezeMem( void* data, int size )
 {
+	if (m_idx + size > m_memory.size())
+		m_error = true;
+
 	if (m_error)
 	{
 		std::memset(data, 0, size);
