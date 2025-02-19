2b8875ae3974a12c2058f86d82d617f4e856f928
GovanifY <me@govanify.com>
2025-02-19 02:18:19 +0100

    appimage: set WM_CLASS to AppRun.wrapped
    This fixes an icon affiliation issue for appimages

--- .github/workflows/scripts/linux/appimage-qt.sh
+++ .github/workflows/scripts/linux/appimage-qt.sh
@@ -122,6 +122,7 @@ done
 
 echo "Copying desktop file..."
 cp "$PCSX2DIR/.github/workflows/scripts/linux/pcsx2-qt.desktop" "net.pcsx2.PCSX2.desktop"
+sed -i "s/StartupWMClass=PCSX2/StartupWMClass=AppRun.wrapped/g" "net.pcsx2.PCSX2.desktop"
 cp "$PCSX2DIR/bin/resources/icons/AppIconLarge.png" "PCSX2.png"
 
 echo "Running linuxdeploy to create AppDir..."
