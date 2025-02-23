b6dc0882c01ca6442202dbbae82eca24dc1d0cfa
refractionpcsx2 <refraction@gmail.com>
2025-02-23 19:19:34 +0000

    GS/HW: Add new HPO - Align to Native With Texture Offset

--- pcsx2/ImGui/FullscreenUI.cpp
+++ pcsx2/ImGui/FullscreenUI.cpp
@@ -3809,7 +3809,8 @@ void FullscreenUI::DrawGraphicsSettingsPage(SettingsInterface* bsi, bool show_ad
 				FSUI_NSTR("Normal (Vertex)"),
 				FSUI_NSTR("Special (Texture)"),
 				FSUI_NSTR("Special (Texture - Aggressive)"),
-				FSUI_NSTR("Align To Native"),
+				FSUI_NSTR("Align to Native"),
+				FSUI_NSTR("Align to Native - with Texture Offset"),
 			};
 			static constexpr const char* s_native_scaling_options[] = {
 				FSUI_NSTR("Normal (Default)"),
