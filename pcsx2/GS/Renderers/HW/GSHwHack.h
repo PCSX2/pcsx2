18ff51ff940e2d3843c74158cecb13af05ea39e5
JordanTheToaster <jordanh48@ymail.com>
2025-05-02 09:35:14 +0100

    GS/HW: Remove Haunting Ground CRC

--- pcsx2/GS/Renderers/HW/GSHwHack.h
+++ pcsx2/GS/Renderers/HW/GSHwHack.h
@@ -37,7 +37,6 @@ public:
 	static bool OI_SonicUnleashed(GSRendererHW& r, GSTexture* rt, GSTexture* ds, GSTextureCache::Source* t);
 	static bool OI_ArTonelico2(GSRendererHW& r, GSTexture* rt, GSTexture* ds, GSTextureCache::Source* t);
 	static bool OI_BurnoutGames(GSRendererHW& r, GSTexture* rt, GSTexture* ds, GSTextureCache::Source* t);
-	static bool OI_HauntingGround(GSRendererHW& r, GSTexture* rt, GSTexture* ds, GSTextureCache::Source* t);
 
 	static bool MV_Growlanser(GSRendererHW& r);
 	static bool MV_Ico(GSRendererHW& r);
