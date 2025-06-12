f12594f08e3b8cd25bb9b5f285de126362166bbc
TheLastRar <TheLastRar@users.noreply.github.com>
2025-06-12 17:53:35 +0100

    Deps: Build FreeType & HalfBuzz on Linux

--- 3rdparty/imgui/include/imconfig.h
+++ 3rdparty/imgui/include/imconfig.h
@@ -91,7 +91,7 @@
 // - plutosvg is currently easier to install, as e.g. it is part of vcpkg. It will support more fonts and may load them faster. See misc/freetype/README for instructions.
 // - Both require headers to be available in the include path + program to be linked with the library code (not provided).
 // - (note: lunasvg implementation is based on Freetype's rsvg-port.c which is licensed under CeCILL-C Free Software License Agreement)
-//#define IMGUI_ENABLE_FREETYPE_PLUTOSVG
+#define IMGUI_ENABLE_FREETYPE_PLUTOSVG
 //#define IMGUI_ENABLE_FREETYPE_LUNASVG
 
 //---- Use stb_truetype to build and rasterize the font atlas (default)
