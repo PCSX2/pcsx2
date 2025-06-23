9b65087c6f607a3b97f9836a4211fcd80fd24c8f
TheLastRar <TheLastRar@users.noreply.github.com>
2025-06-23 10:55:11 +0100

    FSUI: Centre disc/exe icon in game list selected preview
    The ImGui::Image path was missing the centring logic when drawing svg icons.
    Covers where already correctly centred.

--- pcsx2/ImGui/FullscreenUI.cpp
+++ pcsx2/ImGui/FullscreenUI.cpp
@@ -7009,6 +7009,9 @@ void FullscreenUI::DrawGameCover(const GameList::Entry* entry, const ImVec2& siz
 		const ImVec2 image_square(min_size, min_size);
 		GSTexture* const icon_texture = GetTextureForGameListEntryType(entry->type, image_square);
 
+		const ImRect image_rect(CenterImage(size, image_square));
+
+		ImGui::SetCursorPos(origin + image_rect.Min);
 		DrawSvgTexture(icon_texture, image_square);
 	}
 	// Pretend the image we drew was the the size passed to us
@@ -7050,6 +7053,9 @@ void FullscreenUI::DrawFallbackCover(const ImVec2& size)
 	const ImVec2 image_square(min_size, min_size);
 	GSTexture* const icon_texture = GetTextureForGameListEntryType(GameList::EntryType::PS2Disc, image_square);
 
+	const ImRect image_rect(CenterImage(size, image_square));
+
+	ImGui::SetCursorPos(origin + image_rect.Min);
 	DrawSvgTexture(icon_texture, image_square);
 
 	// Pretend the image we drew was the the size passed to us
