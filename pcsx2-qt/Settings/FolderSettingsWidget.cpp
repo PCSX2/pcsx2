fdbe9305a9d811be87901d31e6d9effa21b0238a
KamFretoZ <14798312+kamfretoz@users.noreply.github.com>
2025-07-08 16:45:15 +0700

    Qt: Move screenshot section to Recording tab
    Qt: Move Video Dump Directory to Folder Settings

--- pcsx2-qt/Settings/FolderSettingsWidget.cpp
+++ pcsx2-qt/Settings/FolderSettingsWidget.cpp
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: GPL-3.0+
 
 #include <QtWidgets/QMessageBox>
-#include <algorithm>
 
 #include "FolderSettingsWidget.h"
 #include "SettingWidgetBinder.h"
@@ -20,6 +19,7 @@ FolderSettingsWidget::FolderSettingsWidget(SettingsWindow* dialog, QWidget* pare
 	SettingWidgetBinder::BindWidgetToFolderSetting(sif, m_ui.covers, m_ui.coversBrowse, m_ui.coversOpen, m_ui.coversReset, "Folders", "Covers", Path::Combine(EmuFolders::DataRoot, "covers"));
 	SettingWidgetBinder::BindWidgetToFolderSetting(sif, m_ui.snapshots, m_ui.snapshotsBrowse, m_ui.snapshotsOpen, m_ui.snapshotsReset, "Folders", "Snapshots", Path::Combine(EmuFolders::DataRoot, "snaps"));
 	SettingWidgetBinder::BindWidgetToFolderSetting(sif, m_ui.saveStates, m_ui.saveStatesBrowse, m_ui.saveStatesOpen, m_ui.saveStatesReset, "Folders", "SaveStates", Path::Combine(EmuFolders::DataRoot, "sstates"));
+	SettingWidgetBinder::BindWidgetToFolderSetting(sif, m_ui.videoDumpingDirectory, m_ui.videoDumpingDirectoryBrowse, m_ui.videoDumpingDirectoryOpen, m_ui.videoDumpingDirectoryReset, "Folders", "Videos", Path::Combine(EmuFolders::DataRoot, "videos"));
 }
 
 FolderSettingsWidget::~FolderSettingsWidget() = default;
