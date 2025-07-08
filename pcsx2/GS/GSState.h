08ede10204e7e8f682bcdc9fb2ee2a3074d10e8a
TJnotJT <tjnotjt.github@gmail.com>
2025-07-08 12:11:49 -0400

    GS: Add secondary vertex buffer for copy/modifying vertices.
    Currently only used in HW renderer to fix vertices for provoking-first-vertex APIs.

--- pcsx2/GS/GSState.h
+++ pcsx2/GS/GSState.h
@@ -136,6 +136,7 @@ protected:
 	struct
 	{
 		GSVertex* buff;
+		GSVertex* buff_copy;            // same size buffer to copy/modify the original buffer
 		u32 head, tail, next, maxcount; // head: first vertex, tail: last vertex + 1, next: last indexed + 1
 		u32 xy_tail;
 		GSVector4i xy[4];
