ac105943c3ff204bff8a3de5c81ae46c39d2cf55
TJnotJT <tjnotjt.github@gmail.com>
2025-09-03 23:00:48 -0400

    GS: Early detection of shuffles in vertex kick to prevent autoflushing.

--- pcsx2/GS/GSState.h
+++ pcsx2/GS/GSState.h
@@ -172,6 +172,7 @@ protected:
 	void GrowVertexBuffer();
 	bool IsAutoFlushDraw(u32 prim, int& tex_layer);
 	template<u32 prim> void HandleAutoFlush();
+	bool EarlyDetectShuffle(u32 prim);
 	void CheckCLUTValidity(u32 prim);
 
 	template <u32 prim, bool auto_flush> void VertexKick(u32 skip);
