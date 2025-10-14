016d6cd2c4b6569e54f974a59c6646c98057cd69
TheLastRar <TheLastRar@users.noreply.github.com>
2025-10-14 01:39:21 +0100

    DEV9: Add Lan UDP port forwarding for Sockets

--- pcsx2/Config.h
+++ pcsx2/Config.h
@@ -1029,6 +1029,13 @@ struct Pcsx2Config
 		};
 		static const char* DnsModeNames[];
 
+		enum struct PortMode : int
+		{
+			UDP = 0,
+			TCP = 1,
+		};
+		static const char* PortModeNames[];
+
 		struct HostEntry
 		{
 			std::string Url;
@@ -1038,6 +1045,17 @@ struct Pcsx2Config
 
 			bool operator==(const HostEntry& right) const;
 			bool operator!=(const HostEntry& right) const;
+		};
+
+		struct PortEntry
+		{
+			uint Port{};
+			std::string Desc;
+			PortMode Protocol;
+			bool Enabled;
+
+			bool operator==(const PortEntry& right) const;
+			bool operator!=(const PortEntry& right) const;
 		};
 
 		bool EthEnable{false};
@@ -1058,6 +1076,9 @@ struct Pcsx2Config
 		DnsMode ModeDNS2{DnsMode::Auto};
 
 		std::vector<HostEntry> EthHosts;
+
+		bool LanMode{false};
+		std::vector<PortEntry> OpenPorts;
 
 		bool HddEnable{false};
 		std::string HddFile;
