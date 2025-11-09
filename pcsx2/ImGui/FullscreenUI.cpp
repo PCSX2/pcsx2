0cd14c991976b22fdbbe34c8fbee249139cbb26c
chaoticgd <43898262+chaoticgd@users.noreply.github.com>
2025-11-09 18:01:22 +0000

    VMManager: Fix read from moved-from string boot_params.filename

--- pcsx2/ImGui/FullscreenUI.cpp
+++ pcsx2/ImGui/FullscreenUI.cpp
@@ -1448,7 +1448,7 @@ void FullscreenUI::DoStartPath(const std::string& path, std::optional<s32> state
 		if (VMManager::HasValidVM())
 			return;
 
-		if (VMManager::Initialize(std::move(params)))
+		if (VMManager::Initialize(params))
 			VMManager::SetState(VMState::Running);
 	});
 }
@@ -1472,7 +1472,7 @@ void FullscreenUI::DoStartBIOS()
 			return;
 
 		VMBootParameters params;
-		if (VMManager::Initialize(std::move(params)))
+		if (VMManager::Initialize(params))
 			VMManager::SetState(VMState::Running);
 		else
 			SwitchToLanding();
@@ -7628,7 +7628,7 @@ void FullscreenUI::DoLoadState(std::string path)
 			VMBootParameters params;
 			params.filename = std::move(boot_path);
 			params.save_state = std::move(path);
-			if (VMManager::Initialize(std::move(params)))
+			if (VMManager::Initialize(params))
 				VMManager::SetState(VMState::Running);
 		}
 	});
