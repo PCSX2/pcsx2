3ef7b6ca39fa54ae96790ce8328f16ea09076a53
TJnotJT <tjnotjt.github@gmail.com>
2025-11-13 14:49:06 -0500

    GS/SW: Adjust bounds in assertion to present false positive.
    Account for the fact that bottom/right coordinates are exclusive.

--- pcsx2/GS/Renderers/SW/GSRasterizer.cpp
+++ pcsx2/GS/Renderers/SW/GSRasterizer.cpp
@@ -1550,7 +1550,7 @@ void GSRasterizerList::Queue(const GSRingHeap::SharedPtr<GSRasterizerData>& data
 		m_ds.SetupDraw(*data.get());
 	}
 
-	pxAssert(r.top >= 0 && r.top < 2048 && r.bottom >= 0 && r.bottom < 2048);
+	pxAssert(r.top >= 0 && r.top <= 2048 && r.bottom >= 0 && r.bottom <= 2048);
 
 	int top = r.top >> m_thread_height;
 	int bottom = std::min<int>((r.bottom + (1 << m_thread_height) - 1) >> m_thread_height, top + (int)m_workers.size());
