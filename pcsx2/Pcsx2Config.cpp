db8f2d21ccf7681f5372235079fd70fe266ecc12
SternXD <stern@sidestore.io>
2025-11-21 03:38:03 -0500

    Qt: Add audio capture volume setting

--- pcsx2/Pcsx2Config.cpp
+++ pcsx2/Pcsx2Config.cpp
@@ -1072,6 +1072,7 @@ void Pcsx2Config::GSOptions::LoadSave(SettingsWrapper& wrap)
 	SettingsWrapBitfieldEx(VideoCaptureWidth, "VideoCaptureWidth");
 	SettingsWrapBitfieldEx(VideoCaptureHeight, "VideoCaptureHeight");
 	SettingsWrapBitfieldEx(AudioCaptureBitrate, "AudioCaptureBitrate");
+	SettingsWrapBitfieldEx(AudioCaptureVolume, "AudioCaptureVolume");
 
 	SettingsWrapEntry(Adapter);
 	SettingsWrapEntry(HWDumpDirectory);
