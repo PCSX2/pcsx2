ca5377299af8fa1bbb07ade94d8761026db9b2db
Ty <amfobes@gmail.com>
2025-11-22 17:20:13 -0500

    GS SW: Use implicit GSVector4i int32x4_t conversion

--- pcsx2/GS/Renderers/SW/GSRendererSW.cpp
+++ pcsx2/GS/Renderers/SW/GSRendererSW.cpp
@@ -374,7 +374,7 @@ void GSRendererSW::RewriteVerticesIfSTOverflow()
 					const GSVector4 q = (primclass == GS_SPRITE_CLASS) ? stcq[1].wwww() : stcq[j].wwww();
 					stcq[j] = stcq[j].blend32(uv_new * q, GSVector4::cast(rewrite));
 
-					vertex_copy[i + j].m[0] = GSVector4i::cast(stcq[j]).m;
+					vertex_copy[i + j].m[0] = GSVector4i::cast(stcq[j]);
 					vertex_copy[i + j].m[1] = vertex[index[i + j]].m[1];
 					index[i + j] = i + j;
 				}
