5ce8abfe13e2baff3462890e897c7f23b45590aa
TellowKrinkle <tellowkrinkle@gmail.com>
2025-11-22 23:52:39 -0600

    Host: Add RunOnGSThread helper

--- tests/ctest/core/StubHost.cpp
+++ tests/ctest/core/StubHost.cpp
@@ -152,6 +152,10 @@ void Host::RunOnCPUThread(std::function<void()> function, bool block /* = false 
 {
 }
 
+void Host::RunOnGSThread(std::function<void()> function)
+{
+}
+
 void Host::RefreshGameListAsync(bool invalidate_cache)
 {
 }
