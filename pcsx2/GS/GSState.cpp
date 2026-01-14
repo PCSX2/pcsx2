0244cde98d4c8121bbcfe61498ab0095fb74bdfa
refractionpcsx2 <refraction@gmail.com>
2026-01-14 14:21:43 +0000

    GS: Correct GS to GS direction check and simplify

--- pcsx2/GS/GSState.cpp
+++ pcsx2/GS/GSState.cpp
@@ -2528,7 +2528,7 @@ void GSState::Move()
 	if (m_env.TRXPOS.DIRX)
 	{
 		// Only allow it to reverse if the destination is behind the source.
-		if (!intersect || (sx <= dx && (sx == dx || ((!m_env.TRXPOS.DIRY && sy >= dy) || (m_env.TRXPOS.DIRY && sy < dy)))))
+		if (!intersect || sx < dx)
 		{
 			sx += w - 1;
 			dx += w - 1;
@@ -2538,7 +2538,7 @@ void GSState::Move()
 	if (m_env.TRXPOS.DIRY)
 	{
 		// Only allow it to reverse if the destination is behind the source.
-		if (!intersect || (sy <= dy && (sy == dy || ((!m_env.TRXPOS.DIRX && sx >= dx) || (m_env.TRXPOS.DIRX && sx < dx)))))
+		if (!intersect || sy < dy)
 		{
 			sy += h - 1;
 			dy += h - 1;
