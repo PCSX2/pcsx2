af3877ac7a0aa03285e0ea232d5405c732b6c127
TheLastRar <TheLastRar@users.noreply.github.com>
2026-01-31 16:17:18 +0000

    GS/DX12: Init enhanced barriers to false

--- pcsx2/GS/Renderers/DX12/GSDevice12.h
+++ pcsx2/GS/Renderers/DX12/GSDevice12.h
@@ -314,7 +314,7 @@ private:
 	bool m_allow_tearing_supported = false;
 	bool m_using_allow_tearing = false;
 	bool m_is_exclusive_fullscreen = false;
-	bool m_enhanced_barriers = true;
+	bool m_enhanced_barriers = false;
 	bool m_device_lost = false;
 
 	ComPtr<ID3D12RootSignature> m_tfx_root_signature;
