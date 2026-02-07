4925c3635eb96c001320013a9266e61c8aedbea1
chaoticgd <43898262+chaoticgd@users.noreply.github.com>
2026-02-07 03:02:20 +0000

    Debugger: Prevent duplicate saved addresses from being loaded

--- pcsx2-qt/Debugger/Memory/SavedAddressesView.cpp
+++ pcsx2-qt/Debugger/Memory/SavedAddressesView.cpp
@@ -30,7 +30,8 @@ SavedAddressesView::SavedAddressesView(const DebuggerViewParameters& parameters)
 			DebuggerSettingsManager::loadGameSettings(m_model);
 	});
 
-	DebuggerSettingsManager::loadGameSettings(m_model);
+	if (m_model->rowCount() == 0)
+		DebuggerSettingsManager::loadGameSettings(m_model);
 
 	for (std::size_t i = 0; auto mode : SavedAddressesModel::HeaderResizeModes)
 	{
