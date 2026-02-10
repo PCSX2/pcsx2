0727fec73232c472321794e08df20f33e112fb15
Ziemas <ziemas@ziemas.se>
2026-02-10 03:06:28 +0100

    AudioStream/GSCapture: Use float output

--- pcsx2/Host/SDLAudioStream.cpp
+++ pcsx2/Host/SDLAudioStream.cpp
@@ -107,7 +107,7 @@ bool SDLAudioStream::OpenDevice(bool stretch_enabled, Error* error)
 
 	SDL_SetHint(SDL_HINT_AUDIO_DEVICE_SAMPLE_FRAMES, fmt::format("{}", samples).c_str());
 
-	const SDL_AudioSpec spec = {SDL_AUDIO_S16LE, m_output_channels, static_cast<int>(m_sample_rate)};
+	const SDL_AudioSpec spec = {SDL_AUDIO_F32, m_output_channels, static_cast<int>(m_sample_rate)};
 	m_stream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &spec, AudioCallback, static_cast<void*>(this));
 
 	SDL_AudioSpec obtained_spec = {};
