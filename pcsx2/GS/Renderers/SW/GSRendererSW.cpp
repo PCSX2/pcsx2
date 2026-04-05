e82caf40760d322636ab314ac35709080838306a
TJnotJT <tjnotjt.github@gmail.com>
2026-04-05 12:49:42 -0400

    GS: Ignore CRTC outputs with negative sizes.

--- pcsx2/GS/Renderers/SW/GSRendererSW.cpp
+++ pcsx2/GS/Renderers/SW/GSRendererSW.cpp
@@ -106,8 +106,8 @@ GSTexture* GSRendererSW::GetOutput(int i, float& scale, int& y_offset)
 	GSVector2i framebufferSize = PCRTCDisplays.GetFramebufferSize(i);
 	GSVector4i framebufferRect = PCRTCDisplays.GetFramebufferRect(i);
 
-	// Try to avoid broken/incomplete setups which are probably ingnored on console, but can cause us problems.
-	if (framebufferRect.rempty() || curFramebuffer.FBW == 0)
+	// Try to avoid broken/incomplete setups which are probably ignored on console, but can cause us problems.
+	if (framebufferRect.rempty() || curFramebuffer.FBW == 0 || framebufferSize.x < 0 || framebufferSize.y < 0)
 		return nullptr;
 
 	const int w = curFramebuffer.FBW * 64;
