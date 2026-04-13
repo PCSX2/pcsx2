9f67f72d3cf2d33231a520d6ea784122dd5358cc
KamFretoZ <14798312+kamfretoz@users.noreply.github.com>
2026-04-13 19:45:19 +0700

    Qt: Revamp the status bar

--- pcsx2/SPU2/spu2.cpp
+++ pcsx2/SPU2/spu2.cpp
@@ -150,12 +150,18 @@ void SPU2::UpdateSampleRate()
 
 u32 SPU2::GetOutputVolume()
 {
-	return s_output_stream->GetOutputVolume();
+	return s_output_stream ? s_output_stream->GetOutputVolume() : 0;
 }
 
 void SPU2::SetOutputVolume(u32 volume)
 {
-	s_output_stream->SetOutputVolume(volume);
+	if (VMManager::GetTargetSpeed() == 1.0f)
+		s_standard_volume = volume;
+	else
+		s_fast_forward_volume = volume;
+
+	if (s_output_stream)
+		SPU2::UpdateOutputVolume();
 }
 
 float SPU2::GetNominalRate()
@@ -198,9 +204,9 @@ void SPU2::SaveOutputVolume()
 	if (!s_output_muted)
 	{
 		if (VMManager::GetTargetSpeed() == 1.0f)
-			s_standard_volume = s_output_stream->GetOutputVolume();
+			s_standard_volume = GetOutputVolume();
 		else
-			s_fast_forward_volume = s_output_stream->GetOutputVolume();
+			s_fast_forward_volume = GetOutputVolume();
 	}
 }
 
@@ -268,12 +274,12 @@ void SPU2::OnTargetSpeedChanged()
 	{
 		if (VMManager::GetTargetSpeed() == 1.0f)
 		{
-			s_fast_forward_volume = s_output_stream->GetOutputVolume();
+			s_fast_forward_volume = GetOutputVolume();
 			s_output_stream->SetOutputVolume(s_standard_volume);
 		}
 		else
 		{
-			s_standard_volume = s_output_stream->GetOutputVolume();
+			s_standard_volume = GetOutputVolume();
 			s_output_stream->SetOutputVolume(s_fast_forward_volume);
 		}
 	}
