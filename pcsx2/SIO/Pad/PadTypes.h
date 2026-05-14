d19214152dca9484f09037dffe1a86ba72862be1
RedPanda4552 <redpanda4552@gmail.com>
2026-05-14 12:38:49 -0400

    Pad: Add option to correct diagonal scale on analog sticks

--- pcsx2/SIO/Pad/PadTypes.h
+++ pcsx2/SIO/Pad/PadTypes.h
@@ -101,6 +101,7 @@ namespace Pad
 	// Default stick deadzone/sensitivity.
 	static constexpr float DEFAULT_STICK_DEADZONE = 0.0f;
 	static constexpr float DEFAULT_STICK_SCALE = 1.33f;
+	static constexpr float DEFAULT_USE_DIAGONAL_SCALE_CORRECTION = false;
 	static constexpr float DEFAULT_MOTOR_SCALE = 1.0f;
 	static constexpr float DEFAULT_PRESSURE_MODIFIER = 0.5f;
 	static constexpr float DEFAULT_BUTTON_DEADZONE = 0.0f;
