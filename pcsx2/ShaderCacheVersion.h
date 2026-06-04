13d3491571f82e567eada1777d09f91a4c97d9c4
lightningterror <18107717+lightningterror@users.noreply.github.com>
2026-06-04 22:31:55 +0200

    GS/GL: Add basic clip control fallback.
    When GLAD_GL_ARB_clip_control is not supported then change
    the vertex shader depth ranges to make sure it matches -1 1.

--- pcsx2/ShaderCacheVersion.h
+++ pcsx2/ShaderCacheVersion.h
@@ -3,4 +3,4 @@
 
 /// Version number for GS and other shaders. Increment whenever any of the contents of the
 /// shaders change, to invalidate the cache.
-static constexpr u32 SHADER_CACHE_VERSION = 100; // Last changed in PR 14503
+static constexpr u32 SHADER_CACHE_VERSION = 101; // Last changed in PR 14547
