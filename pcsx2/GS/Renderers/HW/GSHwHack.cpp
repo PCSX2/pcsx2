5ef2087e55652724c77c5ed5f37eb0e419ceb38f
TJnotJT <tjnotjt.github@gmail.com>
2026-06-21 00:54:19 -0500

    GS/HW: Use CreateCompatible() in texture cache to propagate correct formats.
    Co-authored-by: TellowKrinkle

--- pcsx2/GS/Renderers/HW/GSHwHack.cpp
+++ pcsx2/GS/Renderers/HW/GSHwHack.cpp
@@ -1135,9 +1135,9 @@ bool GSHwHack::OI_SonicUnleashed(GSRendererHW& r, GSTexture* rt, GSTexture* ds, 
 		rt_again->m_valid.y /= 2;
 		rt_again->m_valid.w /= 2;
 		rt_again->m_TEX0.PSM = PSMCT32;
-		GSTexture* tex = g_gs_device->CreateRenderTarget(
-			rt_again->m_unscaled_size.x * rt_again->m_scale, rt_again->m_unscaled_size.y * rt_again->m_scale,
-			GSTexture::Format::Color, false);
+		GSTexture* tex = g_gs_device->CreateCompatible(rt_again->m_texture,
+			static_cast<int>(rt_again->m_unscaled_size.x * rt_again->m_scale),
+			static_cast<int>(rt_again->m_unscaled_size.y * rt_again->m_scale), false);
 
 		if (!tex)
 			return false;
