583638f5d1c973ed4feef289884b81601c85e42e
TJnotJT <tjnotjt.github@gmail.com>
2026-06-20 21:21:58 -0400

    GS/DX12: Fix DX12 renderpass restart optimization rebase error.

--- pcsx2/GS/Renderers/DX12/GSDevice12.cpp
+++ pcsx2/GS/Renderers/DX12/GSDevice12.cpp
@@ -4441,12 +4441,12 @@ void GSDevice12::RenderHW(GSHWDrawConfig& config)
 			draw_rt = m_current_render_target;
 			m_pipeline_selector.rt = true;
 		}
-	}
-	else if (!(draw_ds || draw_ds_rov) && m_current_depth_target && config.tex != m_current_depth_target &&
-		draw_rt && m_current_depth_target->GetSize() == draw_rt->GetSize())
-	{
-		draw_ds = m_current_depth_target;
-		m_pipeline_selector.ds = true;
+		else if (!(draw_ds || draw_ds_rov) && m_current_depth_target && config.tex != m_current_depth_target &&
+			draw_rt && m_current_depth_target->GetSize() == draw_rt->GetSize())
+		{
+			draw_ds = m_current_depth_target;
+			m_pipeline_selector.ds = true;
+		}
 	}
 
 	GSTexture12* draw_ds_as_rt = static_cast<GSTexture12*>(m_ds_as_rt);
