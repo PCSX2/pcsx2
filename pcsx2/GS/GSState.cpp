7d049e7d795e51239660ca3d24911c3ac099b1cf
TJnotJT <tjnotjt.github@gmail.com>
2026-07-08 18:42:43 -0400

    GS/HW: Continuous and oneshot ROV heuristics.

--- pcsx2/GS/GSState.cpp
+++ pcsx2/GS/GSState.cpp
@@ -2637,6 +2637,7 @@ void GSState::FlushPrim()
 		m_quad_check_valid_shuffle = false;
 		m_drawlist.clear();
 		m_drawlist_bbox.clear();
+		m_draw_coarse_rasterize.clear();
 
 		if (GSConfig.ShouldDump(s_n, g_perfmon.GetFrame()))
 		{
