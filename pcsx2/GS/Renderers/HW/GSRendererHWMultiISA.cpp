86dc3189c7e5169beea33ef8c736a62cd88dacd9
refractionpcsx2 <refraction@gmail.com>
2026-07-28 20:13:09 +0100

    GS/HW: Consider FBMSK combos when using CSBW

--- pcsx2/GS/Renderers/HW/GSRendererHWMultiISA.cpp
+++ pcsx2/GS/Renderers/HW/GSRendererHWMultiISA.cpp
@@ -545,7 +545,19 @@ bool GSRendererHWFunctions::SwPrimRender(GSRendererHW& hw, bool invalidate_tc, b
 	static_cast<GSSingleRasterizer*>(hw.m_sw_rasterizer.get())->Draw(data);
 
 	if (invalidate_tc)
-		g_texture_cache->InvalidateVideoMem(context->offset.fb, bbox);
+	{
+		GSOffset frame_offs = context->offset.fb;
+
+		if (GSLocalMemory::m_psm[context->FRAME.PSM].trbpp == 32 && context->FRAME.FBMSK)
+		{
+			if (context->FRAME.FBMSK == 0xFF000000)
+				frame_offs = GSRendererHW::GetInstance()->m_mem.GetOffset(context->FRAME.Block(), context->FRAME.FBW, PSMCT24);
+			else if (context->FRAME.FBMSK == 0x00FFFFFF)
+				frame_offs = GSRendererHW::GetInstance()->m_mem.GetOffset(context->FRAME.Block(), context->FRAME.FBW, PSMT8H);
+		}
+
+		g_texture_cache->InvalidateVideoMem(frame_offs, bbox);
+	}
 
 	// Jak does sw prim render, then draws to the same target, and it needs to be uploaded.
 	if (add_ee_transfer)
