5793dbc1efadc632d02ba1e224131597d7a9e766
TJnotJT <tjnotjt.github@gmail.com>
2026-07-30 08:21:31 -0400

    GS:HW: Only enable GPU timing/stats in backend if the option is enabled.
    Helps prevent a crash in Mesa3D Windows drivers.

--- pcsx2/GS/Renderers/OpenGL/GSDeviceOGL.cpp
+++ pcsx2/GS/Renderers/OpenGL/GSDeviceOGL.cpp
@@ -1292,7 +1292,7 @@ bool GSDeviceOGL::SetGPUPipelineStatisticsEnabled(bool enabled)
 	else
 		DestroyPipelineStatisticsQueries();
 
-	return true;
+	return (enabled == m_gpu_pipeline_statistics_enabled);
 }
 
 void GSDeviceOGL::DrawPrimitive()
