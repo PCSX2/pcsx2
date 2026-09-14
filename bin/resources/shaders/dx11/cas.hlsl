9b0ce3f4a328bc6fe724abf83197cc747347d3b5
lightningterror <18107717+lightningterror@users.noreply.github.com>
2026-09-14 14:10:24 +0200

    GS/DX11: Add pixel shader based CAS.
    It's much faster than uav and it's backwards compatible across all feature levels.

--- bin/resources/shaders/dx11/cas.hlsl
+++ bin/resources/shaders/dx11/cas.hlsl
@@ -19,13 +19,17 @@
 
 cbuffer cb : register(b0)
 {
-  uint4 const0;
-  uint4 const1;
-  int2 srcOffset;
+	uint4 const0;
+	uint4 const1;
+	int2 srcOffset;
+	int2 pad;
 };
 
 Texture2D InputTexture : register(t0);
-RWTexture2D<float4> OutputTexture : register(u0);
+
+#if PS_CAS == 0
+	RWTexture2D<float4> OutputTexture : register(u0);
+#endif
 
 #define A_GPU 1
 #define A_HLSL 1
@@ -34,7 +38,7 @@ RWTexture2D<float4> OutputTexture : register(u0);
 
 AF3 CasLoad(ASU2 p)
 {
-  return InputTexture.Load(int3(srcOffset, 0) + int3(p, 0)).rgb;
+	return InputTexture.Load(int3(srcOffset, 0) + int3(p, 0)).rgb;
 }
 
 // Lets you transform input from the load into a linear color space between 0 and 1. See ffx_cas.h
@@ -43,33 +47,56 @@ void CasInput(inout AF1 r, inout AF1 g, inout AF1 b) {}
 
 #include "ffx_cas.h"
 
+#if PS_CAS == 0
+
 [numthreads(64, 1, 1)]
-void main(uint3 LocalThreadId : SV_GroupThreadID, uint3 WorkGroupId : SV_GroupID)
+void cs_main(uint3 LocalThreadId : SV_GroupThreadID, uint3 WorkGroupId : SV_GroupID)
 {
-  // Do remapping of local xy in workgroup for a more PS-like swizzle pattern.
-  AU2 gxy = ARmp8x8(LocalThreadId.x) + AU2(WorkGroupId.x << 4u, WorkGroupId.y << 4u);
+	// Do remapping of local xy in workgroup for a more PS-like swizzle pattern.
+	AU2 gxy = ARmp8x8(LocalThreadId.x) + AU2(WorkGroupId.x << 4u, WorkGroupId.y << 4u);
 
 #if CAS_SHARPEN_ONLY
-  const bool sharpenOnly = true;
+	const bool sharpenOnly = true;
 #else
-  const bool sharpenOnly = false;
+	const bool sharpenOnly = false;
 #endif
 
-  // Filter.
-  AF3 c = (float3)0.0f;
+	// Filter.
+	AF3 c = (float3)0.0f;
 
-  CasFilter(c.r, c.g, c.b, gxy, const0, const1, sharpenOnly);
-  OutputTexture[ASU2(gxy)] = AF4(c, 1);
-  gxy.x += 8u;
+	CasFilter(c.r, c.g, c.b, gxy, const0, const1, sharpenOnly);
+	OutputTexture[ASU2(gxy)] = AF4(c, 1);
+	gxy.x += 8u;
 
-  CasFilter(c.r, c.g, c.b, gxy, const0, const1, sharpenOnly);
-  OutputTexture[ASU2(gxy)] = AF4(c, 1);
-  gxy.y += 8u;
+	CasFilter(c.r, c.g, c.b, gxy, const0, const1, sharpenOnly);
+	OutputTexture[ASU2(gxy)] = AF4(c, 1);
+	gxy.y += 8u;
 
-  CasFilter(c.r, c.g, c.b, gxy, const0, const1, sharpenOnly);
-  OutputTexture[ASU2(gxy)] = AF4(c, 1);
-  gxy.x -= 8u;
+	CasFilter(c.r, c.g, c.b, gxy, const0, const1, sharpenOnly);
+	OutputTexture[ASU2(gxy)] = AF4(c, 1);
+	gxy.x -= 8u;
 
-  CasFilter(c.r, c.g, c.b, gxy, const0, const1, sharpenOnly);
-  OutputTexture[ASU2(gxy)] = AF4(c, 1);
+	CasFilter(c.r, c.g, c.b, gxy, const0, const1, sharpenOnly);
+	OutputTexture[ASU2(gxy)] = AF4(c, 1);
 }
+
+#elif PS_CAS == 1
+
+float4 ps_main(float4 position : SV_Position, float2 uv : TEXCOORD0) : SV_Target
+{
+	// Pixel coordinate for the current fragment
+	AU2 gxy = AU2(position.xy);
+
+#if CAS_SHARPEN_ONLY
+	const bool sharpenOnly = true;
+#else
+	const bool sharpenOnly = false;
+#endif
+
+	AF3 c = (float3)0.0f;
+	CasFilter(c.r, c.g, c.b, gxy, const0, const1, sharpenOnly);
+
+	return AF4(c, 1.0f);
+}
+
+#endif
