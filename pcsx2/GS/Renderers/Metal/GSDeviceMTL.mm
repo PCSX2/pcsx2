e5b0079b73592ec7a036d2c25d10854ef64db92b
TellowKrinkle <tellowkrinkle@gmail.com>
2026-09-22 20:09:40 -0500

    GS:MTL: Mention OCLP in the no-metal-devices error
    It seems to be the top cause of missing Metal devices

--- pcsx2/GS/Renderers/Metal/GSDeviceMTL.mm
+++ pcsx2/GS/Renderers/Metal/GSDeviceMTL.mm
@@ -999,8 +999,9 @@ bool GSDeviceMTL::Create(GSVSyncMode vsync_mode, bool allow_present_throttle)
 		else if (!GSConfig.Adapter.empty())
 			Console.Warning("Metal: Couldn't find adapter %s, using default", GSConfig.Adapter.c_str());
 		m_dev = GSMTLDevice(MRCTransfer(MTLCreateSystemDefaultDevice()));
-		if (!m_dev.dev) {
-			Host::ReportErrorAsync(TRANSLATE_SV("GSDeviceMTL", "No Metal Devices Available"), TRANSLATE_SV("GSDeviceMTL", "No Metal-supporting GPUs were found.  PCSX2 requires a Metal GPU (available on all Macs from 2012 onwards)."));
+		if (!m_dev.dev)
+		{
+			Host::ReportErrorAsync(TRANSLATE_SV("GSDeviceMTL", "No Metal Devices Available"), TRANSLATE_SV("GSDeviceMTL", "No Metal-supporting GPUs were found.  PCSX2 requires a Metal GPU (available on all Macs from 2012 onwards).  If you're using OCLP on a Mac that should support Metal, try rerunning the OCLP installer."));
 			return false;
 		}
 	}
